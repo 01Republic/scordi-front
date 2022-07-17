4c9c28077a731685eb84e44209d6706c800f926d
seongonecho <seongonecho@gmail.com>
2022-07-17 13:10:03 +0700

    로그인화면

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Next.js https://nextjs.org
 
-evergreen-ui https://evergreen.segment.com
+daisyUI https://daisyui.com
 
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,23 @@
+import {NavBar} from "../../components/NavBar";
+import {TextInput} from "../../components/TextInput";
+
+const LoginPage = () => {
+    return (
+        <>
+            <NavBar/>
+            <div className={"container"}>
+                <div className={"w-[24rem] space-y-4 p-4 m-auto"}>
+                    <h1>payflow</h1>
+                    <TextInput label={"이메일"} required={true} placeholder={"이메일을 입력해주세요"}/>
+                    <TextInput label={"비밀번호"} required={true} placeholder={"비밀번호를 입력해주세요"}/>
+                    <div className={'pt-[1rem] space-y-4'}>
+                        <button className="btn btn-primary btn-block">로그인</button>
+                        <button className="btn btn-outline btn-primary btn-block">회원가입</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginPage
