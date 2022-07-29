ad4915ae48012b1317c45b994a8b7d60f33ffc2d
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2022-07-30 08:54:25 +0900

    Add hotjar CDN code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,6 +69,8 @@ const PanelImage = ({ src, width, height, alt = '' }: { src: string, width: numb
   />
 )
 
+const HOTJAR_ID = '3088152';
+
 const Home: NextPage = () => {
   const paneWidth = '1050px';
 
@@ -83,6 +85,17 @@ const Home: NextPage = () => {
         <title>Payplo | 똑똑한 팀을 위한 SaaS 관리 플랫폼</title>
         <meta name="description" content="똑똑한 팀을 위한 SaaS 관리 플랫폼 | Payplo - 구독 서비스 관리를 한 곳에서 쉽고 편하게" />
         <link rel="icon" href="/logo-transparent.png" />
+        <script title="hotjar cdn" dangerouslySetInnerHTML={{ __html: `
+// Hotjar Tracking Code for https://www.payplo.me/
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:${HOTJAR_ID},hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        ` }} />
       </Head>
 
       <div>
