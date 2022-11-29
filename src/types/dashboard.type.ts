955ecb311a7cf66b7c3f6fc26d1e3395e7205d87
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2022-11-29 13:11:59 +0900

    DashboardSummaryDto 파일 export 키워드 누락 수정

--- src/types/dashboard.type.ts
+++ src/types/dashboard.type.ts
@@ -1,16 +1,16 @@
-type DashboardSummaryDto = {
-  total: number; // 이번달 총 비용
-  didPayAmount: number; // 오늘까지 결제한 금액
-  willPayAmount: number; // 남은 결제 금액
-  totalOnLastMonth: number; // 지난달 총 결제액
-  totalOnThisYear: number; // 연간 총 예상 비용
-}
+export type DashboardSummaryDto = {
+    total: number; // 이번달 총 비용
+    didPayAmount: number; // 오늘까지 결제한 금액
+    willPayAmount: number; // 남은 결제 금액
+    totalOnLastMonth: number; // 지난달 총 결제액
+    totalOnThisYear: number; // 연간 총 예상 비용
+};
 
-type DashboardDaySumDto = {
-  date: string; // 날짜
-  year: number; // 년도
-  month: number; // 월
-  day: number; // 일
-  amount: number; // 금액
-  serviceCount: number; // 서비스 개수
-}
+export type DashboardDaySumDto = {
+    date: string; // 날짜
+    year: number; // 년도
+    month: number; // 월
+    day: number; // 일
+    amount: number; // 금액
+    serviceCount: number; // 서비스 개수
+};
