722a8ecb14bf42d24e9cf3f9474750d6a263dd7c
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2022-11-30 18:50:11 +0900

    로고에 홈으로 가는 링크 삽입

--- src/components/AdminHeader.tsx
+++ src/components/AdminHeader.tsx
@@ -1,98 +1,86 @@
-import { useRouter } from 'next/router';
-import { Icon } from './Icon';
-import { FC } from 'react';
-import { UserDto } from '^types/user.type';
-import { useCurrentUser } from '^hooks/useCurrentUser';
-import { removeToken } from '^api/api';
-import { UserLoginPageRoute } from '^pages/users/login';
-import { UserEditPageRoute } from '^pages/users/edit';
+import {useRouter} from 'next/router';
+import {Icon} from './Icon';
+import {FC} from 'react';
+import {UserDto} from '^types/user.type';
+import {useCurrentUser} from '^hooks/useCurrentUser';
+import {removeToken} from '^api/api';
+import {UserLoginPageRoute} from '^pages/users/login';
+import {UserEditPageRoute} from '^pages/users/edit';
 
 interface AdminHeaderProps {
-  title: string;
-  back?: boolean;
-  inputSpace?: any;
+    title: string;
+    back?: boolean;
+    inputSpace?: any;
 }
 
-export const AdminHeader: React.FC<AdminHeaderProps> = ({
-  title,
-  back,
-  inputSpace,
-}) => {
-  const router = useRouter();
-  return (
-    <div className="sticky top-0 flex h-14 flex-shrink-0 items-center justify-between bg-white px-4 shadow">
-      <div className="flex items-center space-x-3 text-lg font-semibold">
-        {back && (
-          <Icon.ArrowLeft
-            className="cursor-pointer"
-            onClick={() => router.back()}
-          />
-        )}
-        <div>{title}</div>
-      </div>
-      {inputSpace}
-    </div>
-  );
-};
-
-const AdminTopNav: FC<AdminHeaderProps> = ({ title, back, inputSpace }) => {
-  const currentUser = useCurrentUser();
-
-  return (
-    <div className="sticky top-0 navbar bg-white shadow z-10">
-      <div className="flex-1 px-2">
-        <a className="text-lg font-bold">{title}</a>
-      </div>
-      <div className="flex-none gap-2">
-        <ProfileDropDown currentUser={currentUser} />
-      </div>
-    </div>
-  );
-};
-
-const ProfileDropDown = ({ currentUser }: { currentUser: UserDto | null }) => {
-  const router = useRouter();
-
-  return (
-    <div className="dropdown dropdown-end">
-      <label
-        tabIndex={0}
-        className="btn btn-ghost btn-circle avatar placeholder inline-flex"
-      >
-        <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
-          <span className="text-xs">
-            {currentUser ? currentUser.name[0] : '?'}
-          </span>
+export const AdminHeader: React.FC<AdminHeaderProps> = ({title, back, inputSpace}) => {
+    const router = useRouter();
+    return (
+        <div className="sticky top-0 flex h-14 flex-shrink-0 items-center justify-between bg-white px-4 shadow">
+            <div className="flex items-center space-x-3 text-lg font-semibold">
+                {back && <Icon.ArrowLeft className="cursor-pointer" onClick={() => router.back()} />}
+                <div>{title}</div>
+            </div>
+            {inputSpace}
         </div>
-      </label>
-      <ul
-        tabIndex={0}
-        className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-      >
-        {currentUser && (
-          <>
-            <li>
-              <a className="justify-between" href={UserEditPageRoute.path()}>
-                Profile
-              </a>
-            </li>
-            {/*<li><a className="justify-between" href={``}>Settings</a></li>*/}
-            <li>
-              <a
-                className="justify-between"
-                onClick={() => {
-                  removeToken();
-                  router.push(UserLoginPageRoute.path());
-                }}
-              >
-                Logout
-              </a>
-            </li>
-          </>
-        )}
-      </ul>
-    </div>
-  );
+    );
 };
 
-export { AdminTopNav };
+const AdminTopNav: FC<AdminHeaderProps> = ({title, back, inputSpace}) => {
+    const currentUser = useCurrentUser();
+
+    return (
+        <div className="sticky top-0 navbar bg-white shadow z-10">
+            <div className="flex-1 px-2">
+                <a className="text-lg font-bold" href={'/'}>
+                    {title}
+                </a>
+            </div>
+            <div className="flex-none gap-2">
+                <ProfileDropDown currentUser={currentUser} />
+            </div>
+        </div>
+    );
+};
+
+const ProfileDropDown = ({currentUser}: {currentUser: UserDto | null}) => {
+    const router = useRouter();
+
+    return (
+        <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar placeholder inline-flex">
+                <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
+                    <span className="text-xs">{currentUser ? currentUser.name[0] : '?'}</span>
+                </div>
+            </label>
+            <ul
+                tabIndex={0}
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+                {currentUser && (
+                    <>
+                        <li>
+                            <a className="justify-between" href={UserEditPageRoute.path()}>
+                                Profile
+                            </a>
+                        </li>
+                        {/*<li><a className="justify-between" href={``}>Settings</a></li>*/}
+                        <li>
+                            <a
+                                className="justify-between"
+                                onClick={() => {
+                                    removeToken();
+                                    router.push(UserLoginPageRoute.path());
+                                }}
+                            >
+                                Logout
+                            </a>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </div>
+    );
+};
+
+export {AdminTopNav};
--- src/layouts/org/mobileLayout/index.tsx
+++ src/layouts/org/mobileLayout/index.tsx
@@ -1,77 +1,86 @@
-import {OrganizationDto} from "^types/organizationTypes";
-import {PreLoader} from "^components/PreLoader";
-import React from "react";
-import {useRouter} from "next/router";
-import Link from "next/link";
-import {Icon} from "^components/Icon";
-import {OrgHomeRoute} from "^pages/orgs/[id]/home";
-import {UserSettingsPageRoute} from "^pages/users/settings";
-import AppSearchPage, { AppSearchPageRoute } from "^pages/apps/search";
+import {OrganizationDto} from '^types/organizationTypes';
+import {PreLoader} from '^components/PreLoader';
+import React from 'react';
+import {useRouter} from 'next/router';
+import Link from 'next/link';
+import {Icon} from '^components/Icon';
+import {OrgHomeRoute} from '^pages/orgs/[id]/home';
+import {UserSettingsPageRoute} from '^pages/users/settings';
+import AppSearchPage, {AppSearchPageRoute} from '^pages/apps/search';
+import Image from 'next/image';
 
 type OrgMobileLayoutProps = {
     org: OrganizationDto | null;
     children: any;
-}
+};
 
 const OrgMobileLayout = ({org, children}: OrgMobileLayoutProps) => {
     const router = useRouter();
 
-    if (!org) return <PreLoader/>;
+    if (!org) return <PreLoader />;
     return (
         <>
             {router.pathname === OrgHomeRoute.pathname ? (
                 <>
-                    <MobileTopBar/>
+                    <MobileTopBar />
                     {children}
                     <div
-                        className={'flex sticky bg-white bottom-0 w-full border border-t-1 items-center p-[22px] justify-center space-x-20'}>
+                        className={
+                            'flex sticky bg-white bottom-0 w-full border border-t-1 items-center p-[22px] justify-center space-x-20'
+                        }
+                    >
                         {/* TODO: 아이콘 나중에 바꿀 것 */}
-                        <MobileNavItem href={OrgHomeRoute.path(org.id)} icon={<Icon.Home/>}/>
-                        <MobileNavItem href={AppSearchPageRoute.pathname} icon={<Icon.Plus/>}/>
-                        <MobileNavItem href={UserSettingsPageRoute.pathname} icon={<Icon.User/>}/>
+                        <MobileNavItem href={OrgHomeRoute.path(org.id)} icon={<Icon.Home />} />
+                        <MobileNavItem href={AppSearchPageRoute.pathname} icon={<Icon.Plus />} />
+                        <MobileNavItem href={UserSettingsPageRoute.pathname} icon={<Icon.User />} />
                     </div>
                 </>
             ) : (
-                <>
-                    {children}
-                </>
+                <>{children}</>
             )}
         </>
-    )
-}
+    );
+};
 
 const MobileTopBar = () => {
     const router = useRouter();
 
     return (
-        <div className={'flex justify-between p-[20px]'}>
+        <div className={'flex sticky top-0 bg-white justify-between p-[20px] border-b'}>
             <div className={'flex'}>
-                <Icon.Star/>
-                <p className={'text-[21px] ml-2'}>scordi</p>
+                <a href="/" className={'flex'}>
+                    {/*<Icon.Star />*/}
+                    <Image
+                        src="/logo-transparent.png"
+                        alt="Scordi logo"
+                        width={24}
+                        height={24}
+                        className="relative top-1 mr-1"
+                    />
+                    <p className={'text-[21px] ml-2 relative font-bold'} style={{top: 1}}>
+                        Scordi
+                    </p>
+                </a>
             </div>
             <div className={'flex space-x-4'}>
-                <Icon.Bell/>
-                <Icon.User onClick={() => router.push(UserSettingsPageRoute.pathname)}/>
+                <Icon.Bell />
+                <Icon.User onClick={() => router.push(UserSettingsPageRoute.pathname)} />
             </div>
         </div>
-    )
-}
+    );
+};
 
 type MobileNavItemProps = {
     icon: any;
     href: string;
-}
+};
 
 const MobileNavItem = (props: MobileNavItemProps) => {
     const router = useRouter();
 
     // TODO: 주소 따라서 아이콘 색상 바꿔주기
 
-    return (
-        <Link href={props.href}>
-            {props.icon}
-        </Link>
-    )
-}
+    return <Link href={props.href}>{props.icon}</Link>;
+};
 
 export default OrgMobileLayout;
