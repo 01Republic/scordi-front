d167b6e8244dcae69418071ed5ae86f806a65207
seongonecho <seongonecho@gmail.com>
2022-12-03 01:26:16 +0700

    메인페이지 pc 화면에서는 좌우로 분할

--- src/pages/orgs/[id]/home.tsx
+++ src/pages/orgs/[id]/home.tsx
@@ -10,13 +10,11 @@ import {BillingListMobile} from '^components/BillingListMobile';
 import {Icon} from '^components/Icon';
 import Calendar from 'react-calendar';
 import {getDashboardCalendar, getDashboardSummary} from '^api/dashboard.api';
-import {intlDateLong} from '^utils/dateTime';
 import {DashboardDaySumDto, DashboardSummaryDto} from '^types/dashboard.type';
 
 export const OrgHomeRoute: PageRoute = {
     pathname: '/orgs/[id]/home',
-    path: (orgId: number) =>
-        OrgHomeRoute.pathname.replace('[id]', String(orgId)),
+    path: (orgId: number) => OrgHomeRoute.pathname.replace('[id]', String(orgId)),
 };
 
 export default function HomePage() {
@@ -25,12 +23,8 @@ export default function HomePage() {
     const [year] = useState(new Date().getFullYear());
     const [month] = useState(new Date().getMonth() + 1);
     const [apps, setApps] = useState<ApplicationDto[]>([]);
-    const [summaryDto, setSummaryDto] = useState<DashboardSummaryDto | null>(
-        null,
-    );
-    const [calendarData, setCalendarData] = useState<
-        DashboardDaySumDto[] | null
-    >([]);
+    const [summaryDto, setSummaryDto] = useState<DashboardSummaryDto | null>(null);
+    const [calendarData, setCalendarData] = useState<DashboardDaySumDto[] | null>([]);
     // const [addService, setAddService] = useState<boolean>(false);
     // const [payment, setPayment] = useState<boolean>(false);
     // const [editService, setEditService] = useState<boolean>(false);
@@ -62,60 +56,46 @@ export default function HomePage() {
 
     return (
         <ContentLayout>
-            <div className="space-y-5">
-                <h2 className="text-24 font-semibold">
-                    5개의 서비스가 등록되었어요!
-                </h2>
+            <div className="space-y-5 lg:flex">
+                <div className={'flex-1 space-y-5 pr-5'}>
+                    <h2 className="text-24 font-semibold">5개의 서비스가 등록되었어요!</h2>
 
-                <div
-                    className={
-                        'flex justify-between p-[16px] bg-[#F9FAFB] rounded-[10px] items-center'
-                    }
-                    onClick={() =>
-                        router.push(`/orgs/${organizationId}/summary`)
-                    }
-                >
-                    <p>이번달 총 비용</p>
-                    <div className={'flex items-center'}>
-                        <p className={'font-semibold'}>
-                            USD {summaryDto.total.toLocaleString()}
-                        </p>
-                        <Icon.ChevronRight />
+                    <div
+                        className={'flex justify-between p-[16px] bg-[#F9FAFB] rounded-[10px] items-center'}
+                        onClick={() => router.push(`/orgs/${organizationId}/summary`)}
+                    >
+                        <p>이번달 총 비용</p>
+                        <div className={'flex items-center'}>
+                            <p className={'font-semibold'}>USD {summaryDto.total.toLocaleString()}</p>
+                            <Icon.ChevronRight />
+                        </div>
                     </div>
+
+                    <Calendar
+                        locale={'ko-KR'}
+                        calendarType={'US'}
+                        value={new Date()}
+                        formatDay={(locale, date) => date.getDate().toString()}
+                        tileContent={({date}) => {
+                            // const thisDay = intlDateLong(date);
+                            const payDay = calendarData?.find(
+                                (item) => new Date(item.date).getDate() === date.getDate(),
+                            );
+                            return !!payDay ? (
+                                <p className={'text-[6px]'}>${payDay.amount.toLocaleString()}</p>
+                            ) : (
+                                <p className={'text-transparent'}>oo</p>
+                            );
+                        }}
+                        next2Label={null}
+                        prev2Label={null}
+                        showNeighboringMonth={false}
+                    />
                 </div>
-
-                <Calendar
-                    locale={'ko-KR'}
-                    calendarType={'US'}
-                    value={new Date()}
-                    formatDay={(locale, date) => date.getDate().toString()}
-                    tileContent={({date}) => {
-                        // const thisDay = intlDateLong(date);
-                        const payDay = calendarData?.find(
-                            (item) =>
-                                new Date(item.date).getDate() ===
-                                date.getDate(),
-                        );
-                        return !!payDay ? (
-                            <p className={'text-[6px]'}>
-                                ${payDay.amount.toLocaleString()}
-                            </p>
-                        ) : (
-                            <p className={'text-transparent'}>oo</p>
-                        );
-                    }}
-                    next2Label={null}
-                    prev2Label={null}
-                    showNeighboringMonth={false}
-                />
-
-                {/* TODO: 여기에 앱을 넣을 게 아니라, 결제예측 모델을 개발하고 예측목록을 넣어야 할 듯. 호출도 월간으로 쿼리 할 수 있는 예측 컨트롤러가 필요. */}
-                <BillingListMobile
-                    summaryDto={summaryDto}
-                    apps={apps}
-                    year={year}
-                    month={month}
-                />
+                <div className={'flex-1 space-y-5'}>
+                    {/* TODO: 여기에 앱을 넣을 게 아니라, 결제예측 모델을 개발하고 예측목록을 넣어야 할 듯. 호출도 월간으로 쿼리 할 수 있는 예측 컨트롤러가 필요. */}
+                    <BillingListMobile summaryDto={summaryDto} apps={apps} year={year} month={month} />
+                </div>
             </div>
         </ContentLayout>
     );
