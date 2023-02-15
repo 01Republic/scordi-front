4781eafbb7c2940efb4d06c458c092bec95bb7f3
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-02-16 08:43:13 +0900

    로그인 페이지에서 리디렉션 처리 누락된 부분 추가 & 구글 로그인 버튼 스타일 약간 수정

--- src/components/pages/UsersLogin/index.tsx
+++ src/components/pages/UsersLogin/index.tsx
@@ -1,26 +1,19 @@
-import React, {memo, useEffect, useState} from 'react';
-import {useRouter} from 'next/router';
+import React, {memo, useState} from 'react';
 import {useForm} from 'react-hook-form';
 import {UserLoginRequestDto} from '^types/user.type';
 import {Modal} from '^components/Modal';
-import {OrgHomeRoute} from '^pages/orgs/[id]/home';
 import {useCurrentUser} from '^hooks/useCurrentUser';
 
 export const UsersLoginPage = memo(() => {
-    const router = useRouter();
-    const {currentUser, setCurrentUser, login} = useCurrentUser();
+    const {currentUser, login, loginRedirect} = useCurrentUser(null);
     const form = useForm<UserLoginRequestDto>();
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        if (currentUser?.orgId) {
-            router.push(OrgHomeRoute.path(currentUser.orgId));
-        }
-    }, [currentUser]);
+    if (currentUser) loginRedirect(currentUser);
 
     const submit = (data: UserLoginRequestDto) => {
         login(data)
-            .then((user) => router.push(OrgHomeRoute.path(user.orgId)))
+            .then((user) => loginRedirect(user))
             .catch(() => setIsModalOpen(true));
     };
 
@@ -34,11 +27,15 @@ export const UsersLoginPage = memo(() => {
                 buttons={[{text: 'Try again', onClick: () => setIsModalOpen(false)}]}
             />
             <div className={'mx-auto py-20 w-full max-w-md space-y-5'} style={{height: '100vh'}}>
-                <form onSubmit={form.handleSubmit(submit)} className={' p-4 m-auto'}>
+                <form onSubmit={form.handleSubmit(submit)} className={'p-4 m-auto'}>
                     <h1 className="text-7xl  mb-8 font-bold">Login</h1>
                     <h5 className="text-4xl  mb-32">You can join us with Google account ! </h5>
                     <div>
-                        <button className="w-full text-center py-3 my-3 border-2 flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-primary focus:bg-blue-50 active:bg-primary-100 transition duration-300 ">
+                        {/*<button className="w-full text-center py-3 my-3 border-2 flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-primary focus:bg-blue-50 active:bg-primary-100 transition duration-300 ">*/}
+                        {/*    <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-6 h-6" alt="" />*/}
+                        {/*    <span>Continue with Google</span>*/}
+                        {/*</button>*/}
+                        <button className="btn btn-block btn-lg btn-outline shadow font-medium normal-case mb-3 space-x-4 bg-white border-slate-200 text-slate-700 hover:bg-white hover:border-primary hover:text-slate-700 focus:bg-blue-50 active:bg-primary-100">
                             <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-6 h-6" alt="" />
                             <span>Continue with Google</span>
                         </button>
