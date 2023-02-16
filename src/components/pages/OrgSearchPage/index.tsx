f72dde567b1c06f0ba82270c2f1ccf0db52e60c8
Mia Seo <tjwlfif@gmail.com>
2023-02-16 10:30:54 +0900

    OrgSearchPage 한글 -> 영어 작업완료
    조직 create하는 부분에서 빈 따옴표가 들어가는 부분이 조금 아쉬워서 추가해봤는데 한번 봐주시면 좋을 것 같습니다!

--- src/components/pages/OrgSearchPage/index.tsx
+++ src/components/pages/OrgSearchPage/index.tsx
@@ -28,8 +28,8 @@ export const OrgSearchPage = memo(() => {
             <div className="flex flex-col w-[100%] sm:w-[60%] md:w-[50%] lg:w-[40%] gap-y-10">
                 <div className="hero-content text-center">
                     <div className="max-w-md">
-                        <h1 className="text-4xl md:text-7xl font-bold">조직 찾기</h1>
-                        <p className="text-lg py-6">조직을 찾아 합류하거나 새로 만들어보세요.</p>
+                        <h1 className="text-4xl md:text-7xl font-bold">Find Organization</h1>
+                        <p className="text-lg py-6">Find your group or Create your group</p>
                     </div>
                 </div>
                 <form
@@ -41,17 +41,19 @@ export const OrgSearchPage = memo(() => {
                 >
                     <input
                         type="text"
-                        placeholder="원하는 조직을 검색해방"
+                        placeholder="Please enter your organization to search."
                         className="input input-bordered w-full h-[8vh] text-lg"
                         {...form.register('keyword')}
                     />
                 </form>
                 {searchedOrgs.length === 0 ? (
-                    <p>검색 결과가 없습니다.</p>
+                    <p>There are no matching organization found in the page.</p>
                 ) : (
                     searchedOrgs.map((org, i) => <SearchedOrgResultItem org={org} key={i} />)
                 )}
-                <button className="btn btn-block btn-primary text-lg">"{inputValue}"을(를) 추가하기</button>
+                <button className="btn btn-block btn-primary text-lg">
+                    Create &nbsp;&nbsp;&nbsp;"{inputValue || `(enter your group)`}"
+                </button>
             </div>
         </div>
     );
