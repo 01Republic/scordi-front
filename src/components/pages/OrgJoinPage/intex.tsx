f7959256bc79ebf4951af3cdfaa316fb282726b2
Mia Seo <tjwlfif@gmail.com>
2023-02-16 15:38:13 +0900

    joinOrg 페이지 퍼블리싱 완료

--- src/components/pages/OrgJoinPage/intex.tsx
+++ src/components/pages/OrgJoinPage/intex.tsx
@@ -1,5 +1,18 @@
 import {memo} from 'react';
+import {FcOk} from 'react-icons/fc';
 
 export const JoinOrgPage = memo(() => {
-    return <>hello</>;
+    return (
+        <div className="flex w-full items-center justify-center" style={{height: '100vh'}}>
+            <div className="flex flex-col items-center w-[100%] sm:w-[60%] md:w-[50%] lg:w-[40%] gap-y-10">
+                <FcOk size="80px" />
+                <div className="hero-content text-center">
+                    <div className="flex flex-col gap-y-10 max-w-md">
+                        <h1 className="text-4xl md:text-7xl font-bold">Your request has been submitted!</h1>
+                        <p className="text-2xl py-6">Please wait for approval</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 });
