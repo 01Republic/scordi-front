a519ef00a68e97ff26d71a119c100b7b975f0283
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-02-16 10:59:57 +0900

    SignIn 페이지 삭제 완료 / Login 페이지 수정 완료
    1. 지연님 작업 내용을 pull 받아서 signIn 페이지가 다시 되돌아왔습니다.
    2. 지연님과 협의 후 SignIn 페이지 삭제 완료했습니다.
    3. 지연님이 작성한 코드 Login 페이지에 코드 수정 완료했습니다.

--- src/components/pages/UsersLogin/index.tsx
+++ src/components/pages/UsersLogin/index.tsx
@@ -5,9 +5,9 @@ import {Modal} from '^components/Modal';
 import {useCurrentUser} from '^hooks/useCurrentUser';
 
 export const UsersLoginPage = memo(() => {
-    const {currentUser, login, loginRedirect} = useCurrentUser(null);
     const form = useForm<UserLoginRequestDto>();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const {login, currentUser, loginRedirect} = useCurrentUser();
 
     if (currentUser) loginRedirect(currentUser);
 
@@ -23,7 +23,7 @@ export const UsersLoginPage = memo(() => {
                 type={'info'}
                 isOpen={isModalOpen}
                 title={'Login failed'}
-                description={'The email or password is not correct.'}
+                description={'Create a Google Account'}
                 buttons={[{text: 'Try again', onClick: () => setIsModalOpen(false)}]}
             />
             <div className={'mx-auto py-20 w-full max-w-md space-y-5'} style={{height: '100vh'}}>
@@ -31,10 +31,6 @@ export const UsersLoginPage = memo(() => {
                     <h1 className="text-7xl  mb-8 font-bold">Login</h1>
                     <h5 className="text-4xl  mb-32">You can join us with Google account ! </h5>
                     <div>
-                        {/*<button className="w-full text-center py-3 my-3 border-2 flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-primary focus:bg-blue-50 active:bg-primary-100 transition duration-300 ">*/}
-                        {/*    <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-6 h-6" alt="" />*/}
-                        {/*    <span>Continue with Google</span>*/}
-                        {/*</button>*/}
                         <button className="btn btn-block btn-lg btn-outline shadow font-medium normal-case mb-3 space-x-4 bg-white border-slate-200 text-slate-700 hover:bg-white hover:border-primary hover:text-slate-700 focus:bg-blue-50 active:bg-primary-100">
                             <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-6 h-6" alt="" />
                             <span>Continue with Google</span>
