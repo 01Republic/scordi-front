6924033213d6adbdb62c2de22f5a13fcb7738c0d
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-02-18 09:02:25 +0900

    기존 로그인 비개방 변경 후 버그수정

--- src/components/pages/UsersLogin/index.tsx
+++ src/components/pages/UsersLogin/index.tsx
@@ -8,7 +8,7 @@ import {GoogleLoginBtn} from './GoogleLoginBtn';
 
 export const UsersLoginPage = memo(() => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const {login, currentUser, loginRedirect} = useCurrentUser();
+    const {currentUser, loginRedirect} = useCurrentUser();
 
     if (currentUser) loginRedirect(currentUser);
 
