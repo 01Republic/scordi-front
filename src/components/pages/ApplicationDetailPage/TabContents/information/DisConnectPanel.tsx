2f03ccfd1eea3f29eb9d3044ea1d2ebec2920471
Jinkyung Lee <83896975+ijinkyung@users.noreply.github.com>
2023-03-02 14:55:44 +0900

    Disconnect 성공 여부 알림 기능 구현 완료

--- src/components/pages/ApplicationDetailPage/TabContents/information/DisConnectPanel.tsx
+++ src/components/pages/ApplicationDetailPage/TabContents/information/DisConnectPanel.tsx
@@ -1,39 +1,44 @@
+import {useRouter} from 'next/router';
 import {memo} from 'react';
 import {useRecoilState} from 'recoil';
 import Swal from 'sweetalert2';
 import {destroyApplication} from '^api/application.api';
 import {applicationIdParamState} from '^atoms/common';
 import {ContentPanel, ContentPanelMiniTitle} from '^layouts/ContentLayout';
-import {errorNotify} from '^utils/toast-notify';
-
-const onDisconnect = (id: number) => {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: 'If you disconnect, It will be difficult to manage the app.',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#0066FF',
-        confirmButtonText: 'Disconnect',
-    })
-        .then((result) => {
-            destroyApplication(id).then((res) => {
-                console.log('💖===>', res);
-                if (result.isConfirmed) {
-                    Swal.fire('Disconnect!', 'Please go to the site and cancel the subscription.', 'success');
-                } else if (result.isConfirmed === false) {
-                    //TODO : 해지불가할때 알림 설정하기
-                    Swal.fire('Disconnect!', 'Please go to the site and cancel the subscription.', 'success');
-                }
-            });
-            // .catch(errorNotify);
-        })
-        .catch(errorNotify);
-};
+import {OrgAppsIndexPageRoute} from '^pages/orgs/[id]/apps';
 
 export const DisConnectPanel = memo(() => {
     const [appId] = useRecoilState(applicationIdParamState);
+    const router = useRouter();
 
+    const onDisconnect = (id: number) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'If you disconnect, It will be difficult to manage the app.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#0066FF',
+            confirmButtonText: 'Disconnect',
+        }).then((result) => {
+            console.log(result);
+            destroyApplication(id)
+                .then((res) => {
+                    if (result.isConfirmed) {
+                        Swal.fire('Disconnect!', 'Please go to the site and cancel the subscription.', 'success').then(
+                            function () {
+                                router.push(OrgAppsIndexPageRoute.path(id));
+                            },
+                        );
+                    }
+                })
+                .catch((error) => {
+                    if (error.code === 'ERR_BAD_REQUEST') {
+                        Swal.fire('Permission Error', 'Please check you permission.', 'error');
+                    }
+                });
+        });
+    };
     const connectionDesc =
         `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi corrupti exercitationem alias doloribus ,  Placeat, dolores.`.trim();
 
