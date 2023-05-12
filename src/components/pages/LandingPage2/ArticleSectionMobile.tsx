e843b43e251faa830f61679ec88e74979eddf034
hyun joo oh <58460131+hyunjoo1130@users.noreply.github.com>
2023-05-12 17:18:15 +0900

    반응형 모바일 구현 완료

--- src/components/pages/LandingPage2/ArticleSectionMobile.tsx
+++ src/components/pages/LandingPage2/ArticleSectionMobile.tsx
@@ -1,6 +1,6 @@
 import React, {memo} from 'react';
 
-interface ArticleSectionProps {
+interface ArticleSectionMobileProps {
     iconUrl: string;
     title: string;
     subtitle: string;
@@ -9,20 +9,20 @@ interface ArticleSectionProps {
     left?: boolean;
 }
 
-export const ArticleSection = memo((props: ArticleSectionProps) => {
+export const ArticleSectionMobile = memo((props: ArticleSectionMobileProps) => {
     const {iconUrl, title, subtitle, desc, imgUrl, left = true} = props;
 
     return (
-        <section id="section-5" className="hidden sm:flex section-box pt-48 pb-24 xl:pt-28 xl:pb-16 lg:pt-32 lg:pb-12">
-            <div className={`section-5-box section-inner ${left ? '' : '!flex-row-reverse lg:!flex-col'}`}>
-                <div className="section-5-left section-desc">
-                    <div className="section-5-left-title">
+        <section id="article-section-mobile" className="flex sm:hidden section-box-mobile py-8">
+            <div className={`article-section-mobile-box section-inner`}>
+                <div className="article-section-mobile-left section-desc">
+                    <div className="article-section-mobile-left-title">
                         {/* Sub title */}
                         <div className="title-top" data-aos="fade-up" data-aos-anchor-placement="center-bottom">
-                            <div className="inline-block w-10 xl:w-8 mr-3">
+                            <div className="inline-block w-10 mr-3">
                                 <img src={iconUrl} alt="icon" className="w-full" />
                             </div>
-                            <p className="s-text-top mt-3 xl:mt-1">{subtitle}</p>
+                            <p className="s-text-top mt-1">{subtitle}</p>
                         </div>
                         {/* Title */}
                         <h2
@@ -34,41 +34,29 @@ export const ArticleSection = memo((props: ArticleSectionProps) => {
                     </div>
                     {/* Description */}
                     <div
-                        className="section-5-left-desc flex flex-col justify-start shrink-0"
+                        className="article-section-mobile-left-desc flex flex-col shrink-0"
                         data-aos="fade-up"
                         data-aos-anchor-placement="center-bottom"
                     >
                         <p className="s-text-sm" dangerouslySetInnerHTML={{__html: desc}} />
                     </div>
-
-                    <div
-                        className="section-5-left-btn lg:hidden"
-                        data-aos="fade-up"
-                        data-aos-anchor-placement="center-bottom"
-                    >
-                        <button className="btn closeBeta-btn">클로즈베타 신청하기</button>
-                    </div>
                 </div>
 
-                <div
-                    className={`section-5-right w-2/4 lg:w-[70%] lg:mt-12 flex ${
-                        left ? 'justify-end lg:justify-center' : 'lg:justify-center'
-                    }`}
-                >
+                <div className={`article-section-mobile-right w-[80%] flex mt-7`}>
                     <img
                         src={imgUrl}
                         alt="service preview image"
-                        className="w-10/12"
+                        className="w-full"
                         data-aos="fade-up"
                         data-aos-anchor-placement="center-bottom"
                     />
                 </div>
                 <div
-                    className="section-5-left-btn hidden lg:block"
+                    className="article-section-mobile-left-btn"
                     data-aos="fade-up"
                     data-aos-anchor-placement="center-bottom"
                 >
-                    <button className="btn closeBeta-btn">클로즈베타 신청하기</button>
+                    <button className="btn closeBeta-btn mt-5">클로즈베타 신청하기</button>
                 </div>
             </div>
         </section>
--- src/components/pages/LandingPage2/ImageBox.tsx
+++ src/components/pages/LandingPage2/ImageBox.tsx
@@ -11,18 +11,18 @@ export const ImageBox = memo((props: ImageBoxProps) => {
     const {imgUrl, leftTxt, centerNum, rightTxt} = props;
 
     return (
-        <div className="section-3-image-box lg:mb-10">
+        <div className="section-3-image-box mb-5">
             <div className="s3-image" data-aos="fade-up" data-aos-anchor-placement="center-bottom">
                 <img src={imgUrl} className="w-full" />
             </div>
             <div
-                className="s3-text text-center -mt-[0.5rem] xl:-mt-[0.2rem] lg:mt-[-1.5rem]"
+                className="s3-text text-center -mt-[1rem]"
                 data-aos="fade-up"
                 data-aos-anchor-placement="center-bottom"
             >
-                <span className="s-text-sm mr-3 xl:mr-2">{leftTxt}</span>
+                <span className="s-text-sm mr-2 sm:mr-3">{leftTxt}</span>
                 <span className="s-text-xl">{centerNum}%</span>
-                <span className="s-text-sm ml-3 xl:ml-2">{rightTxt}</span>
+                <span className="s-text-sm ml-2 sm:mr-3">{rightTxt}</span>
             </div>
         </div>
     );
