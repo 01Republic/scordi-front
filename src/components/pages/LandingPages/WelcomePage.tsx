35b4e435b7f969ec211a69a0ee47480f2a21c491
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-06-18 22:30:50 +0900

    웰컴p/ i18n 적용 완료

--- src/components/pages/LandingPages/WelcomePage.tsx
+++ src/components/pages/LandingPages/WelcomePage.tsx
@@ -4,11 +4,13 @@ import {CheckCircle} from '^components/react-icons/check-circle';
 import {useRouter} from 'next/router';
 import {V3OrgHomePageRoute} from '^pages/v3/orgs/[orgId]';
 import {useCurrentUser} from '^hooks/useCurrentUser';
+import {useTranslation} from 'next-i18next';
 
 export const WelcomePage = memo(() => {
     const router = useRouter();
     const {currentUser} = useCurrentUser();
     const [isAccessible, setIsAccessible] = useState(false);
+    const {t} = useTranslation('sign');
 
     useEffect(() => {
         const accessible = router.query.accessible as string | undefined;
@@ -21,32 +23,32 @@ export const WelcomePage = memo(() => {
             <div className="mx-auto text-center py-20 w-full max-w-lg space-y-5 min-h-[100vh]">
                 <CheckCircle className="w-[60px] mb-10" color="#5E5FEE" />
 
-                <h1 className="text-2xl sm:text-4xl mb-8 font-bold">무료 체험 신청이 완료되었어요!</h1>
+                <h1 className="text-2xl sm:text-4xl mb-8 font-bold">{t('welcome_page.title')}</h1>
 
                 <div className="p-4">
                     <p className="sm:text-lg mb-8">
-                        스코디 팀 매니저가 확인 후, 순차적으로 연락드릴 예정이에요. <br />
-                        이후 무료 체험을 열어드릴게요.
+                        {t('welcome_page.we_will_contact_you')} <br />
+                        {t('welcome_page.then_free_trial_would_be_opened')}
                     </p>
 
                     <div>
-                        <div className="mb-4 btn sm:btn-lg btn-block rounded-2xl text-lg sm:!text-xl shadow-lg btn-scordi-light-200 !text-scordi-500">
-                            📞 1영업일 내에 전화로 연락 드릴게요.
+                        <div className="mb-4 btn sm:btn-lg btn-block normal-case rounded-2xl text-lg sm:!text-xl shadow-lg btn-scordi-light-200 !text-scordi-500">
+                            {t('welcome_page.we_will_call_you_soon')}
                         </div>
 
                         {isAccessible ? (
                             <a
                                 href={currentUser ? `${V3OrgHomePageRoute.path(currentUser.orgId)}` : ''}
-                                className="mb-4 btn sm:btn-lg btn-block btn-ghost rounded-2xl hover:!bg-white text-lg sm:!text-xl text-scordi-light-500 hover:text-scordi-500"
+                                className="mb-4 btn sm:btn-lg btn-block normal-case btn-ghost rounded-2xl hover:!bg-white text-lg sm:!text-xl text-scordi-light-500 hover:text-scordi-500"
                             >
                                 사이트 내부로 이동 (관리자전용 노출)
                             </a>
                         ) : (
                             <a
                                 href="/"
-                                className="mb-4 btn sm:btn-lg btn-block btn-ghost rounded-2xl hover:!bg-white text-lg sm:!text-xl text-scordi-light-500 hover:text-scordi-500"
+                                className="mb-4 btn sm:btn-lg btn-block normal-case btn-ghost rounded-2xl hover:!bg-white text-lg sm:!text-xl text-scordi-light-500 hover:text-scordi-500"
                             >
-                                홈페이지로 이동
+                                {t('welcome_page.go_home')}
                             </a>
                         )}
                     </div>
