a3ce83c1dea675b0a157a734247df9582725933c
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-06-26 15:32:21 +0900

    어드민 사이드바에서 서비스로 돌아가기 버튼 추가

--- src/components/pages/admin/layouts/AdminPageLayout/AdminSideBar.tsx
+++ src/components/pages/admin/layouts/AdminPageLayout/AdminSideBar.tsx
@@ -4,6 +4,9 @@ import {useRouter} from 'next/router';
 import {AdminUsersPageRoute} from '^pages/admin/users';
 import {AdminPostsPageRoute} from '^pages/admin/posts';
 import {useCurrentUser} from '^hooks/useCurrentUser';
+import {V3OrgHomePageRoute} from '^pages/v3/orgs/[orgId]';
+import {PiSignOut} from 'react-icons/pi';
+import {AiOutlineHome} from '@react-icons/all-files/ai/AiOutlineHome';
 
 interface AdminSideBarProps extends WithChildren {}
 
@@ -25,13 +28,25 @@ export const AdminSideBar = memo((props: AdminSideBarProps) => {
                 </li>
             </ul>
 
-            <ul className="menu p-4 mt-auto">
+            <ul className="menu p-4 mt-auto gap-2">
                 {currentUser ? (
-                    <li>
-                        <a className="btn btn-block" onClick={logout}>
-                            로그아웃
-                        </a>
-                    </li>
+                    <>
+                        <li>
+                            <a
+                                className="btn btn-block btn-scordi-light"
+                                onClick={() => router.push(V3OrgHomePageRoute.path(currentUser.orgId))}
+                            >
+                                <AiOutlineHome />
+                                <span className="">서비스로</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a className="btn btn-block" onClick={logout}>
+                                <PiSignOut />
+                                <span className="">로그아웃</span>
+                            </a>
+                        </li>
+                    </>
                 ) : (
                     <li>
                         {/* CANNOT REACH HERE / Because if not logged in, force move out to login page. */}
