a179b0ae53a2fe165d5438c72e4a8ac9325c5ace
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-07-10 03:35:03 +0900

    데모p / 전월 로딩 버튼 텍스트 수정

--- src/components/pages/LandingPages/TastingPage/LoadMore/LoadMoreDraftButton.tsx
+++ src/components/pages/LandingPages/TastingPage/LoadMore/LoadMoreDraftButton.tsx
@@ -1,8 +1,11 @@
 import React, {memo, useState} from 'react';
-import {useDraft} from '^components/pages/LandingPages/TastingPage/hooks/useDraft';
+import {useDraft, useDraftResult} from '../hooks/useDraft';
+import {monthBefore} from '^utils/dateTime';
+import {dayjs} from '^utils/dayjs';
 
 export const LoadMoreDraftButton = memo(() => {
     const {fetchMonthBefore} = useDraft();
+    const {draftSearchFrom} = useDraftResult();
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const loadMore = () => {
@@ -20,9 +23,26 @@ export const LoadMoreDraftButton = memo(() => {
         );
     } else {
         return (
-            <button className="btn btn-link normal-case" onClick={() => loadMore()}>
-                Load a month more ...
+            <button className="btn btn-link normal-case w-full" onClick={() => loadMore()}>
+                {draftSearchFrom ? loadMoreText(draftSearchFrom) : 'Load a month more ...'}
             </button>
         );
     }
 });
+
+function loadMoreText(lastSearched: Date) {
+    const now = new Date();
+    const date = new Date(lastSearched);
+    date.setMonth(date.getMonth() - 1);
+    date.setDate(date.getDate() - 1);
+
+    const words = ['Load'];
+    if (now.getFullYear() === date.getFullYear()) {
+        words.push(dayjs(date).format('MMMM'));
+    } else {
+        words.push(dayjs(date).format('ll'));
+    }
+    words.push('...');
+
+    return words.join(' ');
+}
