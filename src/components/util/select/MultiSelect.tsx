307aa2f326c3c0c8c53489e6b10dcf10f83f313f
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-07-30 20:22:43 +0900

    MultiSelect Component 코드정리

--- src/components/util/select/MultiSelect.tsx
+++ src/components/util/select/MultiSelect.tsx
@@ -1,39 +1,6 @@
 import React from 'react';
 import AsyncCreatableSelect from 'react-select/async-creatable';
 import {MultiValue} from 'react-select';
-// import { ColourOption, colourOptions } from '../data';
-//
-const colourOptions = [
-    {value: 'chocolate', label: 'Chocolate'},
-    {value: 'strawberry', label: 'Strawberry'},
-    {value: 'vanilla', label: 'Vanilla'},
-];
-
-const fetchData = () => {
-    return new Promise((resolve) => {
-        resolve([
-            {value: 'chocolate', label: 'Chocolate'},
-            {value: 'strawberry', label: 'Strawberry'},
-            {value: 'vanilla', label: 'Vanilla'},
-        ]);
-    });
-};
-
-const filterColors = (inputValue: string) => {
-    return colourOptions.filter((i) => i.label.toLowerCase().includes(inputValue.toLowerCase()));
-};
-
-const promiseOptions = (inputValue: string) => {
-    return new Promise<any>((resolve) => {
-        setTimeout(() => {
-            resolve([
-                {value: 'chocolate', label: 'Chocolate'},
-                {value: 'strawberry', label: 'Strawberry'},
-                {value: 'vanilla', label: 'Vanilla'},
-            ]);
-        }, 1000);
-    });
-};
 
 interface Option {
     value: string;
