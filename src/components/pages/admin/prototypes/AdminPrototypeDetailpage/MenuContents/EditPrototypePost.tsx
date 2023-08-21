93892f8f85482fcb8c5ffe134cabcdc5a1e6f558
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-08-21 16:44:51 +0900

    Update EditPrototypePost.tsx

--- src/components/pages/admin/prototypes/AdminPrototypeDetailpage/MenuContents/EditPrototypePost.tsx
+++ src/components/pages/admin/prototypes/AdminPrototypeDetailpage/MenuContents/EditPrototypePost.tsx
@@ -18,13 +18,30 @@ export const EditPrototypePost = memo(() => {
         setPost(post);
     }, [prototype]);
 
+    /**
+     * 최초 생성시 설정
+     */
     useEffect(() => {
+        if (!prototype) return;
+
+        form.setValue('prototypeId', prototype.id);
+        form.setValue('title', prototype.name);
+        form.setValue('content', '');
+        form.setValue('seoTitle', prototype.name);
+        form.setValue('seoDescription', prototype.tagline);
+    }, [prototype]);
+
+    /**
+     * 이미 post 가 있다면
+     */
+    useEffect(() => {
+        if (!prototype) return;
         if (!post) return;
 
-        form.setValue('title', post.title);
+        form.setValue('title', post.title || prototype.name);
         form.setValue('content', post.content);
-        form.setValue('seoTitle', post.seoTitle);
-        form.setValue('seoDescription', post.seoDescription);
+        form.setValue('seoTitle', post.seoTitle || prototype.name);
+        form.setValue('seoDescription', post.seoDescription || prototype.tagline);
         if (post.publishAt) {
             form.setValue('publishAt', new Date(post.publishAt));
         }
@@ -35,7 +52,7 @@ export const EditPrototypePost = memo(() => {
 
     const createOrUpdate = post?.id
         ? (data: UpdateDto) => postManageApi.update(post.id, data)
-        : (data: CreateDto) => postManageApi.create(data);
+        : (data: CreateDto) => postManageApi.create(data).then((res) => setPost(res.data));
 
     const onSubmit = (data: UpdateDto | CreateDto) =>
         // @ts-ignore
