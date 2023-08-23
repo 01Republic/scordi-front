6ff3752eef70b401fc19a2f5773ae2994977ada4
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-08-23 17:25:54 +0900

    메인페이지 / 하단 섹션 모바일 폰트사이즈 수정

--- src/components/pages/LandingPages/HomePage2/Section4.tsx
+++ src/components/pages/LandingPages/HomePage2/Section4.tsx
@@ -6,8 +6,10 @@ export const Section4 = memo(() => {
         <div className="bg-center">
             <HomePageSection sectionClass="bg-gray-100">
                 <div className="text-center mb-[3rem]">
-                    <h2 className="text-5xl mb-[2rem]">새로운 SaaS 관리의 시작</h2>
-                    <p className="text-gray-500 text-[25px]">클릭 한 번으로 시작해보세요</p>
+                    <h2 className="text-[32px] sm:text-[4em] leading-[1.2em] mb-[1rem] sm:mb-[2rem]">
+                        새로운 SaaS 관리의 시작
+                    </h2>
+                    <p className="text-gray-500 text-[18px] sm:text-[25px]">클릭 한 번으로 시작해보세요</p>
                 </div>
 
                 {/* cta button */}
