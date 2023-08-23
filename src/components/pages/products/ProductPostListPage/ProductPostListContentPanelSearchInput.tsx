7ca219d22bd97fe5d55895c2c7cd64e9e9c03fbf
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-08-24 01:21:48 +0900

    SaaS 메뉴 / 목록페이지 검색 인풋 동작 오류 수정

--- src/components/pages/products/ProductPostListPage/ProductPostListContentPanelSearchInput.tsx
+++ src/components/pages/products/ProductPostListPage/ProductPostListContentPanelSearchInput.tsx
@@ -1,23 +1,21 @@
 import {memo} from 'react';
 import {BsSearch} from 'react-icons/bs';
 import {useProductPosts} from '^hooks/usePosts';
+import {useForm} from 'react-hook-form';
 
 export const ProductPostListContentPanelSearchInput = memo(() => {
     const {search} = useProductPosts();
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const input = e.target.value;
-        const query = input === '' ? {} : {keyword: input};
-        search(query);
-    };
+    const form = useForm<{keyword?: string}>();
+    const onSubmit = (query: {keyword?: string}) => search(query);
 
     return (
         <div className="relative flex-1 sm:flex-none">
-            <form>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
                 <input
                     type="text"
                     className="input input-bordered min-w-[300px] w-full pl-[48px]"
                     placeholder="Search ..."
-                    onChange={onChange}
+                    {...form.register('keyword')}
                 />
                 <div className="absolute top-0 bottom-0 m-auto w-[48px] h-[48px] flex items-center justify-center text-gray-400">
                     <BsSearch />
