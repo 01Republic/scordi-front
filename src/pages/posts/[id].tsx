88b0d0a648a5c05456336ac8bbd0f49651a6bd5d
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-08-25 19:03:41 +0900

    국제화 미적용된 푸터들 국제화 적용 (국제화 로딩 안되는 문제)
    국제화 미적용
    - SaaS 상세페이지 푸터
    - 블로그 상세페이지 푸터

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {pathRoute, pathReplace} from '^types/pageRoute.type';
-import {v3CommonRequires} from '^types/utils/18n.type';
+import {publicPageRequires} from '^types/utils/18n.type';
 import {BlogPostDetailPage} from '^components/pages/blog/BlogPostDetailPage';
 import {NextPageContext} from 'next';
 import {postApi} from '^api/post.api';
@@ -57,7 +57,7 @@ export const getServerSideProps = async function ({req, query, locale}: NextPage
             post,
             // Will be passed to the page component as props
             ...(await serverSideTranslations(locale!, [
-                ...v3CommonRequires, // 여기에 이 페이지에서 사용할 locale 파일을 추가하세요.
+                ...publicPageRequires, // 여기에 이 페이지에서 사용할 locale 파일을 추가하세요.
             ])),
         },
     };
--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -6,7 +6,7 @@ import {NextPageContext} from 'next';
 import {applicationPrototypeApi} from '^api/applicationPrototype.api';
 import {ApplicationPrototypeDto} from '^types/applicationPrototype.type';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
-import {v3CommonRequires} from '^types/utils/18n.type';
+import {publicPageRequires} from '^types/utils/18n.type';
 import {isPageLoadedAtom} from '^pages/posts/[id]';
 import {ProductDetailPage} from 'src/components/pages/products/ProductDetailPage';
 
@@ -39,7 +39,7 @@ export const getServerSideProps = async function ({req, query, locale}: NextPage
             prototype,
             // Will be passed to the page component as props
             ...(await serverSideTranslations(locale!, [
-                ...v3CommonRequires, // 여기에 이 페이지에서 사용할 locale 파일을 추가하세요.
+                ...publicPageRequires, // 여기에 이 페이지에서 사용할 locale 파일을 추가하세요.
             ])),
         },
     };
