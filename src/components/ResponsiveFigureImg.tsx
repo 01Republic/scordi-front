6746c3667a9a7d3b794f7d0bb02c0856eb048297
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-08-27 15:33:42 +0900

    SaaS 메뉴 목록페이지 아이템 카드 썸네일의 사이즈 object-fit : cover 적용

--- src/components/ResponsiveFigureImg.tsx
+++ src/components/ResponsiveFigureImg.tsx
@@ -19,7 +19,12 @@ export const ResponsiveFigureImg = memo((props: ResponsiveFigureImgProps) => {
             className={`relative w-full h-0 overflow-hidden ${className}`}
             style={{paddingBottom: responsiveHeight}}
         >
-            <img src={src} alt={alt} className={`absolute top-0 left-0 w-full h-full ${imgClassName}`} loading="lazy" />
+            <img
+                src={src}
+                alt={alt}
+                className={`absolute top-0 left-0 w-full h-full object-cover ${imgClassName}`}
+                loading="lazy"
+            />
         </figure>
     );
 });
