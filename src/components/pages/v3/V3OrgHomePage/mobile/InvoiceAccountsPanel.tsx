3c73da86ada1b5705c27f12610c84aa701b838e5
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-09-02 14:00:26 +0900

    메인페이지 모바일 구독 및 연동계정 패널 마크업 완료

--- src/components/pages/v3/V3OrgHomePage/mobile/InvoiceAccountsPanel.tsx
+++ src/components/pages/v3/V3OrgHomePage/mobile/InvoiceAccountsPanel.tsx
@@ -1,18 +1,23 @@
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import {MobileSection} from '^v3/share/sections/MobileSection';
 import {ContentEmpty} from '^v3/V3OrgHomePage/mobile/ContentEmpty';
 
 export const InvoiceAccountsPanel = memo(() => {
+    const [invoiceAccounts, setInvoiceAccounts] = useState([]);
+    const length = invoiceAccounts.length;
+
     return (
         <MobileSection.Item>
             <MobileSection.Padding>
                 <MobileSection.Heading title="결제 수신 계정">
                     <div className="text-sm text-gray-500">
-                        <div>계정 없음</div>
+                        <div>{length ? `${length}개 연동 중` : '계정 없음'}</div>
                     </div>
                 </MobileSection.Heading>
 
-                <ContentEmpty text="연결된 계정이 없어요" subtext="눌러서 계정 추가" onClick={console.log} />
+                {!length && (
+                    <ContentEmpty text="연결된 계정이 없어요" subtext="눌러서 계정 추가" onClick={console.log} />
+                )}
             </MobileSection.Padding>
         </MobileSection.Item>
     );
--- src/components/pages/v3/V3OrgHomePage/mobile/SubscriptionsPanel.tsx
+++ src/components/pages/v3/V3OrgHomePage/mobile/SubscriptionsPanel.tsx
@@ -1,18 +1,21 @@
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import {MobileSection} from '^v3/share/sections/MobileSection';
 import {ContentEmpty} from '^v3/V3OrgHomePage/mobile/ContentEmpty';
 
 export const SubscriptionsPanel = memo(() => {
+    const [subscriptions, setSubscriptions] = useState([]);
+    const length = subscriptions.length;
+
     return (
         <MobileSection.Item>
             <MobileSection.Padding>
-                <MobileSection.Heading title="이용중인 앱">
+                <MobileSection.Heading title={length ? `${length}개의 구독중인 앱` : '이용중인 앱'}>
                     <div className="text-sm text-gray-500">
-                        <div>앱 없음</div>
+                        <div>{length ? '앱 추가' : '앱 없음'}</div>
                     </div>
                 </MobileSection.Heading>
 
-                <ContentEmpty text="등록된 앱이 없어요" subtext="눌러서 앱 추가" onClick={console.log} />
+                {!length && <ContentEmpty text="등록된 앱이 없어요" subtext="눌러서 앱 추가" onClick={console.log} />}
             </MobileSection.Padding>
         </MobileSection.Item>
     );
