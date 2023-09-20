909b2f286862f16a44b6598a5a82f51e22ea91cd
newbieJanghan <bjh4135@gmail.com>
2023-09-20 12:45:11 +0900

    New: UseAccountSign: Encrypt, Decrypt function 추가

--- src/hooks/useAccounts.ts
+++ src/hooks/useAccounts.ts
@@ -3,6 +3,8 @@ import {useRecoilState} from 'recoil';
 import {getAccountsQueryAtom, accountsSearchResultAtom} from '^atoms/accounts.atom';
 import {FindAllAccountsQueryDto} from '^types/account.type';
 import {accountApi} from '^api/account.api';
+import {LoginDto} from '^types/crawler';
+import CryptoJS from 'crypto-js';
 
 export const useAccounts = () => {
     const orgId = useRouterIdParamState('orgId', orgIdParamState);
@@ -20,4 +22,20 @@ export const useAccounts = () => {
     const movePage = (page: number) => search({...query, page});
 
     return {query, result, search, movePage};
+};
+
+export const useAccountSign = () => {
+    const CRAWLER_SIGN_SECRET = process.env.NEXT_PUBLIC_CRAWLER_SIGN_SECRET as string;
+
+    const encryptLoginQuery = (loginQuery: LoginDto) => {
+        const data = JSON.stringify(loginQuery);
+        return CryptoJS.AES.encrypt(data, CRAWLER_SIGN_SECRET).toString();
+    };
+
+    const decryptSign = (sign: string) => {
+        const bytes = CryptoJS.AES.decrypt(sign, CRAWLER_SIGN_SECRET);
+        return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    };
+
+    return {encrypt: encryptLoginQuery, decrypt: decryptSign};
 };
