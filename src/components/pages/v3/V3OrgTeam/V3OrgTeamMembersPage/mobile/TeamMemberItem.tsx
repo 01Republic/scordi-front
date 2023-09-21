b120efe2e1e59ac5d4b4a16be6872f8645873f6f
newbieJanghan <bjh4135@gmail.com>
2023-09-21 23:25:22 +0900

    Fix: build error

--- src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/mobile/TeamMemberItem.tsx
+++ src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/mobile/TeamMemberItem.tsx
@@ -5,6 +5,7 @@ import {useRecoilValue} from 'recoil';
 import {useRouter} from 'next/router';
 import {orgIdParamState} from '^atoms/common';
 import {V3OrgTeamMemberShowPageRoute} from '^pages/v3/orgs/[orgId]/teams/members/[memberId]';
+import {makeTeamMemberProfile} from '^v3/V3OrgTeam/V3OrgTeamMembersPage/atom';
 
 interface TeamMemberItemProps {
     item: TeamMemberDto;
@@ -14,10 +15,7 @@ export const TeamMemberItem = memo((props: TeamMemberItemProps) => {
     const {item: teamMember} = props;
     const orgId = useRecoilValue(orgIdParamState);
     const router = useRouter();
-
-    const avatar = teamMember.profileImgUrl ?? teamMember.user.profileImgUrl;
-    const name = teamMember.name;
-    const job = teamMember.jobName ?? '미정';
+    const {name, jobName, profileImgUrl} = makeTeamMemberProfile(teamMember);
 
     const onClick = () => router.push(V3OrgTeamMemberShowPageRoute.path(orgId, teamMember.id));
 
@@ -26,11 +24,11 @@ export const TeamMemberItem = memo((props: TeamMemberItemProps) => {
             className="flex items-center gap-4 px-3 py-2.5 -mx-3 bg-base-100 text-gray-700 cursor-pointer hover:bg-neutral"
             onClick={onClick}
         >
-            <Avatar src={avatar} className="w-8 h-8 outline outline-offset-1 outline-slate-100" />
+            <Avatar src={profileImgUrl} className="w-8 h-8 outline outline-offset-1 outline-slate-100" />
             <div className="flex-1">
                 <p className="text-xl text-500">{name}</p>
                 <p className="text-[16px]">
-                    <small className="mr-0.5">{job}</small>
+                    <small className="mr-0.5">{jobName}</small>
                 </p>
             </div>
             <div></div>
