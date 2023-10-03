9f68e79665d57b7e71cb12ed6aeeead5c2e5b98b
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-10-03 17:41:03 +0900

    제보하기 링크 추가

--- src/components/pages/v3/share/sections/ApplyNotFoundProduct.tsx
+++ src/components/pages/v3/share/sections/ApplyNotFoundProduct.tsx
@@ -1,13 +1,18 @@
 import {memo} from 'react';
 import {FaAngleRight} from 'react-icons/fa6';
 import {BiSolidHelpCircle} from 'react-icons/bi';
+import {useRouter} from 'next/router';
 
 /**
  * 스코디에 등록되어있지 않은 앱 제보하기 버튼
  */
 export const ApplyNotFoundProduct = memo(() => {
+    const onClick = () => {
+        window.open('https://forms.gle/rT7hJwTMsnJsK5rg8', '_blank');
+    };
+
     return (
-        <div className="btn btn-block btn-lg bg-white border border-gray-300">
+        <div className="btn btn-block btn-like btn-lg no-selectable bg-white border border-gray-300" onClick={onClick}>
             <div className="w-full h-full flex items-center">
                 <BiSolidHelpCircle size={18} className="text-slate-500 mr-2.5" />
                 <div className="flex-1 text-left">
