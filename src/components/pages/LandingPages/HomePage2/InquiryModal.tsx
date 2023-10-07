3eedb7406c4ffcbc09de95ba0f74df88d2a25223
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-10-07 20:25:38 +0900

    도입문의 모달 / 폼 제출 안되는 문제 해결

--- src/components/pages/LandingPages/HomePage2/InquiryModal.tsx
+++ src/components/pages/LandingPages/HomePage2/InquiryModal.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import {useForm} from 'react-hook-form';
 import {sendSlackNotificationApi} from '^api/utils.api';
 import {useRouter} from 'next/router';
+import {toast} from 'react-toastify';
 
 type IntroductionInquiryModalFormData = {
     name: string;
@@ -28,7 +29,6 @@ export const InquiryModal = memo(() => {
     const form = useForm<IntroductionInquiryModalFormData>();
 
     const onSubmit = (data: IntroductionInquiryModalFormData) => {
-        console.log('on submit');
         sendSlackNotificationApi({
             title: '*도입 문의 알림!*',
             fields: [
@@ -40,88 +40,100 @@ export const InquiryModal = memo(() => {
                 {label: '이메일', value: data.email},
                 {label: '신청일시', value: new Date().toLocaleString()},
             ],
-        }).then(() => close());
+        })
+            .then(() => {
+                form.reset();
+                toast.success('접수되었습니다.\n곧 연락드릴게요!');
+            })
+            .finally(() => close());
     };
 
     return (
-        <Modal className="py-12">
-            <div className="flex justify-between items-center gap-3 mb-10">
+        <Modal className="py-6 w-full max-h-[100vh]">
+            <div className="flex justify-between items-center gap-3 mb-3">
                 <span className="font-bold text-lg flex-1">고객님의 정보가 필요해요!</span>
                 <CloseButton />
             </div>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+            <form>
                 <div className="form-control mb-3">
                     <label className="label">
-                        <span className="label-text">성함</span>
+                        <span className="label-text">성함 *</span>
                     </label>
                     <input
                         type="text"
                         placeholder="스코디"
                         className="input input-bordered"
                         {...form.register('name', {required: true})}
+                        required
                     />
                 </div>
                 <div className="form-control mb-3">
                     <label className="label">
-                        <span className="label-text">팀/직급</span>
+                        <span className="label-text">팀/직급 *</span>
                     </label>
                     <input
                         type="text"
                         placeholder="재무팀"
                         className="input input-bordered"
                         {...form.register('team', {required: true})}
+                        required
                     />
                 </div>
                 <div className="form-control mb-3">
                     <label className="label">
-                        <span className="label-text">회사명</span>
+                        <span className="label-text">회사명 *</span>
                     </label>
                     <input
                         type="text"
                         placeholder="제로원리퍼블릭"
                         className="input input-bordered"
                         {...form.register('company', {required: true})}
+                        required
                     />
                 </div>
                 <div className="form-control mb-3">
                     <label className="label">
-                        <span className="label-text">직원 수</span>
+                        <span className="label-text">직원 수 *</span>
                     </label>
                     <select
                         placeholder="1~10명"
                         className="select select-bordered"
                         {...form.register('companyMember', {required: true})}
+                        required
                     >
                         <option value="1~10명">1~10명</option>
                         <option value="11~30명">11~30명</option>
                         <option value="31~50명">31~50명</option>
                         <option value="51~100명">51~100명</option>
+                        <option value="51~100명">101명 이상</option>
                     </select>
                 </div>
                 <div className="form-control mb-3">
                     <label className="label">
-                        <span className="label-text">연락처</span>
+                        <span className="label-text">연락처 *</span>
                     </label>
                     <input
                         type="text"
                         placeholder="010-1234-5678"
                         className="input input-bordered"
                         {...form.register('phone', {required: true})}
+                        required
                     />
                 </div>
                 <div className="form-control mb-3">
                     <label className="label">
-                        <span className="label-text">이메일</span>
+                        <span className="label-text">이메일 *</span>
                     </label>
                     <input
                         type="email"
                         placeholder="hello@01republic.io"
                         className="input input-bordered"
                         {...form.register('email', {required: true})}
+                        required
                     />
                 </div>
                 <div className="form-control mt-6">
-                    <button type="submit" className="btn btn-scordi-500">
+                    <button type="button" className="btn btn-scordi-500" onClick={form.handleSubmit(onSubmit)}>
                         제출하기
                     </button>
                 </div>
