db251e4f95a579e79e646d207e17fc6c1e9e8e14
i-jinkyung <ijinkyung12@gmail.com>
2023-10-20 16:22:45 +0900

    Fix: billingHistories 페이지 / 모달 뜨지 않는 오류 해결

--- src/components/pages/v3/V3OrgBillingHistoriesPage/index.tsx
+++ src/components/pages/v3/V3OrgBillingHistoriesPage/index.tsx
@@ -3,12 +3,13 @@ import {V3MainLayoutMobile} from '^v3/layouts/V3MainLayout.mobile';
 import {BillingHistoriesPageBody} from './BillingHistoriesPageBody';
 import {useFocusedMonth} from '^v3/V3OrgHomePage/feature/useFocusedMonth';
 import {BottomTabIndex} from '^v3/share/BottomNavMobile';
+import {BillingHistoryDetailModal} from '^v3/share/modals/BillingHistoryDetailModal';
 
 export const V3OrgBillingHistoriesPage = memo(() => {
     useFocusedMonth({cached: false});
 
     return (
-        <V3MainLayoutMobile title="일정" activeTabIndex={BottomTabIndex.HISTORIES}>
+        <V3MainLayoutMobile title="일정" activeTabIndex={BottomTabIndex.HISTORIES} modals={[BillingHistoryDetailModal]}>
             <BillingHistoriesPageBody />
         </V3MainLayoutMobile>
     );
