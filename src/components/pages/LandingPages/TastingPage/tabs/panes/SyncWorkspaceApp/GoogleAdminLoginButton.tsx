fdd8957cb9f42f8a46d0fd79d7381045fde5e292
i-jinkyung <ijinkyung12@gmail.com>
2023-11-21 19:28:49 +0900

    Update: 데모페이지 / SaaS 필터링 기능 구현

--- src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/GoogleAdminLoginButton.tsx
+++ src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/GoogleAdminLoginButton.tsx
@@ -2,34 +2,68 @@ import {memo} from 'react';
 import {GoogleLoginBtn} from '^components/pages/UsersLogin/GoogleLoginBtn';
 import {GoogleOAuthProvider} from '@react-oauth/google';
 import {userSocialGoogleApi} from '^api/social-google.api';
-import {ReportList} from '^components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/ReportList';
-import {useRecoilState} from 'recoil';
+import {useSetRecoilState} from 'recoil';
 import {reportState} from '../../atom';
+import {ReportDto} from '^components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/dto/report.dto';
 
 export const GoogleAdminLoginButton = memo(function GoogleAdminLoginButton() {
     const googleOauthClientId = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID!;
-    const [reportData, setReportData] = useRecoilState(reportState);
+    const setReportData = useSetRecoilState(reportState);
     const {usageReport: googleUsageReportApi} = userSocialGoogleApi.subscriptions;
+
+    const lowBlackList = blackList.map((item) => {
+        return item.toLowerCase();
+    });
+
+    const filterBlackList = (reportList: ReportDto) => {
+        if (!reportList) return;
+
+        reportList.items.forEach((item) => {
+            const apps = item.apps;
+
+            item.apps = apps.filter((app) => {
+                const appName = app.appName.toLowerCase();
+                return !lowBlackList.includes(appName);
+            });
+        });
+
+        setReportData(reportList);
+
+        return reportList;
+    };
 
     const googleLoginSuccessHandler = async (accessToken: string) => {
         // TODO: 관리자 권한 아닐시 return
 
         return await googleUsageReportApi.draft(accessToken).then((res) => {
-            setReportData(res.data);
+            filterBlackList(res.data);
         });
     };
 
     return (
-        <>
-            <div
-                className="tooltip--TastingGoogleButton tooltip tooltip-open tooltip-primary"
-                data-tip="구글 워크스페이스 연동이 필요해요!"
-            >
-                <GoogleOAuthProvider clientId={googleOauthClientId}>
-                    <GoogleLoginBtn googleLoginOnSuccess={googleLoginSuccessHandler} />
-                </GoogleOAuthProvider>
-            </div>
-            {reportData && <ReportList reports={reportData} />}
-        </>
+        <div
+            className="tooltip--TastingGoogleButton tooltip tooltip-open tooltip-primary"
+            data-tip="구글 워크스페이스 연동이 필요해요!"
+        >
+            <GoogleOAuthProvider clientId={googleOauthClientId}>
+                <GoogleLoginBtn googleLoginOnSuccessFn={googleLoginSuccessHandler} />
+            </GoogleOAuthProvider>
+        </div>
     );
 });
+
+const blackList = [
+    'Scordi with admin api',
+    'Google Chrome',
+    'Scordi Dev',
+    'Scordi-google-admin-test',
+    'WOOWACON 2023',
+    'iOS',
+    'My Files',
+    'iOS Account Manager',
+    'Google Drive for desktop',
+    'macOS',
+    'Android device',
+    'SAMSUNG Account',
+    'Google APIs Explorer',
+];
--- src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/ReportList.tsx
+++ src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/ReportList.tsx
@@ -1,23 +1,20 @@
 import {memo} from 'react';
-import {ReportDto} from './dto/report.dto';
+import {useRecoilValue} from 'recoil';
+import {reportState} from '^components/pages/LandingPages/TastingPage/tabs/atom';
 
-interface ReportListProps {
-    reports: ReportDto | undefined;
-}
-
-export const ReportList = memo((props: ReportListProps) => {
-    const {reports} = props;
+export const ReportList = memo(() => {
+    const reportList = useRecoilValue(reportState);
 
     return (
         <section>
             <h1 className="font-semibold mb-5">제로원리퍼블릭에서 사용하는 서비스</h1>
-            {reports?.items.map((item, i) => (
+            {reportList?.items.map((report, i) => (
                 <div key={i} className="">
-                    <h3 className="font-semibold text-gray-500 mb-5">💡{item.email}님이 사용중인 서비스</h3>
+                    <h3 className="font-semibold text-gray-500 mb-5">💡{report.email}님이 사용중인 서비스</h3>
                     <div className="w-full grid grid-cols-5 gap-2 items-center justify-items-stretch mb-10">
-                        {item.apps.map((app) => (
+                        {report?.apps.map((app) => (
                             <div className="card bg-base-100 shadow-xl py-5 px-2 text-center font-semibold">
-                                {app.appName}
+                                <span></span> {app.appName}
                             </div>
                         ))}
                     </div>
