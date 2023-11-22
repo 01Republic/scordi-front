5f99df45dee095d49ccf4cd3f310333111d40d1f
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-11-22 17:44:44 +0900

    SaaS 콜렉션 / 등록된 SaaS 리스트들은 토스트메시지 띄우지말고 프라이싱 페이지로 이동

--- src/components/util/LinkTo.tsx
+++ src/components/util/LinkTo.tsx
@@ -1,4 +1,4 @@
-import {memo} from 'react';
+import {AnchorHTMLAttributes, HTMLAttributeAnchorTarget, memo} from 'react';
 import Link from 'next/link';
 import {LinkProps} from 'next/dist/client/link';
 import {ReactNodeLike} from 'prop-types';
@@ -8,23 +8,37 @@ export interface LinkToProps extends LinkProps {
     text?: ReactNodeLike;
     className?: string;
     onClick?: () => any;
+    target?: HTMLAttributeAnchorTarget;
 }
 
 export const LinkTo = memo((props: LinkToProps & WithChildren) => {
-    const {text = '', onClick, children, className = '', href, ...res} = props;
+    const {text = '', target, onClick, children, className = '', href, ...res} = props;
 
-    if (onClick) {
+    if (target === '_blank') {
+        const attrs: AnchorHTMLAttributes<any> = {};
+        if (href) attrs.href = `${href}`;
+        if (onClick) attrs.onClick = onClick;
         return (
-            <a className={className} onClick={onClick}>
+            <a className={className} {...attrs} target={target} rel="noopener noreferrer">
                 {children || text}
             </a>
         );
-    } else {
+    }
+
+    if (onClick) {
         return (
-            <Link href={href} {...res}>
-                <a className={className}>{children || text}</a>
-            </Link>
+            <a className={className} onClick={onClick} target={target}>
+                {children || text}
+            </a>
         );
     }
+
+    return (
+        <Link href={href} {...res}>
+            <a className={className} target={target}>
+                {children || text}
+            </a>
+        </Link>
+    );
 });
 LinkTo.displayName = 'LinkTo';
