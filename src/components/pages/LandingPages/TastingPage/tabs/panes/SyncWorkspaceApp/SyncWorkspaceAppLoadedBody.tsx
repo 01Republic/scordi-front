6411d3d761b6649642a3f466d01b37c185224680
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-11-23 18:05:55 +0900

    Update SyncWorkspaceAppLoadedBody.tsx

--- src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/SyncWorkspaceAppLoadedBody.tsx
+++ src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/SyncWorkspaceAppLoadedBody.tsx
@@ -19,8 +19,12 @@ export const SyncWorkspaceAppLoadedBody = memo(function LoadedResult() {
         <section className="pt-8">
             <h1 className="font-semibold mb-[3rem]">
                 <span className="text-scordi">{reportByProduct.workspaceName}</span>에서는{' '}
-                <span className="text-scordi">{reportByProduct.items.length}개</span>의 서비스를{' '}
-                <span className="text-scordi">{reportList?.items.length}명</span>이 쓰고 있어요
+                <span className="block sm:inline-block">
+                    <span className="text-scordi">{reportByProduct.items.length}개</span>의 서비스를
+                </span>{' '}
+                <span className="block sm:inline-block">
+                    <span className="text-scordi">{reportList?.items.length}명</span>이 쓰고 있어요
+                </span>
             </h1>
 
             {/*<div className="w-full"></div>*/}
