a191f6d021d205160895e058db4660db03b6e7ec
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-11-26 02:24:47 +0900

    데모페이지 / 구독조회 - 카드리스트에서 바로 서비스 제거 기능 추가

--- src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/results/ProductItemList/index.tsx
+++ src/components/pages/LandingPages/TastingPage/tabs/panes/SyncWorkspaceApp/results/ProductItemList/index.tsx
@@ -1,13 +1,10 @@
 import React, {memo, useEffect} from 'react';
 import {useSetRecoilState} from 'recoil';
 import {plainToInstance} from 'class-transformer';
-import {Avatar} from '^components/Avatar';
-import {useModal} from '^v3/share/modals/useModal';
 import {ReportGroupedByProductItemDto} from '../../dto/view-types/group-by-product/report.grouped-by-product-item.dto';
-import {reportItemModalIsShow, subjectReportProductItem} from '../../atom';
-import {isAddingModeState} from '../ReportItemModal/atom';
+import {subjectReportProductItem} from '../../atom';
 import {AddNewAppService} from './AddNewAppService';
-import {FaQuestion} from 'react-icons/fa6';
+import {ProductItem} from './ProductItem';
 
 interface ProductItemListProps {
     items: ReportGroupedByProductItemDto[];
@@ -24,8 +21,6 @@ function sort(items: ReportGroupedByProductItemDto[]) {
 
 export const ProductItemList = memo((props: ProductItemListProps) => {
     const {items} = props;
-    const {open: openModal} = useModal({isShowAtom: reportItemModalIsShow});
-    const setIsNewMemberAddingMode = useSetRecoilState(isAddingModeState);
     const setSubjectItem = useSetRecoilState(subjectReportProductItem);
 
     useEffect(() => {
@@ -41,30 +36,10 @@ export const ProductItemList = memo((props: ProductItemListProps) => {
         });
     }, [items]);
 
-    const onClickItem = (item: ReportGroupedByProductItemDto) => {
-        setSubjectItem(item);
-        setIsNewMemberAddingMode(false);
-        openModal();
-    };
-
     return (
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {sort(items).map((item, i) => (
-                <div
-                    key={i}
-                    onClick={() => onClickItem(item)}
-                    className="card p-4 bg-base-100 shadow-md hover:shadow-lg flex flex-row gap-2 items-start cursor-pointer btn-animation"
-                >
-                    <div>
-                        <Avatar className="w-8" src={item.product?.image}>
-                            <FaQuestion size={24} className="text-gray-300 h-full w-full p-[6px]" />
-                        </Avatar>
-                    </div>
-                    <div className="flex-1 h-full flex flex-col items-end gap-2">
-                        <p className="text-15 font-semibold text-right leading-none min-h-[30px]">{item.appName}</p>
-                        <p className="mt-auto text-sm text-gray-500">{item.memberList.length}명</p>
-                    </div>
-                </div>
+                <ProductItem item={item} key={i} />
             ))}
 
             <AddNewAppService />
