48b5ff4acff9bcc32053f83ecbac8caf57c0c4f4
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-11-26 16:53:49 +0900

    구독리스트 / 목록페이지 개발 시작

--- src/components/pages/v3/V3OrgAppsPage/index.tsx
+++ src/components/pages/v3/V3OrgAppsPage/index.tsx
@@ -1,20 +1,14 @@
 import React, {memo} from 'react';
-import {WithChildren} from '^types/global.type';
-import {V3MainLayout, V3MainLayoutContainer} from '^v3/layouts/V3MainLayout';
 import {LNBIndex} from '^v3/share/LeftNavBar';
+import {V3ListPageLayout} from '^v3/layouts/V3ListPageLayout';
 
-interface V3OrgAppsPageProps extends WithChildren {}
-
-export const V3OrgAppsPage = memo((props: V3OrgAppsPageProps) => {
-    const {children} = props;
-
+export const V3OrgAppsPage = memo(() => {
     return (
-        <V3MainLayout activeTabIndex={LNBIndex.Subscriptions}>
-            <V3MainLayoutContainer>
-                <section className="mb-6">
-                    <h1>구독리스트</h1>
-                </section>
-            </V3MainLayoutContainer>
-        </V3MainLayout>
+        <V3ListPageLayout activeTabIndex={LNBIndex.Subscriptions}>
+            <section className="mb-6">
+                <h1>구독리스트</h1>
+            </section>
+        </V3ListPageLayout>
     );
 });
+V3OrgAppsPage.displayName = 'V3OrgAppsPage';
