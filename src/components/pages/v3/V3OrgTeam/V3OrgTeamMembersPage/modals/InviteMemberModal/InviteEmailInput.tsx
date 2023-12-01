d3696859bb881d86ad4da38707d129475656b9aa
i-jinkyung <ijinkyung12@gmail.com>
2023-12-01 21:56:57 +0900

    Fix: useInviteMember hook 생성

--- src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/modals/InviteMemberModal/InviteEmailInput.tsx
+++ src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/modals/InviteMemberModal/InviteEmailInput.tsx
@@ -1,62 +1,23 @@
-import {useToast} from '^hooks/useToast';
-import React, {memo, KeyboardEvent, useEffect} from 'react';
+import React, {memo, useEffect} from 'react';
 import {FieldValues, UseFormReturn} from 'react-hook-form';
 import {IoClose} from 'react-icons/io5';
-import {useRecoilState} from 'recoil';
+import {useRecoilValue} from 'recoil';
 import {invitedEmailsAtom} from '^models/TeamMember/atom';
+import {useInviteMember} from '^v3/V3OrgTeam/V3OrgTeamMembersPage/modals/InviteMemberModal/hook';
 
 interface InviteEmailInputProps {
     form: UseFormReturn<FieldValues, any>;
-    confirmOrgMember: () => boolean | undefined;
 }
 
 export const InviteEmailInput = memo((props: InviteEmailInputProps) => {
-    const {form, confirmOrgMember} = props;
-    const {toast} = useToast();
-    const [invitedEmails, setInvitedEmails] = useRecoilState(invitedEmailsAtom);
+    const invitedEmails = useRecoilValue(invitedEmailsAtom);
+    const {removeInvitedEmail, addInvitedEmail} = useInviteMember();
+    const {form} = props;
 
     useEffect(() => {
         const emailInput = document.querySelector('input[name=email]') as HTMLElement;
         emailInput.focus();
     });
-
-    // 초대 이메일 배열에 추가하는 함수
-    const addInvitedEmail = (e: KeyboardEvent<HTMLInputElement>) => {
-        const invitedEmail = e.target.value.trim();
-
-        if (invitedEmail.length === 0) {
-            e.stopPropagation();
-            e.preventDefault();
-            return;
-        }
-
-        const isEmail = invitedEmail.includes('.') && invitedEmail.includes('@');
-        const isOrgMember = confirmOrgMember();
-
-        if (!isEmail) {
-            toast.error('이메일 형식을 확인해주세요');
-            e.stopPropagation();
-            e.preventDefault();
-            return;
-        }
-
-        if (!isOrgMember) {
-            e.stopPropagation();
-            e.preventDefault();
-            return;
-        }
-
-        setInvitedEmails([...invitedEmails, invitedEmail]);
-        form.resetField('email');
-    };
-
-    const removeInvitedEmail = (value: string) => {
-        const remainEmails = invitedEmails.filter((email) => {
-            return email !== value;
-        });
-
-        setInvitedEmails(remainEmails);
-    };
 
     return (
         <div className="border w-full min-h-44 rounded-lg py-1 px-3">
@@ -77,7 +38,7 @@ export const InviteEmailInput = memo((props: InviteEmailInputProps) => {
                     {...form.register('email')}
                     type="email"
                     placeholder="이메일을 입력하세요."
-                    onKeyUp={(e) => e.key === 'Enter' && addInvitedEmail(e)}
+                    onKeyUp={(e) => e.key === 'Enter' && addInvitedEmail(e, form)}
                     className="input w-full p-2 focus:outline-none"
                 />
             </div>
