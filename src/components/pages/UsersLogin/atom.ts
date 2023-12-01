767466d3e3804a229d5e20f27dabf258efa287be
newbieJanghan <bjh4135@gmail.com>
2023-12-01 12:32:54 +0900

    New: 데모페이지 / Report가 Local Storage에 저장됩니다.

--- src/components/pages/UsersLogin/atom.ts
+++ src/components/pages/UsersLogin/atom.ts
@@ -1,27 +1,5 @@
 import {atom} from 'recoil';
-
-/**
- * 페이지 이동 시 atom 이 초기화되지 않도록 유지하는 역할을 수행합니다.
- * TODO 필요에 따라 token 이 초기화될 수 있도록 수정 필요.
- */
-const localStorageEffect = (key: string) => {
-    return ({setSelf, onSet}: any) => {
-        if (typeof window == 'undefined') return;
-
-        const storage = window.localStorage;
-        const savedValue = storage.getItem(key);
-
-        // setSelf : 연결된 atom의 값을 초기화 해주는 함수
-        if (savedValue) {
-            setSelf(JSON.parse(savedValue));
-        }
-
-        // onSet : 해당하는 atom의 값이 변경이 되었을 때 실행되는 함수
-        onSet((newValue: string, isReset: boolean) => {
-            isReset ? storage.removeItem(key) : storage.setItem(key, JSON.stringify(newValue));
-        });
-    };
-};
+import {localStorageEffect} from '^atoms/localStorageEffect';
 
 export const googleAccessTokenAtom = atom<string | null>({
     key: 'googleAccessTokenAtom',
