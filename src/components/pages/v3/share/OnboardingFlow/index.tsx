855b8419cb8f7a934f62d43aed4a207c0092e484
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-12-04 01:33:37 +0900

    온보딩 / 온보딩 조건부 실행 기능 추가

--- src/components/pages/v3/share/OnboardingFlow/index.tsx
+++ src/components/pages/v3/share/OnboardingFlow/index.tsx
@@ -2,25 +2,22 @@ import {memo, useEffect} from 'react';
 import {onboardingModalIsShow} from './atom';
 import {StepNavigator} from './StepNavigator';
 import {StepContent} from './StepContent';
-import {useRecoilState} from 'recoil';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {currentOrgAtom} from '^models/Organization/atom';
 
 export const OnboardingFlow = memo(function OnboardingFlow() {
     const [isShow, setIsShow] = useRecoilState(onboardingModalIsShow);
+    const currentOrg = useRecoilValue(currentOrgAtom);
 
     useEffect(() => {
-        setIsShow(true);
-        if (typeof window !== 'undefined') {
-            window.addEventListener('keydown', (e) => {
-                console.log(e);
-            });
+        if (currentOrg && !currentOrg.lastGoogleSyncHistoryId) {
+            setIsShow(true);
         }
-    }, []);
+    }, [currentOrg]);
 
     return (
         <div className={`modal ${isShow ? 'modal-open' : ''}`}>
             <div className="modal-box h-full min-w-full max-h-full rounded-none p-0">
-                {/*<h3 className="font-bold text-lg">Congratulations random Internet user!</h3>*/}
-
                 <div className="h-full flex flex-col">
                     <StepNavigator />
                     <StepContent />
