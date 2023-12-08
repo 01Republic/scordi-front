97f4bb43394b01410b11a0140ca16620f5c6850e
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-12-08 17:47:34 +0900

    랜덤?으로 컬러 선택 해주는 함수를 getColor 로 추출

--- src/components/pages/v3/share/TeamMemberAvatar.tsx
+++ src/components/pages/v3/share/TeamMemberAvatar.tsx
@@ -1,35 +1,22 @@
 import React, {memo} from 'react';
 import {TeamMemberDto} from '^models/TeamMember';
 import {Avatar} from '^components/Avatar';
+import {getColor} from '^components/util/palette';
 
 interface TeamMemberAvatarProps {
     teamMember: TeamMemberDto;
     className?: string;
 }
 
-const avatarColorPalette = [
-    'bg-green-400',
-    'bg-red-400',
-    'bg-orange-400',
-    'bg-sky-400',
-    'bg-emerald-400',
-    'bg-cyan-400',
-    'bg-pink-400',
-];
-
 export const TeamMemberAvatar = memo((props: TeamMemberAvatarProps) => {
     const {teamMember, className = 'w-10 h-10'} = props;
 
     const {profileImgUrl, name, email} = teamMember.makeTeamMemberProfile();
-    const avatarColor = avatarColorPalette[(email.length + teamMember.id) % avatarColorPalette.length];
+    const avatarColor = getColor(email.length + teamMember.id);
 
     return (
         <Avatar src={profileImgUrl} className={`${className} ${profileImgUrl ? '' : avatarColor}`}>
-            <div
-                data-modulo={(name || email).length % avatarColorPalette.length}
-                title={name || email}
-                className="w-full h-full flex items-center justify-center text-white"
-            >
+            <div title={name || email} className="w-full h-full flex items-center justify-center text-white">
                 {`${name || email}`.toUpperCase()[0]}
             </div>
         </Avatar>
