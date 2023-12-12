ff8c2540c8270c4d7580ec3d573f4f3cbe0f88d0
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-12-12 17:01:34 +0900

    조직에 접근했을 때, 로그인을 체크해서 로그인 풀려있으면 튕겨내는 로직 추가

--- src/models/Organization/hook/index.ts
+++ src/models/Organization/hook/index.ts
@@ -2,21 +2,47 @@ import {useEffect} from 'react';
 import {useRecoilState, useRecoilValue} from 'recoil';
 import {currentOrgAtom, getOrgQuery} from '^models/Organization/atom';
 import {organizationApi} from '^models/Organization/api';
+import {AxiosError} from 'axios';
+import {useAlert} from '^hooks/useAlert';
+import {useRouter} from 'next/router';
+import {UserLoginPageRoute} from '^pages/users/login';
+import {getToken} from '^api/api';
 
 export const useOrganization = () => useRecoilValue(getOrgQuery);
 
 // side effect 발생으로 페이지 컴포넌트에서만 사용하기
 export function useCurrentOrg(id: number) {
+    const router = useRouter();
     const [currentOrg, setCurrentOrg] = useRecoilState(currentOrgAtom);
+    const {alert} = useAlert();
 
     useEffect(() => {
         if (!id || isNaN(id)) return;
         if (currentOrg && currentOrg.id === id) return;
+
+        const loginToken = getToken();
+        if (!loginToken) {
+            alert.error('로그인이 필요합니다!', '로그인 페이지로 이동할까요?').then((result) => {
+                if (result.isConfirmed) {
+                    router.push(UserLoginPageRoute.path());
+                } else {
+                    router.back();
+                }
+            });
+            return;
+        }
+
         organizationApi
             .show(id, {
                 relations: ['lastGoogleSyncHistory', 'lastGoogleSyncHistory.googleTokenData'],
             })
-            .then((res) => setCurrentOrg(res.data));
+            .then((res) => setCurrentOrg(res.data))
+            .catch((e: AxiosError) => {
+                console.log(e.response?.status == 401);
+                if (e.response?.status == 401) {
+                    alert.error('조직을 찾을 수 없습니다', '올바른 접근인지 확인해주세요');
+                }
+            });
     }, [id, currentOrg]);
 
     return {currentOrg, setCurrentOrg};
