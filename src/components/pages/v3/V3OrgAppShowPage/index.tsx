555d219526f1dd24ca049a36877513e2c47cc2c6
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2023-12-14 02:40:36 +0900

    모바일 구독 상세p - 좌상단 백버튼 동작 안하는 문제 수정

--- src/components/pages/v3/V3OrgAppShowPage/index.tsx
+++ src/components/pages/v3/V3OrgAppShowPage/index.tsx
@@ -5,24 +5,16 @@ import {AccountListModal} from '^v3/share/modals/AccountListModal';
 import {useCurrentSubscription} from '^v3/V3OrgAppShowPage/atom';
 import {BillingHistoryDetailModal} from '^v3/share/modals/BillingHistoryDetailModal';
 import {RegisterCreditCardModal} from '^v3/share/modals/ConnectCreditCardModal';
-import {V3OrgAppsPageRoute} from '^pages/v3/orgs/[orgId]/apps';
-import {useSafePathInCurrentOrg} from '^hooks/useSafePath';
 import {AppShowPageBody} from './modals/AppShowPageModal/AppShowPageBody';
 import {SelectTeamMemberModal} from './modals/AppShowPageModal/SelectTeamMemberModal';
 
 export const V3OrgAppShowPage = memo(() => {
     const {currentSubscription} = useCurrentSubscription();
-    const {safePath} = useSafePathInCurrentOrg();
-
-    const onClick = () => {
-        safePath((org) => V3OrgAppsPageRoute.path(org.id));
-    };
 
     return (
         <V3ModalLikeLayoutMobile
             title={currentSubscription ? currentSubscription.product.name() : ''}
             modals={[BillingHistoryDetailModal, SelectTeamMemberModal, AccountListModal, RegisterCreditCardModal]}
-            backBtnOnClick={onClick}
         >
             <MobileSection.List className="h-full">
                 <AppShowPageBody />
