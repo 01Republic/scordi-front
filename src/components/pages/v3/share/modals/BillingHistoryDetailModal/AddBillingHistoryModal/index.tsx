81f6f9ac5ca914f94c169b5293afe8dc60bbcb6a
i-jinkyung <ijinkyung12@gmail.com>
2023-12-27 23:27:38 +0900

    Fix: 결제내역추가모달 / 모달 상태 변경시 form, recoil 초기화

--- src/components/pages/v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/index.tsx
+++ src/components/pages/v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/index.tsx
@@ -8,7 +8,7 @@ import {ModalTopbar, useModal} from '^v3/share/modals';
 import {useBillingHistoryInModal} from '^v3/share/modals/BillingHistoryDetailModal/hook';
 import {MobileSection} from '^v3/share/sections/MobileSection';
 import {PayMethodBody} from '^v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/bodys/PayMethodBody';
-import {useRecoilState} from 'recoil';
+import {useRecoilState, useSetRecoilState} from 'recoil';
 import {AmountBody} from '^v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/bodys/AmountBody';
 import {DetailInfoBody} from '^v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/bodys/DetailInfoBody';
 import {useForm} from 'react-hook-form';
@@ -16,31 +16,42 @@ import {CreateBillingHistoryRequestDto} from '^models/BillingHistory/type';
 import {CurrencySelectModal} from '^v3/share/modals/BillingHistoryDetailModal/CurrencySelectModal';
 import {FinishBody} from '^v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/bodys/FinishBody';
 import {MemoBody} from '^v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/bodys/MemoBody';
+import {isDomesticState, memoState} from '^v3/share/modals/BillingHistoryDetailModal/AddBillingHistoryModal/bodys/atom';
 
 export const AddBillingHistoryModal = memo(() => {
     const {Modal, close, isShow} = useModal(addBillingHistoryShowModal);
     const {billingHistory} = useBillingHistoryInModal();
     const [addBillingHistory, setAddBillingHistory] = useRecoilState(AddBillingHistoryState);
+    const setIsDomestic = useSetRecoilState(isDomesticState);
+    const setMemo = useSetRecoilState(memoState);
     const form = useForm<CreateBillingHistoryRequestDto>();
 
     useEffect(() => {
+        // form, recoil 초기화
         setAddBillingHistory(AddBillingHistory.PayMethod);
         form.reset();
+        setIsDomestic(true);
+        setMemo('메모 남기기');
     }, [isShow]);
 
     // 모달 뒤로가기 버튼
     const onBack = () => {
-        if (addBillingHistory === AddBillingHistory.PayMethod) {
-            close();
-            return;
-        }
-        if (addBillingHistory === AddBillingHistory.Amount) {
-            setAddBillingHistory(AddBillingHistory.PayMethod);
-            return;
-        }
-        if (addBillingHistory === AddBillingHistory.DetailInfo) {
-            setAddBillingHistory(AddBillingHistory.Amount);
-            return;
+        switch (addBillingHistory) {
+            case AddBillingHistory.PayMethod:
+                close();
+                return;
+            case AddBillingHistory.Amount:
+                setAddBillingHistory(AddBillingHistory.PayMethod);
+                return;
+            case AddBillingHistory.DetailInfo:
+                setAddBillingHistory(AddBillingHistory.Amount);
+                return;
+            case AddBillingHistory.Finish:
+                setAddBillingHistory(AddBillingHistory.DetailInfo);
+                return;
+            case AddBillingHistory.Memo:
+                setAddBillingHistory(AddBillingHistory.Finish);
+                return;
         }
     };
 
@@ -56,10 +67,10 @@ export const AddBillingHistoryModal = memo(() => {
                 />
 
                 <MobileSection.Padding>
-                    <h2 className="h1 leading-tight mb-10">
-                        {isShowTitle && ' 새로운 결제 내역을 \n' + ' 등록합니다'}
+                    <h2 className="h1 leading-tight mb-10 whitespace-pre-line">
+                        {isShowTitle && '새로운 결제 내역을\n등록합니다'}
                         {addBillingHistory === AddBillingHistory.Finish && ''}
-                        {addBillingHistory === AddBillingHistory.Memo && '어떤 내용의 \n' + ' 결제 내역인가요?'}
+                        {addBillingHistory === AddBillingHistory.Memo && '어떤 내용의\n결제 내역인가요?'}
                     </h2>
 
                     <section className="flex flex-col gap-5">
