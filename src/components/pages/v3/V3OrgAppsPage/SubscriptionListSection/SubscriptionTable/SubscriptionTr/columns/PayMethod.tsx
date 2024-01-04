805cf061fdb7e49189a9958fbfa0e423ec0d857f
i-jinkyung <ijinkyung12@gmail.com>
2024-01-04 23:23:14 +0900

    Fix: 구독리스트 / 결제수단 카드 별칭 없을때 에러 해결

--- src/components/pages/v3/V3OrgAppsPage/SubscriptionListSection/SubscriptionTable/SubscriptionTr/columns/PayMethod.tsx
+++ src/components/pages/v3/V3OrgAppsPage/SubscriptionListSection/SubscriptionTable/SubscriptionTr/columns/PayMethod.tsx
@@ -4,22 +4,32 @@ import {SubscriptionDto} from '^models/Subscription/types';
 import {useCreditCards} from '^models/CreditCard/hook';
 
 interface PayMethod {
-    lastPaidHistory: BillingHistoryDto | undefined;
     subscription: SubscriptionDto;
+    lastPaidHistory: BillingHistoryDto | undefined;
 }
 export const PayMethod = memo((props: PayMethod) => {
     const {result} = useCreditCards();
-    const {subscription} = props;
+    const {subscription, lastPaidHistory} = props;
 
     const creditCard = result.items.find((item) => item.id === subscription.creditCardId);
+    const billingHistoryPayMethod = lastPaidHistory?.getPaymentMethod();
+
+    // 엑섹 입력 후 해당 변수로 변경
+    const payMethodText = creditCard?.name || billingHistoryPayMethod || '비어있음';
+
+    // 엑셀 입력 후 삭제
+    const payMethodText2: string =
+        creditCard && creditCard.name
+            ? creditCard.name
+            : creditCard
+            ? '별칭이 없어요'
+            : billingHistoryPayMethod
+            ? billingHistoryPayMethod
+            : '비어있음';
 
     return (
         <div>
-            <p className={`${!creditCard && 'text-gray-300'} text-xs`}>
-                {creditCard?.name && creditCard?.name}
-                {creditCard && !creditCard.name && '별칭이 없어요'}
-                {!creditCard && '비어있음'}
-            </p>
+            <p className={`${!creditCard && !billingHistoryPayMethod && 'text-gray-300'} text-xs`}>{payMethodText2}</p>
         </div>
     );
 });
