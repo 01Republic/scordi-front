d84e878bbc77e95a9d0cc33fef5010842f06210e
i-jinkyung <ijinkyung12@gmail.com>
2024-01-04 15:52:41 +0900

    Update: 카드 셀렉터 구현

---
+++ src/components/pages/v3/share/Select/CardSelector/selectOpions.tsx
@@ -0,0 +1,69 @@
+import {components, ContainerProps, ControlProps, MenuListProps, OptionProps, SingleValueProps} from 'react-select';
+import React from 'react';
+import {useModal} from '^v3/share/modals';
+import {newCardModalState} from '^v3/share/modals/NewCardModal/NewCardModalV2/atom';
+
+export const selectStylesOptions = {
+    dropdownIndicator: () => ({display: 'none'}),
+    NoOptionsMessage: () => ({display: 'none'}),
+};
+
+export const CardComponents = () => {
+    const SelectContainer = (props: ContainerProps<any>) => {
+        return <components.SelectContainer {...props}>{props.children}</components.SelectContainer>;
+    };
+
+    const Control = (props: ControlProps<any>) => {
+        return (
+            <components.Control {...props} className="!select !select-bordered !w-full focus:outline-none">
+                {props.children}
+            </components.Control>
+        );
+    };
+
+    const MenuList = (props: MenuListProps<any>) => {
+        const {open} = useModal(newCardModalState);
+
+        return (
+            <components.MenuList {...props}>
+                <div
+                    onClick={open}
+                    className="text-sm text-center text-gray-500 py-2 cursor-pointer transition-all hover:bg-scordi-100 hover:text-scordi"
+                >
+                    새로운 카드 등록하기
+                </div>
+                {props.children}
+            </components.MenuList>
+        );
+    };
+
+    const Option = (props: OptionProps<any>) => {
+        const card = props.data;
+        return (
+            <components.Option {...props}>
+                <div>
+                    <p className="text-sm text-gray-500"> {card.name}</p>
+                    <p>{card.label}</p>
+                </div>
+            </components.Option>
+        );
+    };
+
+    const SingleValue = (props: SingleValueProps<any>) => {
+        const card = props.data;
+
+        return (
+            <components.SingleValue {...props}>
+                <div>
+                    <p className="text-sm text-gray-500"> {card.name}</p>
+                    <p>{card.label}</p>
+                </div>
+            </components.SingleValue>
+        );
+    };
+    const NoOptionsMessage = (props: any) => {
+        return <components.NoOptionsMessage {...props} className="hidden"></components.NoOptionsMessage>;
+    };
+
+    return {SelectContainer, Control, MenuList, Option, SingleValue, NoOptionsMessage};
+};
---
+++ src/components/pages/v3/share/modals/ConnectCreditCardModal/CTAButton/index.tsx
@@ -0,0 +1,32 @@
+import {memo} from 'react';
+import {ModalButton} from '^v3/share/ModalButton';
+import {subscriptionApi} from '^models/Subscription/api';
+import {updateCurrentSubscriptionState, useCurrentSubscription} from '^v3/V3OrgAppShowPage/atom';
+import {useRecoilValue} from 'recoil';
+import {orgIdParamState} from '^atoms/common';
+import {useToast} from '^hooks/useToast';
+import {useModal} from '^v3/share/modals';
+import {connectCreditCardModal} from '^v3/share/modals/ConnectCreditCardModal/atom';
+
+export const CTAButton = memo(() => {
+    const {currentSubscription, loadCurrentSubscription} = useCurrentSubscription();
+    const formData = useRecoilValue(updateCurrentSubscriptionState);
+    const orgId = useRecoilValue(orgIdParamState);
+    const {close} = useModal(connectCreditCardModal);
+    const {toast} = useToast();
+
+    const onClick = () => {
+        if (!orgId || !currentSubscription) return;
+
+        const subscriptionId = currentSubscription.id;
+
+        const req = subscriptionApi.update(subscriptionId, formData);
+
+        req.then(() => {
+            toast.success('등록되었습니다.');
+            close();
+            loadCurrentSubscription(orgId, subscriptionId);
+        });
+    };
+    return <ModalButton onClick={onClick} />;
+});
