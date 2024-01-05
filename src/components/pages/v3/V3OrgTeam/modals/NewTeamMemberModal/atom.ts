1e2c639baf695c843533f439cb0054f68504a2cc
i-jinkyung <ijinkyung12@gmail.com>
2024-01-05 19:05:00 +0900

    Fix: 멤버직접추가모달 / 멤버중복생성되는 에러 해결

--- src/components/pages/v3/V3OrgTeam/modals/NewTeamMemberModal/atom.ts
+++ src/components/pages/v3/V3OrgTeam/modals/NewTeamMemberModal/atom.ts
@@ -1,6 +1,12 @@
 import {atom} from 'recoil';
+import {CreateTeamMemberDto} from '^models/TeamMember';
 
 export const isOpenNewTeamMemberModalAtom = atom({
     key: 'isOpenNewTeamMemberModalAtom',
     default: false,
+});
+
+export const createNewTeamMemberAtom = atom<CreateTeamMemberDto>({
+    key: 'createNewTeamMemberAtom',
+    default: {} as CreateTeamMemberDto,
 });
