3797d69d1ddd6c27f54bb413958fca9ea2f86c09
i-jinkyung <ijinkyung12@gmail.com>
2024-01-08 18:37:00 +0900

    Update: 멤버리스트 / 정렬 기능 구현

---
+++ src/components/pages/v3/share/table/columns/share/SortableTH.tsx
@@ -0,0 +1,36 @@
+import {WithChildren} from '^types/global.type';
+import {memo, useState} from 'react';
+import {TiArrowSortedDown, TiArrowSortedUp} from 'react-icons/ti';
+
+interface SortableTHProps extends WithChildren {
+    className?: string;
+    onClick?: (sortKey: string, value: 'ASC' | 'DESC') => any;
+    sortKey?: string;
+    sortVal?: 'ASC' | 'DESC';
+}
+
+export const SortableTH = memo((props: SortableTHProps) => {
+    const {className = '', sortKey, sortVal = 'ASC', onClick, children} = props;
+    const [value, setValue] = useState<boolean>();
+
+    const isSortable = !!(sortKey && onClick);
+
+    const sort = () => {
+        if (!isSortable) return;
+
+        onClick(sortKey, !value ? 'ASC' : 'DESC');
+        setValue((v) => !v);
+    };
+
+    return (
+        <th onClick={sort} className={`cursor-pointer bg-transparent ${className}`}>
+            {isSortable ? (
+                <div className={`flex items-center ${className}`}>
+                    {children} {typeof value === 'boolean' && value ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
+                </div>
+            ) : (
+                children
+            )}
+        </th>
+    );
+});
