a19d95de72c4c53fddc767b06274fd61c0dc85c0
i-jinkyung <ijinkyung12@gmail.com>
2024-01-09 17:02:14 +0900

    FIx: 멤버리스트 / 멤버초대 기능 구현

--- src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/TeamMemberTableSection/TaemMemberTable/TeamMemberTableRow/TeamMemberStatus/InviteButton.tsx
+++ src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/TeamMemberTableSection/TaemMemberTable/TeamMemberTableRow/TeamMemberStatus/InviteButton.tsx
@@ -1,25 +1,56 @@
-import React, {memo} from 'react';
-import {TeamMemberDto, useSendInviteEmail} from '^models/TeamMember';
+import React, {memo, useState} from 'react';
+import {teamMemberApi, TeamMemberDto, useSendInviteEmail, useTeamMembers} from '^models/TeamMember';
 import {FaRegEnvelope} from 'react-icons/fa';
+import {CgSpinner} from 'react-icons/cg';
+import {useRecoilValue} from 'recoil';
+import {orgIdParamState} from '^atoms/common';
 
 interface InviteButtonProps {
     teamMember: TeamMemberDto;
 }
 
 export const InviteButton = memo((props: InviteButtonProps) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const {reload} = useTeamMembers();
     const {sendEmail} = useSendInviteEmail();
     const {teamMember} = props;
 
-    const onClick = () => {
-        if (!teamMember.email) return;
+    const onClick = async () => {
+        if (!teamMember.email || isLoading) return;
+        setIsLoading(true);
 
-        sendEmail(teamMember.email);
+        try {
+            await sendEmail(teamMember.email, teamMember.id);
+            reload();
+        } catch (e) {
+            console.error(e);
+        } finally {
+            setIsLoading(false);
+        }
     };
 
+    return <>{isLoading ? <LoadingBtn /> : <InviteBtn onClick={() => onClick()} />}</>;
+});
+
+InviteButton.displayName = 'InviteButton';
+
+interface InviteBtn {
+    onClick: () => void;
+}
+const InviteBtn = (props: InviteBtn) => {
+    const {onClick} = props;
     return (
         <button onClick={onClick} className="btn btn-sm normal-case gap-2 items-center">
             <FaRegEnvelope /> Invite
         </button>
     );
-});
-InviteButton.displayName = 'InviteButton';
+};
+
+const LoadingBtn = () => {
+    return (
+        <button className="btn-disabled opacity-40 btn btn-sm btn-scordi btn-outline normal-case gap-2">
+            <CgSpinner className="animate-spin" />
+            Inviting
+        </button>
+    );
+};
