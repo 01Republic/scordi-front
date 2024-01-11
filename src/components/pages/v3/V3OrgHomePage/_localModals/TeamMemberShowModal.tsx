3bac7913210ab8d8c53dc6ec7ec5110ba45c6150
i-jinkyung <ijinkyung12@gmail.com>
2024-01-11 22:32:18 +0900

    Fix: 멤버삭제 / 멤버삭제시 membership도 삭제

--- src/components/pages/v3/V3OrgHomePage/_localModals/TeamMemberShowModal.tsx
+++ src/components/pages/v3/V3OrgHomePage/_localModals/TeamMemberShowModal.tsx
@@ -1,6 +1,7 @@
 import {memo} from 'react';
 import {useTeamMembers_Dashboard} from '^models/TeamMember';
 import {TeamMemberShowModal} from '^v3/V3OrgTeam/modals/TeamMemberShowModal';
+import {useMemberships} from '^models/Membership/hook';
 
 /**
  * [조직홈p] 멤버 상세 모달
@@ -8,6 +9,14 @@ import {TeamMemberShowModal} from '^v3/V3OrgTeam/modals/TeamMemberShowModal';
 
 export const TeamMemberDetailModal = memo(() => {
     const {reload} = useTeamMembers_Dashboard();
+    const {reload: loadMembership} = useMemberships();
 
-    return <TeamMemberShowModal onClose={() => reload()} />;
+    return (
+        <TeamMemberShowModal
+            onClose={() => {
+                reload();
+                loadMembership();
+            }}
+        />
+    );
 });
--- src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/_localModals/TeamMemberDetailModal.tsx
+++ src/components/pages/v3/V3OrgTeam/V3OrgTeamMembersPage/_localModals/TeamMemberDetailModal.tsx
@@ -1,14 +1,19 @@
 import {memo} from 'react';
 import {useTeamMembers} from '^models/TeamMember';
 import {TeamMemberShowModal} from '^v3/V3OrgTeam/modals/TeamMemberShowModal';
+import {useMemberships} from '^models/Membership/hook';
 
 export const TeamMemberDetailModal = memo(() => {
     const list = useTeamMembers();
+    const {reload: loadMembership} = useMemberships();
 
     return (
         <TeamMemberShowModal
             onClose={() => {
-                if (!list.isExist) list.reload();
+                if (!list.isExist) {
+                    list.reload();
+                    loadMembership();
+                }
             }}
         />
     );
