8353fbff1b850ea1881da7d45ffcbd4fd438b4f1
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-01-13 13:00:04 +0900

    구독리스트 / 구독 테이블 정렬기능 - 사용인원 & 최신결제금액 정렬 방법 수정

--- src/components/pages/v3/V3OrgAppsPage/SubscriptionListSection/SubscriptionTable/SubscriptionTr/columns/LatestPayAmount.tsx
+++ src/components/pages/v3/V3OrgAppsPage/SubscriptionListSection/SubscriptionTable/SubscriptionTr/columns/LatestPayAmount.tsx
@@ -1,24 +1,26 @@
-import {memo, useEffect, useState} from 'react';
-import {BillingHistoryDto} from '^models/BillingHistory/type';
+import {memo} from 'react';
 import {useRecoilValue} from 'recoil';
 import {displayCurrencyAtom} from '^components/pages/LandingPages/TastingPage/pageAtoms';
 import {getCurrencySymbol} from '^api/tasting.api/gmail/agent/parse-email-price';
 import {currencyFormat} from '^utils/number';
+import {SubscriptionDto} from '^models/Subscription/types';
 
 interface LatestPayAmountProps {
-    latestBillingHistory?: BillingHistoryDto;
+    subscription: SubscriptionDto;
 }
 
 export const LatestPayAmount = memo((props: LatestPayAmountProps) => {
     const displayCurrency = useRecoilValue(displayCurrencyAtom);
     const symbol = getCurrencySymbol(displayCurrency);
-    const {latestBillingHistory} = props;
-    const payAmount = latestBillingHistory?.getPriceIn(displayCurrency) || 0;
+    const {subscription} = props;
+
+    // const payAmount = latestBillingHistory?.getPriceIn(displayCurrency) || 0;
+    const billingAmount = subscription.currentBillingAmount?.toDisplayPrice(displayCurrency) || 0;
 
     return (
-        <p className="text-sm">
+        <p className="text-sm" {...subscription.currentBillingAmount}>
             <small className="mr-1">{symbol}</small>
-            <span>{currencyFormat(payAmount, '')}</span>
+            <span>{currencyFormat(billingAmount, '')}</span>
         </p>
     );
 });
