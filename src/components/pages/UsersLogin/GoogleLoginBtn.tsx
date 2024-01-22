4d94e0e92657b54d98ffbce293ed480c9fabe829
i-jinkyung <ijinkyung12@gmail.com>
2024-01-22 18:11:06 +0900

    Fix: google login button / button component props 추가

--- src/components/pages/UsersLogin/GoogleLoginBtn.tsx
+++ src/components/pages/UsersLogin/GoogleLoginBtn.tsx
@@ -31,10 +31,11 @@ interface GoogleLoginBtnProps {
     about?: keyof typeof SCOPE_MAP;
     className?: string;
     logoSize?: string;
+    ButtonComponent?: () => JSX.Element;
 }
 
 export const GoogleLoginBtn = memo((props: GoogleLoginBtnProps) => {
-    const {onCode, googleLoginOnSuccessFn, about, className, logoSize} = props;
+    const {onCode, googleLoginOnSuccessFn, about, className, logoSize, ButtonComponent} = props;
     const googleLoginOnSuccess = googleLoginOnSuccessFn ? googleLoginOnSuccessFn : useGoogleLoginSuccessHandler2();
     const setAccessToken = useSetRecoilState(googleAccessTokenAtom);
     const scope = about ? SCOPE_MAP[about] : SCOPE_ALL;
@@ -63,16 +64,24 @@ export const GoogleLoginBtn = memo((props: GoogleLoginBtnProps) => {
     });
 
     return (
-        <button
-            onClick={() => loginButtonOnClick()}
-            className={`${className} btn btn-lg btn-outline shadow font-medium normal-case space-x-4 bg-white border-slate-200 text-slate-700 hover:bg-white hover:border-primary hover:text-slate-700 focus:bg-blue-50 active:bg-primary-100`}
-        >
-            <img
-                src="https://www.svgrepo.com/show/355037/google.svg"
-                className={`${logoSize ? logoSize : 'w-6 h-6'}`}
-                alt=""
-            />
-            <span>Continue with Google</span>
-        </button>
+        <>
+            {ButtonComponent ? (
+                <div onClick={() => loginButtonOnClick()}>
+                    <ButtonComponent />
+                </div>
+            ) : (
+                <button
+                    onClick={() => loginButtonOnClick()}
+                    className={`${className} btn btn-lg btn-outline shadow font-medium normal-case space-x-4 bg-white border-slate-200 text-slate-700 hover:bg-white hover:border-primary hover:text-slate-700 focus:bg-blue-50 active:bg-primary-100`}
+                >
+                    <img
+                        src="https://www.svgrepo.com/show/355037/google.svg"
+                        className={`${logoSize ? logoSize : 'w-6 h-6'}`}
+                        alt=""
+                    />
+                    <span>Continue with Google</span>
+                </button>
+            )}
+        </>
     );
 });
