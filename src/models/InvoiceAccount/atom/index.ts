160974ef7824ff405c5d97d565c23190309bc0f0
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-01-23 17:11:35 +0900

    구독상세모달 / 인보이스계정 속성 컴포넌트 수정

--- src/models/InvoiceAccount/atom/index.ts
+++ src/models/InvoiceAccount/atom/index.ts
@@ -4,3 +4,13 @@ import {pagedResourceAtom} from '^hooks/usePagedResource';
 export const invoiceAccountListAtom = pagedResourceAtom<InvoiceAccountDto, FindAllInvoiceAccountQueryDto>({
     key: 'invoiceAccountListAtom',
 });
+
+// 구독상세모달에서, 해당 구독에 연결된 인보이스 계정 리스트를 보여줄 때 사용
+export const invoiceAccountsOfSubscription = pagedResourceAtom<InvoiceAccountDto, FindAllInvoiceAccountQueryDto>({
+    key: 'invoiceAccountsOfSubscription',
+});
+
+// 인보이스계정 선택 모달에서, 선택할 수 있는 인보이스 계정 리스트를 보여줄 때 사용
+export const invoiceAccountsInSelectModal = pagedResourceAtom<InvoiceAccountDto, FindAllInvoiceAccountQueryDto>({
+    key: 'invoiceAccountsInSelectModal',
+});
--- src/models/InvoiceAccount/hook/index.ts
+++ src/models/InvoiceAccount/hook/index.ts
@@ -1,12 +1,18 @@
 import {FindAllInvoiceAccountQueryDto, InvoiceAccountDto} from '^models/InvoiceAccount/type';
 
-import {invoiceAccountApi} from '^models/InvoiceAccount/api';
-import {invoiceAccountListAtom} from '^models/InvoiceAccount/atom';
 import {PagedResourceAtoms, usePagedResource} from '^hooks/usePagedResource';
+import {invoiceAccountApi} from '../api';
+import {invoiceAccountListAtom, invoiceAccountsInSelectModal, invoiceAccountsOfSubscription} from '../atom';
 
 export const useInvoiceAccounts = () => useInvoiceAccountsV3(invoiceAccountListAtom);
 
-export const useInvoiceAccountsV3 = (
+// 구독상세모달에서, 해당 구독에 연결된 인보이스 계정 리스트를 보여줄 때 사용
+export const useInvoiceAccountsOfSubscription = () => useInvoiceAccountsV3(invoiceAccountsOfSubscription);
+
+// 인보이스계정 선택 모달에서, 선택할 수 있는 인보이스 계정 리스트를 보여줄 때 사용
+export const useInvoiceAccountListInSelectModal = () => useInvoiceAccountsV3(invoiceAccountsInSelectModal);
+
+const useInvoiceAccountsV3 = (
     atoms: PagedResourceAtoms<InvoiceAccountDto, FindAllInvoiceAccountQueryDto>,
     mergeMode = false,
 ) => {
