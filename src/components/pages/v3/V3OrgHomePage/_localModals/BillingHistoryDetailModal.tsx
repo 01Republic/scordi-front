5c3a428197837725133d6ab2486baa1234dfe1d2
i-jinkyung <ijinkyung12@gmail.com>
2024-01-30 17:04:25 +0900

    Fix: 결제내역 변경 후 구독 상세 info 업데이트 에러 해결

--- src/components/pages/v3/V3OrgHomePage/_localModals/BillingHistoryDetailModal.tsx
+++ src/components/pages/v3/V3OrgHomePage/_localModals/BillingHistoryDetailModal.tsx
@@ -1,9 +1,25 @@
 import {memo} from 'react';
-import {BillingHistoryDetailModal} from '^v3/share/modals/BillingHistoryDetailModal';
+import {useRecoilValue} from 'recoil';
+import {orgIdParamState} from '^atoms/common';
 import {useDashboardSubscriptions} from '^models/Subscription/hook';
+import {useBillingHistoryListOfSubscription} from '^models/BillingHistory/hook';
+import {BillingHistoryDetailModal} from '^v3/share/modals/BillingHistoryDetailModal';
+import {appIdState, useCurrentSubscription} from '^v3/V3OrgAppShowPage/atom';
 
 export const BillingHistoryDetailModalInDashBoard = memo(() => {
-    const {reload} = useDashboardSubscriptions();
+    const {reload: reloadDashboardSubscriptions} = useDashboardSubscriptions();
+    const {reload: reloadBillingHistoryList} = useBillingHistoryListOfSubscription();
+    const {loadCurrentSubscription} = useCurrentSubscription();
+    const orgId = useRecoilValue(orgIdParamState);
+    const appId = useRecoilValue(appIdState);
 
-    return <BillingHistoryDetailModal onFinish={reload} />;
+    const onFinish = () => {
+        if (!appId) return;
+
+        reloadBillingHistoryList(); // 결제 내역 reload
+        reloadDashboardSubscriptions(); // 구독 테이블 reload
+        loadCurrentSubscription(orgId, appId); // 구독 상세 info reload
+    };
+
+    return <BillingHistoryDetailModal onFinish={onFinish} />;
 });
--- src/components/pages/v3/share/modals/BillingHistoryDetailModal/DeleteButton.tsx
+++ src/components/pages/v3/share/modals/BillingHistoryDetailModal/DeleteButton.tsx
@@ -3,7 +3,6 @@ import {useRecoilState} from 'recoil';
 import {isBillingHistoryEditModeAtom} from '^v3/share/modals/BillingHistoryDetailModal/atom';
 import {DeleteButton} from '^v3/share/modals/AppShowPageModal/DeleteButton';
 import {useBillingHistoryInModal, useBillingHistoryModal} from '^v3/share/modals/BillingHistoryDetailModal/hook';
-import {useBillingHistoryListOfSubscription} from '^models/BillingHistory/hook';
 
 interface DeleteButtonProps {
     onFinish?: () => any;
@@ -11,14 +10,12 @@ interface DeleteButtonProps {
 export const BillingHistoryDeleteButton = memo(function BillingHistoryDeleteButton(props: DeleteButtonProps) {
     const [isEditMode, setIsEditMode] = useRecoilState(isBillingHistoryEditModeAtom);
     const {setIsShow} = useBillingHistoryModal();
-    const {reload: loadHistories} = useBillingHistoryListOfSubscription();
     const {deleteBillingHistory} = useBillingHistoryInModal();
 
     const {onFinish} = props;
 
     const onClick = () =>
         deleteBillingHistory().then(() => {
-            loadHistories();
             setIsEditMode(false);
             setIsShow(false);
             onFinish && onFinish();
