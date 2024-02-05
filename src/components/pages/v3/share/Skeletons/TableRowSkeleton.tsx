d53c513e1c32745b84c23f7bebb10f189783818f
i-jinkyung <ijinkyung12@gmail.com>
2024-02-05 11:50:59 +0900

    Fix: 구독테이블 skeleton 스타일 수정

--- src/components/pages/v3/share/Skeletons/TableRowSkeleton.tsx
+++ src/components/pages/v3/share/Skeletons/TableRowSkeleton.tsx
@@ -5,37 +5,37 @@ export const TableRowSkeleton = () => {
         <>
             {TableRow.map(() => (
                 <tr>
-                    <td className="flex gap-2 items-center">
-                        <div className="skeleton !w-10 !h-10 rounded-full" />
-                        <div className="skeleton !w-36 border" />
+                    <td className="flex gap-2 items-center w-60">
+                        <div className="skeleton !w-8 !h-8 rounded-full" />
+                        <div className="skeleton !w-40" />
                     </td>
                     <td className="py-5">
-                        <div className="skeleton border" />
+                        <div className="skeleton" />
                     </td>
                     <td className="py-5">
-                        <div className="skeleton border" />
+                        <div className="skeleton" />
                     </td>
                     <td className="py-5">
-                        <div className="skeleton border" />
+                        <div className="skeleton" />
                     </td>
-                    <td className="flex gap-2 items-center">
-                        <div className="skeleton !w-10 !h-10 rounded-full" />
+                    <td className="flex gap-2 items-center w-40">
+                        <div className="skeleton !w-8 !h-8 rounded-full" />
                         <div className="flex-1 gap-1">
-                            <div className="skeleton !w-28 border mb-1" />
-                            <div className="skeleton !w-20 border" />
+                            <div className="skeleton mb-1" />
+                            <div className="skeleton !w-10" />
                         </div>
                     </td>
                     <td className="py-5">
-                        <div className="skeleton  border !w-10" />
+                        <div className="skeleton  !w-10" />
                     </td>
                     <td className="py-5">
-                        <div className="skeleton  border !w-28" />
+                        <div className="skeleton  !w-28" />
                     </td>
-                    <td className="flex gap-2 items-center">
-                        <div className="skeleton !w-10 !h-10 rounded-full" />
-                        <div className="flex-1 gap-1">
-                            <div className="skeleton !w-20 border mb-1" />
-                            <div className="skeleton !w-32 border" />
+                    <td className="flex gap-2 items-center w-40">
+                        <div className="skeleton !w-8 !h-8 rounded-full" />
+                        <div className="flex-1">
+                            <div className="skeleton !w-10 mb-1" />
+                            <div className="skeleton" />
                         </div>
                     </td>
                 </tr>
