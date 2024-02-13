416f31282fb80bbfbece8435264c2db3aef126df
i-jinkyung <ijinkyung12@gmail.com>
2024-02-13 20:02:34 +0900

    Fix: infinite loop 에러 임시 해결

--- src/pages/v3/orgs/[orgId]/connects/index.tsx
+++ src/pages/v3/orgs/[orgId]/connects/index.tsx
@@ -14,22 +14,22 @@ export const V30ConnectsPageRoute = pathRoute({
         }),
 });
 
-export const getStaticPaths = async () => ({
-    paths: [{params: {orgId: '1'}}],
-    fallback: true,
-});
-
-export const getStaticProps = async ({locale}: any) => ({
-    props: {
-        // Will be passed to the page component as props
-        ...(await serverSideTranslations(locale, [
-            ...v3CommonRequires,
-            'org-home',
-            'google-compliance',
-            // 여기에 이 페이지에서 사용할 locale 파일을 추가하세요.'org-home'
-        ])),
-    },
-});
+// export const getStaticPaths = async () => ({
+//     paths: [{params: {orgId: '1'}}],
+//     fallback: true,
+// });
+//
+// export const getStaticProps = async ({locale}: any) => ({
+//     props: {
+//         // Will be passed to the page component as props
+//         ...(await serverSideTranslations(locale, [
+//             ...v3CommonRequires,
+//             'org-home',
+//             'google-compliance',
+//             // 여기에 이 페이지에서 사용할 locale 파일을 추가하세요.'org-home'
+//         ])),
+//     },
+// });
 
 export default function Page() {
     const orgId = useRouterIdParamState('orgId', orgIdParamState);
