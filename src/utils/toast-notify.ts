a18bbb1ddbeebc52c63153f76b481a805214ad4d
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-04-24 19:40:59 +0900

    기본 api 응답 토스트의 모듈 변경

--- src/utils/toast-notify.ts
+++ src/utils/toast-notify.ts
@@ -1,4 +1,4 @@
-import {toast} from 'react-toastify';
+import {toast} from 'react-hot-toast';
 
 export const successNotify = (msg: string) => {
     toast.success(msg);
@@ -8,6 +8,6 @@ export const errorNotify = (err: any) => {
     const error = err?.response?.data || {message: '', code: ''};
     const message = error.message as string[] | string;
     const code = error.code as string;
-    const toastKind = code === 'DUPLICATED_ENTITY' ? toast.info : toast.error;
+    const toastKind = code === 'DUPLICATED_ENTITY' ? toast : toast.error;
     Array.isArray(message) ? toastKind(message[0]) : toastKind(message);
 };
