b916d3de1204be5e0823483a86038272625c20bb
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-04-28 07:32:07 +0900

    SaaS 컬렉션에서 타입에러 수정

--- src/clients/public/products/ProductListPage/ProductListContentPanelSearchInput.tsx
+++ src/clients/public/products/ProductListPage/ProductListContentPanelSearchInput.tsx
@@ -5,18 +5,21 @@ import {BsSearch} from 'react-icons/bs';
 import {useProductsInSaaSCollection} from '^models/Product/hook';
 import {FindAllProductQuery} from '^models/Product/type';
 import {currentProductCategoryAtom} from './ProductListSidePanel';
+import {debounce} from 'lodash';
 
 export const ProductListContentPanelSearchInput = memo(() => {
     const {search} = useProductsInSaaSCollection();
-    const form = useForm<FindAllProductQuery>();
     const [currentCategory, setCurrentCategory] = useRecoilState(currentProductCategoryAtom);
 
-    const onSubmit = (query: FindAllProductQuery) => {
-        const name = query.name;
+    const onChange = debounce((name?: string) => {
         if (name) {
-            query.order = {id: 'DESC'};
-            search(query);
-            setCurrentCategory(`Search: ${query.name}`);
+            search({
+                isLive: true,
+                itemsPerPage: 0,
+                name,
+                order: {id: 'DESC'},
+            });
+            setCurrentCategory(`Search: ${name}`);
         } else {
             search({
                 isLive: true,
@@ -25,7 +28,7 @@ export const ProductListContentPanelSearchInput = memo(() => {
             });
             setCurrentCategory(`☁️ All`);
         }
-    };
+    }, 500);
 
     // 사이드패널에서 카테고리 클릭시, 인풋을 클리어해줍니다.
     useEffect(() => {
@@ -34,17 +37,17 @@ export const ProductListContentPanelSearchInput = memo(() => {
         // 카테고리가 'Search: ' 로 시작하면, 검색으로 인한 변동이르모, 인풋을 비우지 않도록 합니다.
         if (currentCategory.startsWith('Search: ')) return;
 
-        form.resetField('name');
+        // form.setValue('name', '');
     }, [currentCategory]);
 
     return (
         <div className="relative flex-1 sm:flex-none">
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+            <form>
                 <input
                     type="text"
                     className="input input-bordered min-w-[300px] w-full pl-[48px]"
                     placeholder="Search ..."
-                    {...form.register('name')}
+                    onChange={(e) => onChange(e.target.value)}
                 />
                 <div className="absolute top-0 bottom-0 m-auto w-[48px] h-[48px] flex items-center justify-center text-gray-400">
                     <BsSearch />
