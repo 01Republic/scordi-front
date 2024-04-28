e6fbfe57b39c7e0d26775b3724cc55d14ab5bf80
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-04-29 02:36:35 +0900

    앱 찾기 모달 / 모바일에서 전체화면 모드로 레이아웃 반응형 수정

--- src/clients/private/_modals/SearchAppModal/index.tsx
+++ src/clients/private/_modals/SearchAppModal/index.tsx
@@ -3,6 +3,7 @@ import {AnimatedModal} from '^components/modals/_shared/AnimatedModal';
 import {SearchAppModalInput} from './SearchAppModalInput';
 import {SearchResultSection} from './SearchResultSection';
 import {SelectedProductSection} from './SelectedProductSection';
+import {FaTimes} from 'react-icons/fa';
 
 interface SearchAppModalProps {
     isOpened: boolean;
@@ -14,10 +15,17 @@ export const SearchAppModal = memo((props: SearchAppModalProps) => {
 
     return (
         <AnimatedModal open={isOpened} onClose={onClose}>
-            <div className="max-w-lg mx-auto w-full self-start mt-20">
+            <div className="sm:max-w-lg mx-auto w-full self-start sm:mt-20">
                 <div className="w-full"></div>
 
-                <div className="modal-box w-full scale-100 p-0">
+                <div className="modal-box max-w-screen-sm sm:max-w-[32rem] w-full scale-100 p-0 rounded-none sm:rounded-box min-h-screen max-h-screen sm:min-h-min relative">
+                    <button
+                        onClick={onClose}
+                        className="sm:hidden btn btn-square !bg-transparent !border-none text-gray-400 hover:text-gray-500 transition-all absolute top-0 right-0 z-[1]"
+                    >
+                        <FaTimes size={20} />
+                        {/*닫기*/}
+                    </button>
                     <SearchAppModalInput />
                     <SearchResultSection />
                     <SelectedProductSection />
