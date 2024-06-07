6079c0924c70434da960b5f4add14c7e9be8fd7f
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-06-07 17:21:00 +0900

    구독 등록 플로우 / 문의 링크 추가

--- src/clients/private/_layouts/ConnectSubscriptionsLayout/LeftAppsStatusPanel/Inquiry.tsx
+++ src/clients/private/_layouts/ConnectSubscriptionsLayout/LeftAppsStatusPanel/Inquiry.tsx
@@ -1,19 +1,25 @@
 import {memo} from 'react';
-import {FiLifeBuoy} from 'react-icons/fi';
+import {BsQuestionCircle} from 'react-icons/bs';
+import {LinkTo} from '^components/util/LinkTo';
 
 export const Inquiry = memo(function Inquiry() {
     return (
         <div>
-            <FiLifeBuoy size={20} className="text-gray-500 mb-3 relative -left-[1px]" />
+            <BsQuestionCircle size={20} className="text-gray-500 mb-3 relative -left-[1px]" />
 
-            <h4 className="text-13 mb-3">Having trouble?</h4>
+            <h4 className="text-13 mb-3">연동에 문제가 있나요?</h4>
             <p className="text-12 font-medium text-gray-500 leading-[1.5] mb-4">
-                Feel free to contact us and we will always <br /> help you through the process.
+                아래 1:1 채팅상담 버튼을 눌러 발생된 문제를 설명한 뒤, <br /> 배정된 담당자에게 도움을 요청 해 주세요.
             </p>
 
-            <button className="btn btn-sm normal-case !bg-white !border-gray-300 !text-gray-500 !rounded-md shadow hover:shadow-lg">
-                Contact us
-            </button>
+            <LinkTo
+                href="https://scordi.channel.io/lounge"
+                target="_blank"
+                displayLoading={false}
+                className="btn btn-sm normal-case !bg-white !border-gray-300 !text-gray-500 !rounded-md shadow hover:shadow-lg"
+            >
+                1:1 채팅상담
+            </LinkTo>
         </div>
     );
 });
