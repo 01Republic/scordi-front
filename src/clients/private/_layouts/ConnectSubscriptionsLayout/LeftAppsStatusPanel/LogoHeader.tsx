98946db1571c98ae95af2022a371d82f27b70ece
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-06-09 23:15:18 +0900

    구독 등록하기 플로우 / 돌아가기 기능 추가

--- src/clients/private/_layouts/ConnectSubscriptionsLayout/LeftAppsStatusPanel/LogoHeader.tsx
+++ src/clients/private/_layouts/ConnectSubscriptionsLayout/LeftAppsStatusPanel/LogoHeader.tsx
@@ -2,12 +2,22 @@ import {memo} from 'react';
 
 export const LogoHeader = memo(function LogoHeader() {
     return (
-        <img
-            src="/images/logo/scordi/png/symbol_and_text/symbol_and_text--primary.png"
-            alt=""
-            draggable={false}
-            loading="lazy"
-            className="w-[140px]"
-        />
+        <div className="flex items-center gap-4">
+            <img
+                src="/images/logo/scordi/png/symbol/circle/symbol-black-circle.png"
+                alt="scordi symbol logo"
+                draggable={false}
+                loading="lazy"
+                className="w-9 p-[2px]"
+            />
+
+            <img
+                src="/images/logo/scordi/png/text/text-black.png"
+                alt="scordi text logo"
+                draggable={false}
+                loading="lazy"
+                className="w-[120px] pb-[2px]"
+            />
+        </div>
     );
 });
--- src/clients/private/_layouts/ConnectSubscriptionsLayout/LeftAppsStatusPanel/index.tsx
+++ src/clients/private/_layouts/ConnectSubscriptionsLayout/LeftAppsStatusPanel/index.tsx
@@ -2,11 +2,25 @@ import {memo} from 'react';
 import {LogoHeader} from './LogoHeader';
 import {SelectedAppsSection} from './SelectedAppsSection';
 import {Inquiry} from './Inquiry';
+import {LinkTo} from '^components/util/LinkTo';
+import {FaAngleLeft} from 'react-icons/fa6';
+import {useRouter} from 'next/router';
 
 export const LeftAppsStatusPanel = memo(function LeftAppsStatusPanel() {
+    const router = useRouter();
     return (
         <aside className="w-full max-w-[400px] min-w-[250px] bg-gray-50 relative min-h-screen">
             <div className="pt-10 pb-14 px-10">
+                <div className="mb-2 text-14">
+                    <LinkTo
+                        onClick={() => router.back()}
+                        className="flex items-center gap-2 transition-all hover:gap-3 cursor-pointer"
+                        displayLoading={false}
+                    >
+                        <FaAngleLeft />
+                        <span>돌아가기</span>
+                    </LinkTo>
+                </div>
                 <LogoHeader />
             </div>
             <div
