4cbd8e01ac7f21860971803620cf7dedc88753cf
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-07-10 16:36:08 +0900

    구버전 UI 에서 발생하는 타입에러 해결

--- src/clients/private/orgs/subscriptions/OrgSubscriptionConnectsPage/ContentFunnels/inputs/PaymentMethod/PaymentMethodSelectModal/MoreButtonContent/CreditCardUpdateSwalForm.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionConnectsPage/ContentFunnels/inputs/PaymentMethod/PaymentMethodSelectModal/MoreButtonContent/CreditCardUpdateSwalForm.tsx
@@ -11,6 +11,7 @@ import {creditCardApi} from '^models/CreditCard/api';
 import {errorNotify} from '^utils/toast-notify';
 import {toast} from 'react-hot-toast';
 import Swal from 'sweetalert2';
+import {useAltForm} from '^hooks/useAltForm';
 
 interface CreditCardUpdateSwalFormProps {
     creditCard: CreditCardDto;
@@ -21,25 +22,28 @@ interface CreditCardUpdateSwalFormProps {
 export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormProps) => {
     const {creditCard, onSave, isCodefConnected} = props;
     const {id, organizationId: orgId, numbers} = creditCard;
+    const {formData, setFormValue, handleSubmit} = useAltForm(plainToInstance(UpdateCreditCardDto, {}));
     const form = useForm<UpdateCreditCardDto>();
 
     useEffect(() => {
-        // @ts-ignore
-        form.setValue('name', creditCard.name);
-        form.setValue('issuerCompany', creditCard.issuerCompany);
-        form.setValue('networkCompany', creditCard.networkCompany);
-        form.setValue('memo', creditCard.memo);
-
-        form.setValue('number1', numbers.number1);
-        form.setValue('number2', numbers.number2);
-        form.setValue('number3', numbers.number3);
-        form.setValue('number4', numbers.number4);
+        const cardNumbers = creditCard.numbers;
+        setFormValue({
+            name: creditCard.name,
+            isPersonal: creditCard.isPersonal,
+            isCreditCard: creditCard.isCreditCard,
+            usingStatus: creditCard.usingStatus,
+            memo: creditCard.memo,
+            number1: cardNumbers.number1,
+            number2: cardNumbers.number2,
+            number3: cardNumbers.number3,
+            number4: cardNumbers.number4,
+        });
     }, []);
 
-    const onSubmit = debounce(async (data: UpdateCreditCardDto) => {
-        const formData = plainToInstance(UnSignedCreditCardFormData, data);
+    const onSubmit = debounce(async () => {
+        const data = plainToInstance(UnSignedCreditCardFormData, formData);
         await creditCardApi
-            .update(orgId, id, formData.toUpdateDto())
+            .update(orgId, id, data.toUpdateDto())
             .then((res) => {
                 toast.success('변경되었습니다.');
                 onSave(res.data);
@@ -49,7 +53,7 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
     }, 500);
 
     return (
-        <SwalForm onSubmit={form.handleSubmit(onSubmit)}>
+        <SwalForm onSubmit={handleSubmit(onSubmit)}>
             <section className="">
                 <h4 className="text-xl sm:text-lg text-left">결제 수단 수정</h4>
             </section>
@@ -63,7 +67,8 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
                             type="text"
                             placeholder="디자인 팀 카드"
                             className="input border-gray-200 bg-gray-100 text-16 w-full"
-                            {...form.register('memo')}
+                            defaultValue={formData.memo || undefined}
+                            onChange={(e) => setFormValue({memo: e.target.value})}
                         />
                     </label>
                 </div>
@@ -74,7 +79,8 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
 
                         <select
                             className="select font-normal border-gray-200 bg-gray-100 text-16 w-full disabled:text-gray-400 disabled:opacity-100"
-                            {...form.register('issuerCompany')}
+                            defaultValue={formData.issuerCompany || undefined}
+                            onChange={(e) => setFormValue({issuerCompany: e.target.value})}
                             required={!isCodefConnected}
                             disabled={isCodefConnected}
                         >
@@ -94,7 +100,8 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
                             type="text"
                             placeholder="KB국민 4902"
                             className="input border-gray-200 bg-gray-100 text-16 w-full disabled:text-gray-400"
-                            {...form.register('name')}
+                            defaultValue={formData.name || undefined}
+                            onChange={(e) => setFormValue({name: e.target.value})}
                             required={!isCodefConnected}
                             disabled={isCodefConnected}
                         />
@@ -112,7 +119,7 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
                                 placeholder="●●●●"
                                 defaultValue={numbers.number1}
                                 onChange={(e) => {
-                                    form.setValue('number1', inputTextToCardNumberInShortFormat(e));
+                                    setFormValue({number1: inputTextToCardNumberInShortFormat(e)});
                                 }}
                                 disabled={isCodefConnected}
                             />
@@ -122,7 +129,7 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
                                 placeholder="●●●●"
                                 defaultValue={numbers.number2}
                                 onChange={(e) => {
-                                    form.setValue('number2', inputTextToCardNumberInShortFormat(e));
+                                    setFormValue({number2: inputTextToCardNumberInShortFormat(e)});
                                 }}
                                 disabled={isCodefConnected}
                             />
@@ -132,7 +139,7 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
                                 placeholder="●●●●"
                                 defaultValue={numbers.number3}
                                 onChange={(e) => {
-                                    form.setValue('number3', inputTextToCardNumberInShortFormat(e));
+                                    setFormValue({number3: inputTextToCardNumberInShortFormat(e)});
                                 }}
                                 disabled={isCodefConnected}
                             />
@@ -142,7 +149,7 @@ export const CreditCardUpdateSwalForm = memo((props: CreditCardUpdateSwalFormPro
                                 placeholder="●●●●"
                                 defaultValue={numbers.number4}
                                 onChange={(e) => {
-                                    form.setValue('number4', inputTextToCardNumberInShortFormat(e));
+                                    setFormValue({number4: inputTextToCardNumberInShortFormat(e)});
                                 }}
                                 disabled={isCodefConnected}
                             />
