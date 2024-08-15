07033b0c9a7f96ba6ce08276e35d4c79844d8a10
seongonecho <seongonecho@gmail.com>
2024-08-16 00:57:54 +0700

    update: 구독리스트 목록 페이지 빈 테이블 표시

--- src/clients/private/orgs/subscriptions/OrgSubscriptionListPage/index.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionListPage/index.tsx
@@ -10,6 +10,7 @@ import {debounce} from 'lodash';
 import {LinkTo} from '^components/util/LinkTo';
 import {OrgSubscriptionSelectPageRoute} from '^pages/orgs/[id]/subscriptions/select';
 import {FaPlus} from 'react-icons/fa6';
+import {EmptyTable} from '^clients/private/_components/table/EmptyTable';
 
 export const OrgSubscriptionListPage = memo(function OrgSubscriptionListPage() {
     const orgId = useRecoilValue(orgIdParamState);
@@ -33,34 +34,40 @@ export const OrgSubscriptionListPage = memo(function OrgSubscriptionListPage() {
         });
     }, 500);
 
+    const AddSubscriptionButton = () => (
+        <LinkTo href={OrgSubscriptionSelectPageRoute.path(orgId)} className="btn btn-scordi gap-2" loadingOnBtn>
+            <FaPlus />
+            <span>새 구독 등록</span>
+        </LinkTo>
+    );
+
     return (
         <ListPage
             onReady={onReady}
             breadcrumb={['구독', {text: '구독 리스트', active: true}]}
             titleText="구독 리스트"
-            Buttons={() => (
-                <LinkTo href={OrgSubscriptionSelectPageRoute.path(orgId)} className="btn btn-scordi gap-2" loadingOnBtn>
-                    <FaPlus />
-                    <span>새 구독 등록</span>
-                </LinkTo>
-            )}
+            Buttons={() => <AddSubscriptionButton />}
             ScopeHandler={undefined}
             onSearch={onSearch}
             searchInputPosition="start-of-buttons"
         >
-            <ListTableContainer
-                pagination={result.pagination}
-                movePage={movePage}
-                changePageSize={changePageSize}
-                unit="개"
-            >
-                <ListTable
-                    items={result.items}
-                    isLoading={isLoading}
-                    Header={() => <SubscriptionTableHeader orderBy={orderBy} />}
-                    Row={({item}) => <SubscriptionTableRow subscription={item} reload={reload} />}
-                />
-            </ListTableContainer>
+            {result.items.length > 0 ? (
+                <ListTableContainer
+                    pagination={result.pagination}
+                    movePage={movePage}
+                    changePageSize={changePageSize}
+                    unit="개"
+                >
+                    <ListTable
+                        items={result.items}
+                        isLoading={isLoading}
+                        Header={() => <SubscriptionTableHeader orderBy={orderBy} />}
+                        Row={({item}) => <SubscriptionTableRow subscription={item} reload={reload} />}
+                    />
+                </ListTableContainer>
+            ) : (
+                <EmptyTable icon={'🔍'} message="등록된 구독이 없어요." Buttons={() => <AddSubscriptionButton />} />
+            )}
         </ListPage>
     );
 });
