93825d66098bc3a405d70d7dc37165553ec8d03c
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-08-26 14:29:38 +0900

    design: 프로필 수정 모달 UI 스타일 수정

--- src/clients/private/_modals/EditUserProfileModal/index.tsx
+++ src/clients/private/_modals/EditUserProfileModal/index.tsx
@@ -31,9 +31,9 @@ export const EditUserProfileModal = (props: EditUserProfileModalProps) => {
 
     return (
         <AnimatedModal open={isOpened} onClose={onClose}>
-            <div className="w-[48rem] bg-white px-6 py-4 sm:px-12 sm:py-10 rounded-2xl flex flex-col gap-6 sm:gap-10">
+            <div className="w-[48rem] bg-white px-6 py-4 sm:px-12 sm:py-10 rounded-2xl flex flex-col gap-5 sm:gap-10">
                 <div className="w-full flex justify-between items-center">
-                    <h3 className="font-bold text-lg">내 계정</h3>
+                    <h3 className="font-bold text-18">내 계정</h3>
                     <button>
                         <IoClose size={26} />
                     </button>
@@ -41,37 +41,63 @@ export const EditUserProfileModal = (props: EditUserProfileModalProps) => {
 
                 <article className="w-full flex flex-col sm:flex-row">
                     <div className="w-[128px] h-[128px]">아바타</div>
-                    <div>
+                    <div className="grow">
                         <div>
-                            <h4>{name}</h4>
-                            <p className="text-gray-700 text-14 capitalize">
+                            <h4 className="font-bold text-18 mb-1">{name}</h4>
+                            <p className="text-gray-700 capitalize mb-6">
                                 {membershipLevel.toLowerCase()} @{currentOrg?.name}
                             </p>
-                            <div>
-                                <p>
-                                    <span>휴대전화 번호</span>
+                            <div className="mb-6 flex flex-col gap-2 text-14 text-gray-900">
+                                <p className="flex items-center gap-4">
+                                    <span className="w-32">휴대전화 번호</span>
                                     <span>{phone}</span>
                                 </p>
-                                <p>
-                                    <span>이메일</span>
+                                <p className="flex items-center gap-4">
+                                    <span className="w-32">이메일</span>
                                     <span>{email}</span>
                                 </p>
                             </div>
                             <p>
-                                <button>비밀번호 변경하기</button>
+                                <button className="cursor-not-allowed text-gray-300 mb-6">비밀번호 변경하기</button>
                             </p>
                         </div>
 
-                        <div>선</div>
+                        <div className="h-[1.5px] w-full bg-stroke-gray mb-6"></div>
 
-                        <div>
-                            <div>
-                                <h4>알림</h4>
-                                <div className="card card-bordered"></div>
+                        <div className="flex flex-col gap-4">
+                            <div className="w-full">
+                                <h4 className="font-semibold">알림</h4>
+                                <div className="flex flex-col gap-3">
+                                    <div className="rounded-2xl border border-stroke-gary p-4 flex flex-col gap-2">
+                                        <div className="flex justify-between items-center">
+                                            <span className="text-14 font-medium">Email</span>
+                                            <button>스위치</button>
+                                        </div>
+                                        <p className="text-12 text-gray-400">
+                                            mia@01republic.io으로 scordi 관련 알림 메일이 발송됩니다.
+                                        </p>
+                                    </div>
+                                    <div className="rounded-2xl border border-stroke-gary p-4 flex flex-col gap-2">
+                                        <div className="flex justify-between items-center">
+                                            <span className="text-14 font-medium">SMS</span>
+                                            <button>스위치</button>
+                                        </div>
+                                        <p className="text-12 text-gray-400">
+                                            010-8770-1941으로 scordi 관련 알림 SMS가 발송됩니다.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <h4>혜택 및 이벤트 알림</h4>
-                                <div className="card card-bordered"></div>
+
+                            <div className="w-full">
+                                <h4 className="font-semibold">혜택 및 이벤트 알림</h4>
+                                <div className="rounded-2xl border border-stroke-gary p-4 flex flex-col gap-2">
+                                    <div className="flex justify-between items-center">
+                                        <span className="text-14 font-medium">마케팅 정보 수신 동의</span>
+                                        <button>스위치</button>
+                                    </div>
+                                    <p className="text-12 text-gray-400">scordi의 혜택·정보를 받아 볼 수 있습니다.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
