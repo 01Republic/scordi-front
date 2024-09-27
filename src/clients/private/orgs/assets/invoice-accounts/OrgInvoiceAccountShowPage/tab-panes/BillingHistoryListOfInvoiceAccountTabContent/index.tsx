cffb32a0d65e80a52ebce2d3b5815d765f745cde
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-09-27 14:23:56 +0900

    공사중 표시 추가

--- src/clients/private/orgs/assets/invoice-accounts/OrgInvoiceAccountShowPage/tab-panes/BillingHistoryListOfInvoiceAccountTabContent/index.tsx
+++ src/clients/private/orgs/assets/invoice-accounts/OrgInvoiceAccountShowPage/tab-panes/BillingHistoryListOfInvoiceAccountTabContent/index.tsx
@@ -3,6 +3,7 @@ import {useRecoilValue} from 'recoil';
 import {orgIdParamState} from '^atoms/common';
 import {useCurrentInvoiceAccount} from '../../atom';
 import {useBillingHistoryListOfInvoiceAccount} from '^models/BillingHistory/hook';
+import {FcVlc} from 'react-icons/fc';
 
 export const BillingHistoryListOfInvoiceAccountTabContent = memo(function () {
     const orgId = useRecoilValue(orgIdParamState);
@@ -22,7 +23,12 @@ export const BillingHistoryListOfInvoiceAccountTabContent = memo(function () {
 
     return (
         <section className="py-4">
-            <div>BillingHistoryListOfInvoiceAccountTabContent</div>
+            <div className="flex items-center justify-center py-10">
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <FcVlc fontSize={40} className="opacity-60" />
+                    <div className="font-bold text-2xl text-gray-300">공사중이에요</div>
+                </div>
+            </div>
         </section>
     );
 });
