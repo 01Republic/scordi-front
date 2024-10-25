5e4ad4f8378bae98a29d11f8cbdb97553fab3f75
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-10-25 18:13:37 +0900

    1025 QA / 구독 등록하기 검색 페이지 텍스트 워싱

--- src/clients/private/orgs/subscriptions/OrgSubscriptionSelectPage/index.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionSelectPage/index.tsx
@@ -18,9 +18,7 @@ export const OrgSubscriptionSelectPage = memo(function OrgSubscriptionSelectPage
             <main className="w-full min-h-screen container max-w-4xl sm:pt-[80px] pb-8">
                 <div className="px-4 pt-10 sm:px-0 sm:pt-0 sm:text-center">
                     <h1 className="text-xl sm:text-2xl mb-3 sm:mb-4">어떤 앱을 이용하고 있나요?</h1>
-                    <p className="text-14 sm:text-16 mb-6 sm:mb-10">
-                        We'll give you personalized workflow recommendations based on the apps you choose.
-                    </p>
+                    <p className="text-14 sm:text-16 mb-6 sm:mb-10">아래 검색창에 구독 서비스 이름을 입력해보세요</p>
                 </div>
 
                 <div className="px-4 sm:px-0 relative">
