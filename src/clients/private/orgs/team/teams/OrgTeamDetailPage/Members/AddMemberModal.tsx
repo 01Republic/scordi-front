e45c41c3a91b45d382fbc7fffd62c9e19364e9b0
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-11-29 06:01:12 +0900

    팀 상세 페이지 / 멤버추가모달에 사용되는 (추가할 수 있는 멤버목록) 전용 atom, hook 추가 및 리팩토링

--- src/clients/private/orgs/team/teams/OrgTeamDetailPage/Members/AddMemberModal.tsx
+++ src/clients/private/orgs/team/teams/OrgTeamDetailPage/Members/AddMemberModal.tsx
@@ -2,11 +2,12 @@ import React, {memo, useEffect} from 'react';
 import {ModalProps} from '^components/modals/_shared/Modal.types';
 import {orgIdParamState, teamIdParamState} from '^atoms/common';
 import {useRecoilValue} from 'recoil';
-import {TeamMemberDto, useTeamMembersInTeamMembersTable} from '^models/TeamMember';
+import {TeamMemberDto, useAddableTeamMemberListInAddTeamMemberModal} from '^models/TeamMember';
 import {teamMembershipApi} from '^models/TeamMembership/api';
 import {TeamMembershipDto} from '^models/TeamMembership/type';
 import {TeamMemberSelectItem} from '^v3/share/modals/AppShowPageModal/TeamMemberSelectModal/TeamMemberSelectItem';
 import {toast} from 'react-hot-toast';
+import {debounce} from 'lodash';
 
 interface AddMemberModalProps extends ModalProps {
     preItems?: TeamMembershipDto[];
@@ -16,24 +17,35 @@ export const AddMemberModal = memo(function AddMemberModal(props: AddMemberModal
     const orgId = useRecoilValue(orgIdParamState);
     const teamId = useRecoilValue(teamIdParamState);
     const {isOpened, onClose} = props;
-    const {result, search, reload} = useTeamMembersInTeamMembersTable();
+    const {result, search, reset} = useAddableTeamMemberListInAddTeamMemberModal();
     const [selected, setSelected] = React.useState<TeamMemberDto[]>([]);
 
-    const onSave = () => {
-        const requests = selected.map((member) =>
-            teamMembershipApi.create(orgId, {teamId: teamId, teamMemberId: member.id}),
-        );
-        const req = Promise.allSettled(requests);
-        req.then(() => {
-            toast.success('새로운 멤버를 연결 했어요');
-            setSelected([]);
-            onClose();
+    const fetchAddableTeamMembers = () => {
+        if (!orgId || isNaN(orgId)) return;
+        if (!teamId || isNaN(teamId)) return;
+
+        search({
+            relations: ['teams'],
+            where: {organizationId: orgId},
+            itemsPerPage: 0,
+            order: {id: 'DESC'},
         });
     };
 
-    const entries = result.items.filter(
-        (item) => !props.preItems?.map((item) => item.teamMember?.id).includes(item.id),
-    );
+    const onSave = () => {
+        const requests = selected.map(({id: teamMemberId}) => {
+            return teamMembershipApi.create(orgId, {teamId, teamMemberId});
+        });
+
+        Promise.allSettled(requests)
+            .then(() => toast.success('새로운 멤버를 연결 했어요'))
+            .then(() => setSelected([]))
+            .then(() => onClose());
+    };
+
+    const availableTeamMembers = result.items.filter(({teams = []}) => {
+        return teams.every((team) => team.id !== teamId);
+    });
 
     const onCloseModal = () => {
         setSelected([]);
@@ -41,8 +53,8 @@ export const AddMemberModal = memo(function AddMemberModal(props: AddMemberModal
     };
 
     useEffect(() => {
-        !!orgId && !!teamId && reload();
-    }, [orgId, teamId]);
+        isOpened ? fetchAddableTeamMembers() : reset();
+    }, [isOpened]);
 
     return (
         <div
@@ -57,14 +69,24 @@ export const AddMemberModal = memo(function AddMemberModal(props: AddMemberModal
                     e.preventDefault();
                 }}
             >
-                <div className="p-4 bg-scordi">
-                    <h3 className="font-bold text-lg text-white">팀에 등록할 팀 멤버를 선택해 주세요</h3>
-                    <p className="text-sm text-white opacity-70">이미 추가된 멤버는 뺐어요</p>
-                </div>
+                {availableTeamMembers.length ? (
+                    <div className="p-4 bg-scordi">
+                        <h3 className="font-bold text-lg text-white">팀에 등록할 팀 멤버를 선택해 주세요</h3>
+                        <p className="text-sm text-white opacity-70">이미 추가된 멤버는 뺐어요</p>
+                    </div>
+                ) : (
+                    <div className="p-4">
+                        <h3 className="font-bold text-lg text-scordi">
+                            <span>{result.pagination.totalItemCount && '더 이상'}</span>{' '}
+                            <span>추가 할 수 있는 팀원이 없어요</span>
+                        </h3>
+                    </div>
+                )}
+
                 <div className="px-4 pb-4 flex flex-col h-[50vh] overflow-y-auto no-scrollbar">
                     <div className="flex-1 py-4 px-2 text-sm">
                         <ul>
-                            {entries.map((teamMember, i) => (
+                            {availableTeamMembers.map((teamMember, i) => (
                                 <li key={i}>
                                     <TeamMemberSelectItem
                                         item={teamMember}
