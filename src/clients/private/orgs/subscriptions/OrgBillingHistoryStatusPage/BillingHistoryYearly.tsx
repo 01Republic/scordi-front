8ded56d54e23b6febf4e5871bc9931feb1abf89c
seongonecho <seongonecho@gmail.com>
2024-12-07 23:10:37 +0900

    add: 연도별 뷰에 배경 추가

--- src/clients/private/orgs/subscriptions/OrgBillingHistoryStatusPage/BillingHistoryYearly.tsx
+++ src/clients/private/orgs/subscriptions/OrgBillingHistoryStatusPage/BillingHistoryYearly.tsx
@@ -5,6 +5,7 @@ import {useBillingHistoryStatus} from '^hooks/useBillingHistoryStatus';
 import {FaQuestion} from 'react-icons/fa6';
 import {Avatar} from '^components/Avatar';
 import {EmptyTable} from '^clients/private/_components/table/EmptyTable';
+import {CardContainerTableLayout} from '^clients/private/_components/table/ListTable';
 
 interface BillingHistoryYearlyProps {
     history: BillingHistoriesYearlySumBySubscriptionDto[];
@@ -44,57 +45,59 @@ export const BillingHistoryYearly = memo((props: BillingHistoryYearlyProps) => {
     };
 
     return (
-        <div className="bg-white border border-gray-300 overflow-hidden shadow rounded-2xl">
-            <div className="overflow-x-auto w-full">
-                <table className="table w-full text-sm">
-                    <thead>
-                        <tr className="bg-slate-100">
-                            <th className="sticky left-0 !bg-slate-100 z-10">서비스명</th>
-                            <th className="text-right">유/무료</th>
-                            <th className="text-right">평균지출액</th>
-                            {reversedYears.map((year) => (
-                                <th key={year} className="text-right">
-                                    {year}년
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {history.length === 0 ? (
-                            <tr>
-                                <td colSpan={reversedYears.length + 3} className="text-center py-8">
-                                    <EmptyTable message="데이터가 없습니다." />
-                                </td>
+        <CardContainerTableLayout>
+            <div className="bg-white border border-gray-300 overflow-hidden shadow rounded-lg">
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full text-sm">
+                        <thead>
+                            <tr className="bg-slate-100">
+                                <th className="sticky left-0 !bg-slate-100 z-10">서비스명</th>
+                                <th className="text-right">유/무료</th>
+                                <th className="text-right">평균지출액</th>
+                                {reversedYears.map((year) => (
+                                    <th key={year} className="text-right">
+                                        {year}년
+                                    </th>
+                                ))}
                             </tr>
-                        ) : (
-                            history.map((item, idx) => (
-                                <tr key={idx} className="group">
-                                    <td className="sticky left-0 bg-white z-10 flex space-x-2 font-medium">
-                                        <Avatar
-                                            className="w-6 h-6"
-                                            src={item.subscription.product.image}
-                                            alt={item.subscription.product.name()}
-                                            draggable={false}
-                                            loading="lazy"
-                                        >
-                                            <FaQuestion size={24} className="text-gray-300 h-full w-full p-[6px]" />
-                                        </Avatar>
-                                        <span>{item.subscription.product.name()}</span>
+                        </thead>
+                        <tbody>
+                            {history.length === 0 ? (
+                                <tr>
+                                    <td colSpan={reversedYears.length + 3} className="text-center py-8">
+                                        <EmptyTable message="데이터가 없습니다." />
                                     </td>
-                                    <td className="text-right">
-                                        <IsFreeTierTagUI value={item.subscription.isFreeTier} />
-                                    </td>
-                                    <td className="text-right font-medium">
-                                        {item.subscription.currentBillingAmount?.symbol}{' '}
-                                        {getAverageCost(item).toLocaleString()}
-                                    </td>
-                                    {renderYearlyColumns(item.items, reversedYears)}
                                 </tr>
-                            ))
-                        )}
-                    </tbody>
-                </table>
+                            ) : (
+                                history.map((item, idx) => (
+                                    <tr key={idx} className="group">
+                                        <td className="sticky left-0 bg-white z-10 flex space-x-2 font-medium">
+                                            <Avatar
+                                                className="w-6 h-6"
+                                                src={item.subscription.product.image}
+                                                alt={item.subscription.product.name()}
+                                                draggable={false}
+                                                loading="lazy"
+                                            >
+                                                <FaQuestion size={24} className="text-gray-300 h-full w-full p-[6px]" />
+                                            </Avatar>
+                                            <span>{item.subscription.product.name()}</span>
+                                        </td>
+                                        <td className="text-right">
+                                            <IsFreeTierTagUI value={item.subscription.isFreeTier} />
+                                        </td>
+                                        <td className="text-right font-medium">
+                                            {item.subscription.currentBillingAmount?.symbol}{' '}
+                                            {getAverageCost(item).toLocaleString()}
+                                        </td>
+                                        {renderYearlyColumns(item.items, reversedYears)}
+                                    </tr>
+                                ))
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </CardContainerTableLayout>
     );
 });
