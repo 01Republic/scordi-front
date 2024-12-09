5f16d640fc692576bc28041b6b3965619820c2b7
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2024-12-09 21:19:28 +0900

    앱 구독 등록p / 청구서 계정 선택 문구 수정

--- src/clients/private/orgs/subscriptions/OrgSubscriptionConnectsPage/ContentFunnels/steps/InvoiceAccountSelectStep.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionConnectsPage/ContentFunnels/steps/InvoiceAccountSelectStep.tsx
@@ -4,7 +4,7 @@ import {InvoiceAccountSelect} from '../inputs/InvoiceAccountSelect';
 
 export const InvoiceAccountSelectStep = memo(function InvoiceAccountSelectStep() {
     return (
-        <StepLayout title="청구서(인보이스)를 받고 있는 이메일이 있나요?" desc="">
+        <StepLayout title="청구서를 받고 있는 이메일이 있나요?" desc="">
             <InvoiceAccountSelect />
         </StepLayout>
     );
