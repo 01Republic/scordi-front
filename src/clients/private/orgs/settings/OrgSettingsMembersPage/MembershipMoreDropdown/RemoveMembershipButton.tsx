d2e0f36205eb2cffa2f35c145ae9262e54144e05
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2024-12-11 10:32:31 +0900

    설정p / 멤버초대 취소 또는 삭제에 따른 컨펌창 수정

--- src/clients/private/orgs/settings/OrgSettingsMembersPage/MembershipMoreDropdown/RemoveMembershipButton.tsx
+++ src/clients/private/orgs/settings/OrgSettingsMembersPage/MembershipMoreDropdown/RemoveMembershipButton.tsx
@@ -14,22 +14,35 @@ export const RemoveMembershipButton = memo((props: RemoveMembershipButtonProps) 
     const {membership, reload} = props;
 
     const onClick = async () => {
-        const isConfirmed = await confirm2(
-            `멤버를 삭제할까요?`,
+        const isInvitation = !membership.userId;
+        const resolveMembershipActionMessage = isInvitation ? '멤버 초대를 취소할까요?' : '멤버를 삭제할까요?';
+
+        const resolveMembershipActionConfirmMessage = isInvitation ? (
+            <p>
+                이미 초대장이 발송된 멤버입니다.
+                <br />
+                <b>워크스페이스 전체</b>에서 멤버가 삭제됩니다. <br />
+                그래도 초대를 최소하시겠어요?
+            </p>
+        ) : (
             <p>
                 이 작업은 취소할 수 없습니다.
                 <br />
                 <b>워크스페이스 전체</b>에서 삭제됩니다. <br />
                 그래도 삭제하시겠어요?
-            </p>,
-        ).then((res) => res.isConfirmed);
+            </p>
+        );
+
+        const isConfirmed = await confirm2(resolveMembershipActionMessage, resolveMembershipActionConfirmMessage).then(
+            (res) => res.isConfirmed,
+        );
 
         if (!isConfirmed) return;
 
         return membershipApi
             .destroy(membership.id)
             .then(() => {
-                const message = !membership.userId ? '초대를 취소했습니다.' : '삭제했습니다.';
+                const message = isInvitation ? '초대를 취소했어요.' : '삭제했습니다.';
                 toast.success(message);
             })
             .catch(errorToast)
