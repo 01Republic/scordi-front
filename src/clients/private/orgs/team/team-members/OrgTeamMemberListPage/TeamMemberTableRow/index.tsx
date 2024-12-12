84767cf5ca4966af1f85b59b4e8008107c681462
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2024-12-12 11:36:10 +0900

    테이블뷰 기준값 수정 / 구성원 목록p / 비고란 추가

--- src/clients/private/orgs/team/team-members/OrgTeamMemberListPage/TeamMemberTableRow/index.tsx
+++ src/clients/private/orgs/team/team-members/OrgTeamMemberListPage/TeamMemberTableRow/index.tsx
@@ -1,10 +1,16 @@
 import React, {memo, useState} from 'react';
-import {TeamMemberDto} from '^models/TeamMember';
+import {teamMemberApi, TeamMemberDto, UpdateTeamMemberDto} from '^models/TeamMember';
 import {TeamMemberAvatar} from '^v3/share/TeamMemberAvatar';
 import {TeamSelect} from '^v3/V3OrgTeam/V3OrgTeamMembersPage/TeamMemberTableSection/TaemMemberTable/TeamMemberTableRow/TeamSelect';
 import {TeamMemberStatusDropdown} from '^v3/V3OrgTeam/V3OrgTeamMembersPage/TeamMemberTableSection/TaemMemberTable/TeamMemberTableRow/TeamMemberStatusDropdown';
 import {OrgTeamMemberShowPageRoute} from '^pages/orgs/[id]/teamMembers/[teamMemberId]';
 import {OpenButtonColumn} from '^clients/private/_components/table/OpenButton';
+import {AirInputText} from '^v3/share/table/columns/share/AirInputText';
+import {TeamDto} from '^models/Team/type';
+import {useRecoilValue} from 'recoil';
+import {orgIdParamState} from '^atoms/common';
+import {toast} from 'react-hot-toast';
+import {errorToast} from '^api/api';
 
 interface TeamMemberTableRowProps {
     teamMember: TeamMemberDto;
@@ -13,12 +19,21 @@ interface TeamMemberTableRowProps {
 }
 
 export const TeamMemberTableRow = memo((props: TeamMemberTableRowProps) => {
+    const orgId = useRecoilValue(orgIdParamState);
     const [isLoading, setIsLoading] = useState(false);
     const {teamMember, onClick, reload} = props;
     const showPagePath = OrgTeamMemberShowPageRoute.path(teamMember.organizationId, teamMember.id);
 
     const hoverBgColor = 'group-hover:bg-scordi-light-50 transition-all';
     const loadingStyle = isLoading ? 'opacity-50 pointer-events-none' : '';
+
+    const update = async (dto: UpdateTeamMemberDto) => {
+        return teamMemberApi
+            .update(orgId, teamMember.id, {notes: dto.notes})
+            .then(() => toast.success('변경사항을 저장했어요.'))
+            .catch(errorToast)
+            .finally(() => reload && reload());
+    };
 
     return (
         <tr className="group">
@@ -71,6 +86,20 @@ export const TeamMemberTableRow = memo((props: TeamMemberTableRowProps) => {
                 <p className="block text-14 font-normal text-gray-400 group-hover:text-scordi-300 truncate">
                     {teamMember.phone}
                 </p>
+            </td>
+
+            {/* 비고 */}
+            <td
+                className={`cursor-pointer ${hoverBgColor} ${loadingStyle}`}
+                onClick={() => onClick && onClick(teamMember)}
+            >
+                <AirInputText
+                    defaultValue={teamMember.notes || undefined}
+                    onChange={async (notes) => {
+                        if (teamMember.notes === notes) return;
+                        return update({notes});
+                    }}
+                />
             </td>
 
             {/*/!* 권한 *!/*/}
