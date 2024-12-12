760442d3b76620c132b25d0b3c91de1faa92f8f8
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-12-12 19:16:06 +0900

    결제현황p / 중복된 코드 추상화

--- src/models/BillingHistory/type/billing-history-status.controller.dto.ts
+++ src/models/BillingHistory/type/billing-history-status.controller.dto.ts
@@ -15,14 +15,12 @@ export class BillingHistoryStatusDateRangeDto {
 }
 
 /**
- * yearlySum : 구독의 연간 합계 금액 조회
- * ----------------------------------------
+ * 추상 클래스
  */
 
-// 구독의 당해년도 합계 금액
-class BillingHistoriesYearlySumItemDto {
+// [추상] 구독의 특정 단위기간 합계 금액
+abstract class BillingHistoriesSumItemDto {
     subscriptionId: number;
-    issuedYear: number;
     amount: number;
     symbol: MoneyDto['symbol'];
     code: MoneyDto['code'];
@@ -37,13 +35,12 @@ class BillingHistoriesYearlySumItemDto {
     }
 }
 
-// 구독의 연간 합계 금액 내역
-export class BillingHistoriesYearlySumBySubscriptionDto {
+// [추상] 구독의 기간내 합계 금액 내역
+abstract class BillingHistoriesSumBySubscriptionDto {
     @TypeCast(() => SubscriptionDto)
     subscription: SubscriptionDto;
 
-    @TypeCast(() => BillingHistoriesYearlySumItemDto)
-    items: BillingHistoriesYearlySumItemDto[];
+    abstract items: BillingHistoriesSumItemDto[];
 
     getCostSum(exchangeRate: number, displayCurrency = CurrencyCode.KRW) {
         return this.items.reduce((sum, item) => {
@@ -77,63 +74,33 @@ export class BillingHistoriesYearlySumBySubscriptionDto {
 }
 
 /**
+ * yearlySum : 구독의 연간 합계 금액 조회
+ * ----------------------------------------
+ */
+
+// 구독의 '단위 년도' 합계 금액
+class BillingHistoriesYearlySumItemDto extends BillingHistoriesSumItemDto {
+    issuedYear: number;
+}
+
+// 구독의 연도별 합계 금액 내역
+export class BillingHistoriesYearlySumBySubscriptionDto extends BillingHistoriesSumBySubscriptionDto {
+    @TypeCast(() => BillingHistoriesYearlySumItemDto)
+    items: BillingHistoriesYearlySumItemDto[];
+}
+
+/**
  * monthlySum : 구독의 월간 합계 금액 조회
  * ----------------------------------------
  */
 
-// 구독의 당해월 합계 금액
-class BillingHistoriesMonthlySumItemDto {
-    subscriptionId: number;
+// 구독의 '단위 월' 합계 금액
+class BillingHistoriesMonthlySumItemDto extends BillingHistoriesSumItemDto {
     issuedYearMonth: number;
-    amount: number;
-    symbol: MoneyDto['symbol'];
-    code: MoneyDto['code'];
-
-    getCurrentAmount(exchangeRate: number, displayCurrency = CurrencyCode.KRW) {
-        const currentAmount =
-            displayCurrency === CurrencyCode.KRW && this.code !== CurrencyCode.KRW
-                ? this.amount * exchangeRate
-                : this.amount;
-
-        return currentAmount || 0;
-    }
 }
 
-// 구독의 월간 합계 금액 내역
-export class BillingHistoriesMonthlySumBySubscriptionDto {
-    @TypeCast(() => SubscriptionDto)
-    subscription: SubscriptionDto;
-
+// 구독의 월별 합계 금액 내역
+export class BillingHistoriesMonthlySumBySubscriptionDto extends BillingHistoriesSumBySubscriptionDto {
     @TypeCast(() => BillingHistoriesMonthlySumItemDto)
     items: BillingHistoriesMonthlySumItemDto[];
-
-    getCostSum(exchangeRate: number, displayCurrency = CurrencyCode.KRW) {
-        return this.items.reduce((sum, item) => {
-            if (displayCurrency === CurrencyCode.KRW && item.code !== CurrencyCode.KRW) {
-                return sum + item.amount * exchangeRate;
-            } else {
-                return sum + item.amount;
-            }
-        }, 0);
-    }
-
-    getCostSumToKRW(exchangeRate: number) {
-        return this.items.reduce((sum, item) => {
-            if (item.code === CurrencyCode.KRW) {
-                return sum + item.amount;
-            } else {
-                return sum + item.amount * exchangeRate;
-            }
-        }, 0);
-    }
-
-    getAverageCost(exchangeRate: number, displayCurrency = CurrencyCode.KRW) {
-        const total = this.getCostSum(exchangeRate, displayCurrency);
-        return total / this.items.length;
-    }
-
-    getAverageCostToKRW(exchangeRate: number) {
-        const total = this.getCostSumToKRW(exchangeRate);
-        return total / this.items.length;
-    }
 }
