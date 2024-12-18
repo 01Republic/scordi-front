04d8d160be744bd670af33d097691808b178ab10
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2024-12-18 14:09:02 +0900

    이달의 지출 총액 컴포넌트

---
+++ src/clients/private/orgs/home/OrgDashboardPage/MonthlyTotalExpense/index.tsx
@@ -0,0 +1,9 @@
+import {DashboardLayout} from '^clients/private/orgs/home/OrgDashboardPage/DashboardLayout';
+
+export const MonthlyTotalExpense = () => {
+    return (
+        <DashboardLayout title="이달의 지출 총액">
+            <div>오예</div>
+        </DashboardLayout>
+    );
+};
