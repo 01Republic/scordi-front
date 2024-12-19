71686e06fcd0ad311950bae2bdbfcd356cc4b010
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-12-20 04:08:37 +0900

    어드민 조직관리 - 연동 탭 / 코드에프 카드 삭제 api 및 기능 추가

--- src/clients/admin/orgs/AdminOrgDetailPage/tabContents/connects/ConnectWithCardTabContent/CodefCardListContent/CodefCardItem.tsx
+++ src/clients/admin/orgs/AdminOrgDetailPage/tabContents/connects/ConnectWithCardTabContent/CodefCardListContent/CodefCardItem.tsx
@@ -13,12 +13,14 @@ import {adminOrgDetail} from '^admin/orgs/AdminOrgDetailPage';
 import {LoadableBox} from '^components/util/loading';
 import {MdOutlineClear} from 'react-icons/md';
 import Tippy from '@tippyjs/react';
-import Tippy2 from '@tippyjs/react/headless';
 import {IoMdMore} from 'react-icons/io';
 import {creditCardApi} from '^models/CreditCard/api';
 import {toast} from 'react-hot-toast';
 import {MoreDropdown} from '^clients/private/_components/MoreDropdown';
 import {IoRefresh} from '@react-icons/all-files/io5/IoRefresh';
+import {confirm2, confirmed} from '^components/util/dialog';
+import {codefCardApi} from '^models/CodefCard/api';
+import {errorToast} from '^api/api';
 
 interface CodefCardItemProps {
     codefCard: CodefCardDto;
@@ -49,12 +51,40 @@ export const CodefCardItem = memo((props: CodefCardItemProps) => {
         }
     };
 
-    const disconnectCreditCard = () => {
+    // 이 '코드에프 카드' 항목 삭제
+    const removeCodefCard = () => {
+        if (!org) return;
+        const removeConfirm = () => {
+            return confirm2(
+                '진짜 삭제할까요?',
+                <div>
+                    <p>
+                        나중에 카드사 계정 연동이 최신화 되었을 때, DB에 없는 카드가 감지되면 사용자에게 신규카드가
+                        발견된 것 처럼 보일 수 있습니다.
+                    </p>
+                    <p>그래도 삭제할까요?</p>
+                </div>,
+            );
+        };
+
+        return confirmed(removeConfirm())
+            .then(() => codefCardApi.destroy(org.id, codefCard.id))
+            .then(() => toast.success(`코드에프 카드 (${codefCard.resCardName}) 삭제완료`))
+            .then(() => codefCard.creditCardId && disconnectCreditCard('연결된 결제수단(카드)도 함께 제거 할까요?'))
+            .then(() => reload())
+            .catch(errorToast);
+    };
+
+    // 연결된 결제수단(카드) 제거
+    const disconnectCreditCard = (title?: string) => {
         if (!org || !codefCard.creditCardId) return;
-        creditCardApi
-            .destroy(org.id, codefCard.creditCardId)
+        const creditCardId = codefCard.creditCardId;
+
+        return confirmed(confirm2(title || '진짜 삭제할까요?'))
+            .then(() => creditCardApi.destroy(org.id, creditCardId))
             .then(() => toast.success('Successfully disconnected'))
-            .then(() => reload());
+            .then(() => reload())
+            .catch(errorToast);
     };
 
     return (
@@ -160,14 +190,14 @@ export const CodefCardItem = memo((props: CodefCardItemProps) => {
                         {() => (
                             <div className="card card-bordered card-compact rounded-md shadow-lg bg-white text-12 min-w-[100px]">
                                 <div
-                                    onClick={disconnectCreditCard}
+                                    onClick={() => removeCodefCard()}
                                     className="cursor-pointer px-2 py-1 hover:bg-slate-100 btn-animation"
                                 >
                                     이 '코드에프 카드' 항목 삭제
                                 </div>
                                 {codefCard.creditCardId && (
                                     <div
-                                        onClick={disconnectCreditCard}
+                                        onClick={() => disconnectCreditCard()}
                                         className="cursor-pointer px-2 py-1 hover:bg-slate-100 btn-animation"
                                     >
                                         연결된 결제수단(카드) 제거
