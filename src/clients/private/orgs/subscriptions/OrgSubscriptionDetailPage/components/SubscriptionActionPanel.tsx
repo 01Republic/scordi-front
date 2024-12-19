83a36198c2718654b2586a5f317b2bfd64ee3b06
seongonecho <seongonecho@gmail.com>
2024-12-20 01:12:43 +0900

    update: 구독 상태 변경 버튼 -> 구독 삭제버튼으로 대체

--- src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/components/SubscriptionActionPanel.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/components/SubscriptionActionPanel.tsx
@@ -19,6 +19,7 @@ import {
 import {FaQuestion} from 'react-icons/fa6';
 import {PiEmptyBold} from 'react-icons/pi';
 import {SubscriptionStatus, SubscriptionUsingStatus} from '^models/Subscription/types';
+import {confirm2} from '^components/util/dialog';
 
 export const SubscriptionActionPanel = memo(function SubscriptionActionPanel() {
     const {reload, currentSubscription: subscription} = useCurrentSubscription();
@@ -30,11 +31,6 @@ export const SubscriptionActionPanel = memo(function SubscriptionActionPanel() {
         window.open(subscription.product.pricingPageUrl, '_blank');
     };
 
-    const handleRefresh = () => {
-        // TODO 이거 뭔기능이더라.. -> 카드나 이메일 동기화하는 기능 -> 규리님이랑 논의 필요
-        toast.success('refresh');
-    };
-
     const handleChangeStatus = (status: SubscriptionUsingStatus) => {
         // TODO: readonly 임. (readonly usingStatus: SubscriptionUsingStatus)
         // subscriptionApi.update(subscription.id, {status}).then(() => {
@@ -43,10 +39,18 @@ export const SubscriptionActionPanel = memo(function SubscriptionActionPanel() {
         // });
     };
 
-    const handleRemove = () => {
-        // TODO: 예쁜 모달로 변경
-        const confirm = window.confirm('정말 삭제하시겠어요?');
-        if (!confirm) return;
+    const handleRemove = async () => {
+        const isConfirmed = await confirm2(
+            '구독을 삭제할까요?',
+            <div className="text-16">
+                이 작업은 취소할 수 없습니다.
+                <br />
+                <b>워크스페이스 전체</b>에서 삭제됩니다. <br />
+                그래도 삭제하시겠어요?
+            </div>,
+            'warning',
+        ).then((res) => res.isConfirmed);
+        if (!isConfirmed) return;
         subscriptionApi.destroy(subscription?.id).then(() => {
             toast.success('구독이 삭제되었어요.');
             router.replace(OrgSubscriptionListPageRoute.path(subscription.organizationId));
@@ -59,8 +63,8 @@ export const SubscriptionActionPanel = memo(function SubscriptionActionPanel() {
             onClick: handleGoToPricingPage,
         },
         {
-            icon: <MdRefresh fontSize={20} />,
-            onClick: handleRefresh,
+            icon: <CgTrash fontSize={20} />,
+            onClick: handleRemove,
         },
     ];
 
@@ -76,39 +80,6 @@ export const SubscriptionActionPanel = memo(function SubscriptionActionPanel() {
                     {button.icon}
                 </button>
             ))}
-
-            <ListPageDropdown>
-                <button
-                    tabIndex={0}
-                    className={`btn btn-square !bg-white border border-slate-300 rounded-md hover:border-slate-400 hover:shadow transition-all mb-1`}
-                >
-                    <CgMore fontSize={20} />
-                </button>
-
-                <ListPageDropdownMenu>
-                    <MethodOption
-                        Icon={MdOutlineFreeBreakfast}
-                        title="무료로 변경"
-                        onClick={() => handleChangeStatus(SubscriptionUsingStatus.FREE)}
-                    />
-                    <MethodOption
-                        Icon={MdOutlinePayment}
-                        title="유료로 변경"
-                        onClick={() => handleChangeStatus(SubscriptionUsingStatus.PAID)}
-                    />
-                    <MethodOption
-                        Icon={FaQuestion}
-                        title="미정으로 변경"
-                        onClick={() => handleChangeStatus(SubscriptionUsingStatus.NONE)}
-                    />
-                    <MethodOption
-                        Icon={PiEmptyBold}
-                        title="해지 상태로 변경"
-                        onClick={() => handleChangeStatus(SubscriptionUsingStatus.QUIT)}
-                    />
-                    <MethodOption Icon={CgTrash} title="구독 삭제하기" onClick={handleRemove} />
-                </ListPageDropdownMenu>
-            </ListPageDropdown>
         </div>
     );
 });
