3bfa9a3bcc27f3c5cd35cd5ab57e987aac559080
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2024-12-23 14:41:58 +0900

    대시보드p / 섹션별 레이아웃 컴포넌트 구현

--- src/clients/private/orgs/home/OrgDashboardPage/DashboardLayout.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/DashboardLayout.tsx
@@ -3,14 +3,21 @@ import {WithChildren} from '^types/global.type';
 
 interface DashboardLayoutProps extends WithChildren {
     title: string;
+    className?: string;
+    subTitle?: string;
+    Buttons?: () => JSX.Element;
 }
 
 export const DashboardLayout = memo((props: DashboardLayoutProps) => {
-    const {title, children} = props;
+    const {title, className, subTitle, Buttons, children} = props;
 
     return (
-        <article className="w-full flex flex-col justify-start gap-5 border rounded-3xl bg-white p-7">
-            <h6 className="font-semibold text-20">{title}</h6>
+        <article className={`w-full flex flex-col justify-start gap-5 border rounded-3xl bg-white p-7 ${className}`}>
+            <div className="flex items-center justify-between">
+                <h6 className="font-semibold text-20">{title}</h6>
+                {subTitle && <p className="font-semibold text-[22px]">{subTitle}</p>}
+                {Buttons && <Buttons />}
+            </div>
             <div>{children}</div>
         </article>
     );
