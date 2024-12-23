772ec360ac2d9494532500f76da077b076cfd889
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2024-12-23 14:28:39 +0900

    대시보드p / 이달이 지출 총액 섹션 - MonthlyTotalExpense 컴포넌트 이름 변경으로 인한 삭제

--- src/clients/private/orgs/home/OrgDashboardPage/MonthlyTotalExpense/index.tsx
+++
@@ -1,9 +0,0 @@
-import {DashboardLayout} from '^clients/private/orgs/home/OrgDashboardPage/DashboardLayout';
-
-export const MonthlyTotalExpense = () => {
-    return (
-        <DashboardLayout title="이달의 지출 총액">
-            <div>오예</div>
-        </DashboardLayout>
-    );
-};
