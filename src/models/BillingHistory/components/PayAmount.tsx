6b8ca6666e50f249a726915834307d3de2dd06f6
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-12-29 19:51:42 +0900

    청구서 계정 상세p - 결제내역탭 / 결제내역의 금액 표기 수정 및 결제실패 상태 출력 수정

--- src/models/BillingHistory/components/PayAmount.tsx
+++ src/models/BillingHistory/components/PayAmount.tsx
@@ -1,5 +1,5 @@
 import React, {memo} from 'react';
-import {BillingHistoryDto} from '^models/BillingHistory/type';
+import {BillingHistoryDto, BillingHistoryStatus} from '^models/BillingHistory/type';
 
 interface PayAmountProps {
     billingHistory: BillingHistoryDto;
@@ -8,17 +8,21 @@ interface PayAmountProps {
 // 결제금액
 export const PayAmount = memo((props: PayAmountProps) => {
     const {billingHistory} = props;
+    const {about, payAmount} = billingHistory;
 
-    if (!billingHistory.payAmount) return <></>;
+    if (!payAmount) return <></>;
 
-    const {payAmount} = billingHistory;
-    const {code, amount, symbol, format} = payAmount;
-    const {exchangedCurrency, dollarPrice, exchangeRate} = payAmount;
+    // const {code, amount, symbol, format} = payAmount;
+    // const {exchangedCurrency, dollarPrice, exchangeRate} = payAmount;
 
     return (
-        <div className={`flex items-center gap-1 justify-end ${billingHistory.paidAt ? '' : 'text-red-400'}`}>
-            <span>{symbol}</span>
-            <span>{Math.round(amount).toLocaleString()}</span>
+        <div
+            className={`flex items-center gap-1 justify-end ${
+                about === BillingHistoryStatus.PayFail ? 'text-red-400' : ''
+            }`}
+        >
+            <span>{payAmount.symbol}</span>
+            <span>{payAmount.roundedAmount.toLocaleString()}</span>
         </div>
     );
 });
