70b6ce01f7be3a68ac276cd6e824690d5d4ac747
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-12-29 19:49:31 +0900

    청구서 계정 상세p - 결제내역탭 / 결제내역의 상태 표기 로직 변경 (billingHistory.about)

--- src/models/BillingHistory/components/BillingHistoryStatusTagUI.tsx
+++ src/models/BillingHistory/components/BillingHistoryStatusTagUI.tsx
@@ -1,5 +1,5 @@
 import {memo} from 'react';
-import {BillingHistoryDto} from '^models/BillingHistory/type';
+import {BillingHistoryDto, BillingHistoryStatus} from '^models/BillingHistory/type';
 import {TagUI} from '^v3/share/table/columns/share/TagUI';
 import {getColor} from '^components/util/palette';
 
@@ -9,9 +9,25 @@ interface BillingHistoryStatusTagUIProps {
 
 export const BillingHistoryStatusTagUI = memo((props: BillingHistoryStatusTagUIProps) => {
     const {billingHistory} = props;
+    const about = billingHistory.about;
 
-    if (billingHistory.paidAt) return <TagUI className={`bg-green-200`}>결제됨</TagUI>;
-    return <TagUI className={`bg-red-400 text-white`}>실패</TagUI>;
+    switch (about) {
+        case BillingHistoryStatus.PayWait:
+            return <TagUI className={`bg-green-100`}>예정</TagUI>;
+        case BillingHistoryStatus.PaySuccess:
+            return <TagUI className={`bg-green-200`}>결제됨</TagUI>;
+        case BillingHistoryStatus.PayFail:
+            return <TagUI className={`bg-red-400 text-white`}>실패</TagUI>;
+        case BillingHistoryStatus.Warning:
+        case BillingHistoryStatus.Info:
+            return <TagUI className={`bg-blue-200`}>정보</TagUI>;
+        case BillingHistoryStatus.Unknown:
+        default:
+            return <TagUI className={`bg-gray-200`}>미정</TagUI>;
+    }
+
+    // if (billingHistory.paidAt) return <TagUI className={`bg-green-200`}>결제됨</TagUI>;
+    // return <TagUI className={`bg-red-400 text-white`}>실패</TagUI>;
     // return <TagUI className={`bg-red-200`}>실패</TagUI>;
     // return <TagUI className={`bg-gray-200`}>예정</TagUI>;
 });
