b435f57e5b60c226477ab6ac9c9495f5d6ae003c
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2024-12-30 10:45:15 +0900

    SubscriptionSeat / 컬럼 추가 (시트 상태, 유/무료 여부)

--- src/models/SubscriptionSeat/type/SubscriptionSeat.dto.ts
+++ src/models/SubscriptionSeat/type/SubscriptionSeat.dto.ts
@@ -5,6 +5,14 @@ import {FindAllQueryDto} from '^types/utils/findAll.query.dto';
 import {TeamDto} from '^models/Team/type';
 import {PaginationDto} from '^types/utils/pagination.dto';
 
+// 시트 상태 - 미정, 무료, 유료, 해지
+export enum SubscriptionSeatStatus {
+    NONE = 'NONE', // 미정
+    FREE = 'FREE', // 무료
+    PAID = 'PAID', // 유료
+    QUIT = 'QUIT', // 해지
+}
+
 /**
  * 구독별 운영중인 시트
  */
@@ -12,6 +20,8 @@ export class SubscriptionSeatDto {
     id: number; // ID
     teamMemberId: number | null; // 팀 멤버 ID
     subscriptionId: number | null; // 구독 ID
+    status: SubscriptionSeatStatus; // 시트 상태 (default: 유료)
+    isPaid: boolean; // 유/무료 여부 (default: 유료)
     @TypeCast(() => Date) startAt?: Date | null; // 계정부여일
     @TypeCast(() => Date) finishAt?: Date | null; // 계정회수(예정)일
     memo: string | null; // 메모
