6eba84d9e03ab8ad0e340d8d8b6201801b9c421f
seongonecho <seongonecho@gmail.com>
2025-01-01 16:35:13 +0900

    update: 멤버탭 필터 작동

--- src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/tabs/SubscriptionMemberTab.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/tabs/SubscriptionMemberTab.tsx
@@ -11,6 +11,9 @@ import {MemberStatusScopeHandler} from '^clients/private/orgs/subscriptions/OrgS
 import {FaPlus} from 'react-icons/fa6';
 import {SubscriptionTeamMemberSelectModal} from '^clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/components/selects/SubscriptionTeamMemberSelect';
 import {useSubscriptionSeatsInMemberTab} from '^models/SubscriptionSeat/hook/useSubscriptionSeats';
+import {SubscriptionUsingStatus} from '^models/Subscription/types';
+import {Paginated} from '^types/utils/paginated.dto';
+import {SubscriptionSeatDto} from '^models/SubscriptionSeat/type';
 
 export const SubscriptionMemberTab = memo(function SubscriptionMemberTab() {
     const orgId = useRecoilValue(orgIdParamState);
@@ -18,6 +21,7 @@ export const SubscriptionMemberTab = memo(function SubscriptionMemberTab() {
     const {search, result, isLoading, isEmptyResult, isNotLoaded, movePage, changePageSize, reload, orderBy} =
         useSubscriptionSeatsInMemberTab();
     const [isOpened, setIsOpened] = useState(false);
+    const [originSeats, setOriginSeats] = useState<Paginated<SubscriptionSeatDto>>();
 
     if (!orgId || !subscription) return null;
 
@@ -25,27 +29,60 @@ export const SubscriptionMemberTab = memo(function SubscriptionMemberTab() {
         search({
             relations: ['teamMember', 'teamMember.teams'],
             order: {id: 'DESC'},
-        });
+        }).then((res) => setOriginSeats(res));
     };
 
     const onClose = () => {
         setIsOpened(false);
-        reload();
+        onPageReload();
     };
 
-    const willRemoveSeatCount = () => {
-        const willRemoveSeats = (subscription.subscriptionSeats || []).filter(
+    const onPageReload = () => {
+        reload().then((res) => setOriginSeats(res));
+    };
+
+    const getUsingCount = () => {
+        return (originSeats?.items || []).filter((seat) => !!seat.teamMemberId).length;
+    };
+    const usingCount = getUsingCount().toLocaleString();
+
+    const allSeatCount = (originSeats?.items.length || 0).toLocaleString();
+
+    const getWillRemoveSeatCount = () => {
+        const willRemoveSeats = (originSeats?.items || []).filter(
             (seat) => !!seat.finishAt && seat.finishAt.getMonth() === new Date().getMonth(),
         );
         return willRemoveSeats.length;
     };
+    const willRemoveSeatCount = getWillRemoveSeatCount().toLocaleString();
 
-    const removedSeatCount = () => {
-        const willRemoveSeats = (subscription.subscriptionSeats || []).filter(
+    const getRemovedSeatCount = () => {
+        const willRemoveSeats = (originSeats?.items || []).filter(
             (seat) => !!seat.finishAt && seat.finishAt < new Date(),
         );
         return willRemoveSeats.length;
     };
+    const removedSeatCount = getRemovedSeatCount().toLocaleString();
+
+    const onChangeScopeHandler = (status: SubscriptionUsingStatus | null) => {
+        let query = {};
+        if (status === SubscriptionUsingStatus.PAID) query = {isPaid: true};
+        if (status === SubscriptionUsingStatus.FREE) query = {isPaid: false};
+        if (status === SubscriptionUsingStatus.NONE) query = {finishAt: 'NULL'};
+        if (status === SubscriptionUsingStatus.QUIT) query = {finishAt: {op: 'not', val: 'NULL'}};
+        search({
+            relations: ['teamMember', 'teamMember.teams'],
+            order: {id: 'DESC'},
+            where: query,
+        });
+    };
+
+    const LinkMemberButton = () => (
+        <button className={'btn btn-outline btn-sm text-14 bg-white'} onClick={() => setIsOpened(true)}>
+            <FaPlus />
+            &nbsp;멤버 연결하기
+        </button>
+    );
 
     useEffect(() => {
         orgId && onReady();
@@ -56,38 +93,33 @@ export const SubscriptionMemberTab = memo(function SubscriptionMemberTab() {
             <div className={'bg-gray-200 grid grid-cols-4 p-4 space-x-4 rounded'}>
                 <StatusCard
                     title={'현재 할당된 계정'}
-                    titleValue={(subscription?.usedMemberCount || 0).toString()}
+                    titleValue={usingCount}
                     icon={<RiUser3Fill size={20} className="h-full w-full p-[6px] text-white" />}
                     iconColor={'bg-purple-400'}
                 />
                 <StatusCard
                     title={'구매한 계정'}
-                    titleValue={(subscription?.subscriptionSeats?.length || 0).toString()}
+                    titleValue={allSeatCount}
                     icon={<RiUserFollowFill size={20} className="h-full w-full p-[6px] text-white" />}
                     iconColor={'bg-orange-400'}
                 />
                 <StatusCard
                     title={'이번달 회수(예정) 계정'}
-                    titleValue={willRemoveSeatCount().toString()}
+                    titleValue={willRemoveSeatCount}
                     icon={<RiUserUnfollowFill size={20} className="h-full w-full p-[6px] text-white" />}
                     iconColor={'bg-pink-400'}
                 />
                 <StatusCard
                     title={'해지 완료된 계정'}
-                    titleValue={removedSeatCount().toString()}
+                    titleValue={removedSeatCount}
                     icon={<RiUserForbidFill size={20} className="h-full w-full p-[6px] text-white" />}
                     iconColor={'bg-blue-400'}
                 />
             </div>
 
             <div className={'flex justify-between'}>
-                {/* TODO 클릭 시 해당 필터로 동작, 근데 멤버 상태를 알 수 없음 */}
-                <MemberStatusScopeHandler onSearch={(status) => console.log(status)} />
-
-                <button className={'btn btn-outline btn-sm text-14 bg-white'} onClick={() => setIsOpened(true)}>
-                    <FaPlus />
-                    &nbsp;멤버 연결하기
-                </button>
+                <MemberStatusScopeHandler onSearch={onChangeScopeHandler} />
+                <LinkMemberButton />
             </div>
 
             <ListTableContainer
@@ -100,18 +132,13 @@ export const SubscriptionMemberTab = memo(function SubscriptionMemberTab() {
                 isEmptyResult={isEmptyResult}
                 emptyMessage="조회된 구성원이 없어요."
                 emptyButtonText="구성원 등록"
-                EmptyButtons={() => (
-                    <button className={'btn btn-outline btn-sm text-14 bg-white'} onClick={() => setIsOpened(true)}>
-                        <FaPlus />
-                        &nbsp;멤버 연결하기
-                    </button>
-                )}
+                EmptyButtons={LinkMemberButton}
             >
                 <ListTable
                     items={result.items}
                     isLoading={false}
                     Header={() => <TeamMemberInSubscriptionTableHeader orderBy={orderBy} />}
-                    Row={({item}) => <TeamMemberInSubscriptionTableRow seat={item} reload={reload} />}
+                    Row={({item}) => <TeamMemberInSubscriptionTableRow seat={item} reload={onPageReload} />}
                 />
             </ListTableContainer>
 
