b0f92eacda116b3fdd53f21e2ec577f35d90fb29
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-02 17:52:57 +0900

    대시보드p / 리스트아이템 레이아웃 구현

---
+++ src/clients/private/orgs/home/OrgDashboardPage/DashboardItemListLayout.tsx
@@ -0,0 +1,48 @@
+import React, {memo} from 'react';
+import Link from 'next/link';
+import cn from 'classnames';
+import {Avatar} from '^components/Avatar';
+
+interface ListItemLayoutProps {
+    url: string;
+    src: string;
+    Icon: React.ElementType;
+    avatarClassName?: string;
+    title: string;
+    subTitle: string;
+    message: string;
+    low?: boolean;
+}
+
+export const DashboardItemListLayout = memo((props: ListItemLayoutProps) => {
+    const {url, src, avatarClassName, title, subTitle, message, Icon, low = false} = props;
+
+    return (
+        <li className="first:border-t-0 last:border-b-0 border-t-[0.5px] border-b-[0.5px] p-4">
+            <Link href={url}>
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                        {src ? (
+                            <Avatar src={src} className={avatarClassName} draggable={false} loading="lazy" />
+                        ) : (
+                            <div className="w-7 h-7 rounded-full text-scordi flex items-center justify-center border border-scordi-light">
+                                <Icon />
+                            </div>
+                        )}
+
+                        <div
+                            className={cn('flex', {
+                                'flex-row items-center gap-3': low,
+                                'flex-col': !low,
+                            })}
+                        >
+                            <p className="font-normal text-14 text-gray-800">{title}</p>
+                            <p className="font-normal text-12 text-gray-400">{subTitle}</p>
+                        </div>
+                    </div>
+                    <p className="font-medium text-16 text-gray-900">{message}</p>
+                </div>
+            </Link>
+        </li>
+    );
+});
