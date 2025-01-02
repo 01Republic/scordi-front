e7f93f75d240ab2850476f10b6a49dfb512473dc
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-02 18:35:03 +0900

    대시보드p / 결제 수단 섹션 구현 hook으로 데이터 불러오기 및 데이터 사용

--- src/clients/private/orgs/home/OrgDashboardPage/PaymentMethodsSection/index.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/PaymentMethodsSection/index.tsx
@@ -1,58 +1,67 @@
 import {DashboardLayout} from '^clients/private/orgs/home/OrgDashboardPage/DashboardLayout';
+import {GoCreditCard, GoMail} from 'react-icons/go';
 import {Avatar} from '^components/Avatar';
 import React from 'react';
+import {useRouter} from 'next/router';
+import {useRecoilValue} from 'recoil';
+import {orgIdParamState} from '^atoms/common';
+import {currencyFormat} from '^utils/number';
+import {usePaymentMethodListInDashboard} from '^models/_dashboard/hook';
+import {OrganizationDto} from '^models/Organization/type';
+import {DashboardItemListLayout} from '^clients/private/orgs/home/OrgDashboardPage/DashboardItemListLayout';
+import {EmptyTableLayout} from '^clients/private/orgs/home/OrgDashboardPage/EmptyTableLayout';
 
-interface PaymentMethodsSectionSectionProps {}
+interface PaymentMethodsSectionSectionProps {
+    currentOrg: OrganizationDto | null;
+}
 
 export const PaymentMethodsSection = (props: PaymentMethodsSectionSectionProps) => {
+    const orgId = useRecoilValue(orgIdParamState);
+    const router = useRouter();
+    const {data: paymentMethods, isLoading} = usePaymentMethodListInDashboard(orgId);
+
+    const showCardNumber = (first: string, second: string) => {
+        const secondNumber = second.slice(0, 2);
+        const privateMask = '****';
+        return `${first}-${secondNumber}**-${privateMask}-${privateMask}`;
+    };
+
+    if (!paymentMethods) return <EmptyTableLayout title="결제수단" Icon={() => <GoCreditCard />} />;
     return (
-        <DashboardLayout title="결제수단" subTitle="총 -2,500,000원">
+        <DashboardLayout
+            title="결제수단"
+            subTitle={
+                paymentMethods.total.payAmountSum
+                    ? `-${currencyFormat(paymentMethods.total.payAmountSum)}`
+                    : currencyFormat(paymentMethods.total.payAmountSum)
+            }
+        >
             <section className="w-full flex flex-col gap-10">
                 <ul>
-                    <li className="first:border-t-0 last:border-b-0 border-t-[0.5px] border-b-[0.5px] p-4">
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center gap-2">
-                                <Avatar className="w-7 h-7" draggable={false} loading="lazy" />
-                                <div>
-                                    <p className="font-normal text-14 text-gray-800">
-                                        [KB국민카드] 5582_26**_****_****
-                                    </p>
-                                    <p className="font-normal text-12 text-gray-400">끝자리: 0000</p>
-                                </div>
-                            </div>
-                            <p className="font-medium text-16 text-gray-900">- 1,500,000원</p>
-                        </div>
-                    </li>
-                    <li className="first:border-t-0 last:border-b-0 border-t-[0.5px] border-b-[0.5px] p-4">
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center gap-2">
-                                <Avatar className="w-7 h-7" draggable={false} loading="lazy" />
-                                <div>
-                                    <p className="font-normal text-14 text-gray-800">
-                                        [KB국민카드] 5582_26**_****_****
-                                    </p>
-                                    <p className="font-normal text-12 text-gray-400">끝자리: 0000</p>
-                                </div>
-                            </div>
-                            <p className="font-medium text-16 text-gray-900">- 1,000,000원</p>
-                        </div>
-                    </li>
-                    <li className="first:border-t-0 last:border-b-0 border-t-[0.5px] border-b-[0.5px] p-4">
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center gap-2">
-                                <Avatar className="w-7 h-7" draggable={false} loading="lazy" />
-                                <div>
-                                    <p className="font-normal text-14 text-gray-800">
-                                        [KB국민카드] 5582_26**_****_****
-                                    </p>
-                                    <p className="font-normal text-12 text-gray-400">끝자리: 0000</p>
-                                </div>
-                            </div>
-                            <p className="font-medium text-16 text-gray-900">- 1,000,000원</p>
-                        </div>
-                    </li>
+                    {paymentMethods?.items.map((item) => (
+                        <DashboardItemListLayout
+                            key={item.id}
+                            url={`${orgId}/creditCards/${item.creditCard?.id}`}
+                            src={item.creditCard?.company?.logo || ''}
+                            avatarClassName="w-7 h-7"
+                            Icon={() => <GoCreditCard />}
+                            title={`[${item.creditCard?.profileName}] ${showCardNumber(
+                                item.creditCard?.secretInfo?.number1 || '****',
+                                item.creditCard?.secretInfo.number2 || '****',
+                            )}`}
+                            subTitle={`끝자리: ${item.creditCard?.secretInfo.number4}`}
+                            message={
+                                item.payAmountSum
+                                    ? `-${currencyFormat(item.payAmountSum)}`
+                                    : currencyFormat(item.payAmountSum)
+                            }
+                        />
+                    ))}
                 </ul>
-                <button className="w-full flex items-center justify-center font-semibold text-14 text-gray-400">
+                <button
+                    onClick={() => router.push(`${orgId}/creditCards`)}
+                    className="w-full flex items-center justify-center font-semibold text-14 text-gray-400"
+                >
                     전체보기
                 </button>
             </section>
