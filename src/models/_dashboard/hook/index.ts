b1bf2164a53a8939d47625b2a008e542959a9d1e
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-02 17:55:18 +0900

    대시보드p / 결제 수단 섹션 워크스페이스 결제수단 목록 hook 구현

---
+++ src/models/_dashboard/hook/index.ts
@@ -0,0 +1,20 @@
+//대시보드 - 결제수단 섹션에서 워크스페이스 결제수단 목록 불러오기
+import {FromToQueryDto} from '^types/billing.type';
+import {useQuery} from '@tanstack/react-query';
+import {dashboardApi} from '^models/_dashboard/api';
+import {getToday} from '^utils/dateTime';
+
+export const usePaymentMethodListInDashboard = (orgId: number, params?: FromToQueryDto) => {
+    const defaultFrom = new Date(getToday().getFullYear(), getToday().getMonth(), 1, 12);
+
+    const finalParams: FromToQueryDto = {
+        from: params?.from || defaultFrom,
+        to: params?.to || getToday(),
+    };
+
+    return useQuery({
+        queryKey: ['paymentMethods', orgId],
+        queryFn: () => dashboardApi.creditCardsSection(orgId, finalParams).then((res) => res.data),
+        enabled: !!orgId || !isNaN(orgId),
+    });
+};
