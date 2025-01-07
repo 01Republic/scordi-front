510780cfab57591c685ceda800d508755d081ec6
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-07 14:56:00 +0900

    대시보드p - 개별 바로가기 버튼을 컴포넌트 페이지로 분리, currentOrg props 제거

--- src/clients/private/orgs/home/OrgDashboardPage/index.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/index.tsx
@@ -1,40 +1,23 @@
 import React from 'react';
-import {useRecoilValue} from 'recoil';
-import {HiOutlineSquaresPlus} from 'react-icons/hi2';
-import {GoCreditCard, GoMail} from 'react-icons/go';
-import {AiOutlineUserAdd} from 'react-icons/ai';
-import {currentOrgAtom} from '^models/Organization/atom';
-import {OrgSubscriptionSelectPageRoute} from '^pages/orgs/[id]/subscriptions/select';
 import {MainLayout} from '^clients/private/_layouts/MainLayout';
-import {QuickButton} from '^clients/private/orgs/home/OrgDashboardPage/QuickButton';
+import {QuickButtonSection} from './QuickButtonSection';
 import {MonthlyTotalExpenseSection} from './MonthlyTotalExpenseSection';
 import {PaymentMethodsSection} from './PaymentMethodsSection';
 import {InvoiceAccountsSection} from './InvoiceAccountsSection';
-import {YearlySection} from '^clients/private/orgs/home/OrgDashboardPage/YearlySection';
+import {YearlySection} from './YearlySection';
 
 export const OrgDashboardPage = () => {
-    const currentOrg = useRecoilValue(currentOrgAtom);
-
     return (
         <MainLayout>
             <section className="container px-4 flex flex-col gap-10">
                 <div className="flex items-center justify-between mt-10">
                     <h5 className="font-bold text-24">대시보드</h5>
-                    <div className="flex gap-3">
-                        <QuickButton
-                            text="구독 추가"
-                            Icon={() => <HiOutlineSquaresPlus />}
-                            url={currentOrg ? OrgSubscriptionSelectPageRoute.path(currentOrg.id) : '#'}
-                        />
-                        <QuickButton text="결제수단 추가" Icon={() => <GoCreditCard />} />
-                        <QuickButton text="청구서 메일 추가" Icon={() => <GoMail />} />
-                        <QuickButton text="구성원 추가" Icon={() => <AiOutlineUserAdd />} />
-                    </div>
+                    <QuickButtonSection />
                 </div>
-                <MonthlyTotalExpenseSection currentOrg={currentOrg} />
+                <MonthlyTotalExpenseSection />
                 <div className="flex gap-5">
                     <PaymentMethodsSection />
-                    <InvoiceAccountsSection currentOrg={currentOrg} />
+                    <InvoiceAccountsSection />
                 </div>
                 <YearlySection />
             </section>
