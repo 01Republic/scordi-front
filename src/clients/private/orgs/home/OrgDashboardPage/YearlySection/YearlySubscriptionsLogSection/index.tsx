256002cc5a30d6217ca751874b9f03ce0f7b276c
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-07 12:20:49 +0900

    대시보드p / 올해의 구독 현황 섹션 페이지 구현

---
+++ src/clients/private/orgs/home/OrgDashboardPage/YearlySection/YearlySubscriptionsLogSection/index.tsx
@@ -0,0 +1,36 @@
+import {DashboardLayout} from '^clients/private/orgs/home/OrgDashboardPage/DashboardLayout';
+import {BarGraph} from '^clients/private/orgs/home/OrgDashboardPage/YearlySection/YearlySubscriptionsLogSection/BarGraph';
+import {BillingHistoriesMonthlySumItemDto} from '^models/BillingHistory/type';
+import {memo} from 'react';
+
+interface YearlySubscriptionsLogSectionProps {
+    monthsPayLog: {month: string; items: BillingHistoriesMonthlySumItemDto[] | undefined}[];
+}
+
+export const YearlySubscriptionsLogSection = memo((props: YearlySubscriptionsLogSectionProps) => {
+    const {monthsPayLog} = props;
+
+    return (
+        <DashboardLayout title="올해의 구독 현황" className="!w-2/3">
+            <section className="w-full flex flex-col gap-10">
+                <div className="flex gap-5">
+                    <section className="w-full flex flex-col gap-3 border rounded-xl p-5">
+                        <div className="flex items-center gap-2">
+                            <div className="w-2 h-2 bg-blue-800 rounded-full" />
+                            <p>2024년, 오늘 기준 구독 지출액</p>
+                        </div>
+                        <p className="font-bold text-28">5,000,000원</p>
+                    </section>
+                    <section className="w-full flex flex-col gap-3 border rounded-xl p-5">
+                        <div className="flex items-center gap-2">
+                            <div className="w-2 h-2 bg-pink-200 rounded-full" />
+                            <p>2024년, 예상 구독 지출액</p>
+                        </div>
+                        <p className="font-bold text-28">10,000,000원</p>
+                    </section>
+                </div>
+                <BarGraph monthsPayLog={monthsPayLog} />
+            </section>
+        </DashboardLayout>
+    );
+});
