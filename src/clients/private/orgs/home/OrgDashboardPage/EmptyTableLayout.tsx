ffe004a2be721e578f583c04469d29465913cbb0
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-09 15:59:37 +0900

    대시보드p/ 빈테이블 내부 버튼 함수 또는 url을 받을 수 있도록 변경

--- src/clients/private/orgs/home/OrgDashboardPage/EmptyTableLayout.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/EmptyTableLayout.tsx
@@ -8,20 +8,36 @@ import {LoadableBox} from '^components/util/loading';
 interface ListItemLayoutProps {
     title: string;
     Icon: React.ElementType;
+    className?: string;
+    onClick?: () => void;
+    url?: string;
 }
 
 export const EmptyTableLayout = memo((props: ListItemLayoutProps) => {
-    const {title, Icon} = props;
+    const {title, Icon, className, onClick, url} = props;
 
     return (
-        <article className={`w-full flex flex-col justify-start border rounded-3xl bg-white p-7`}>
+        <article className={`w-full flex flex-col justify-start border rounded-3xl bg-white p-7 ${className}`}>
             <p className="flex items-center font-semibold text-20">{title}</p>
-            <section className="w-full h-full flex flex-col gap-3 items-center justify-center">
+            <section className=" h-full flex flex-col gap-3 items-center justify-center">
                 <p className="text-sm font-normal text-neutral-500">{`등록된 ${title}이 없어요.`}</p>
-                <button className="flex items-center gap-2 p-3 text-neutral-50 btn-scordi rounded-lg">
-                    <Icon className="w-[10px]" />
-                    <p className="text-14 font-medium">{`${title} 등록`}</p>
-                </button>
+                {url ? (
+                    <Link
+                        href={url ? url : '#'}
+                        className="flex items-center gap-2 p-3 text-neutral-50 btn-scordi rounded-lg"
+                    >
+                        <Icon className="w-[10px]" />
+                        <p className="text-14 font-medium">{`${title} 등록`}</p>
+                    </Link>
+                ) : (
+                    <button
+                        onClick={onClick}
+                        className="flex items-center gap-2 p-3 text-neutral-50 btn-scordi rounded-lg"
+                    >
+                        <Icon className="w-[10px]" />
+                        <p className="text-14 font-medium">{`${title} 등록`}</p>
+                    </button>
+                )}
             </section>
         </article>
     );
