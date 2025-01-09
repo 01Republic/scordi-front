60bf0c8c4b229cbb7b3e15d5c762f6323b24dbed
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-01-09 13:04:55 +0900

    대시보드 / 청구서계정 섹션 데이터 조회 API 추가

--- src/models/_dashboard/api.ts
+++ src/models/_dashboard/api.ts
@@ -1,6 +1,6 @@
 import {api} from '^api/api';
 import {FromToQueryDto} from '^types/billing.type';
-import {DashboardCreditCardsSectionResultDto} from '^models/_dashboard/type';
+import {DashboardCreditCardsSectionResultDto, DashboardInvoiceAccountsSectionResultDto} from '^models/_dashboard/type';
 import {oneDtoOf} from '^types/utils/response-of';
 
 export const dashboardApi = {
@@ -9,5 +9,12 @@ export const dashboardApi = {
         const url = `/dashboard/${orgId}/sections/credit-cards`;
         const ResultDto = DashboardCreditCardsSectionResultDto;
         return api.get<DashboardCreditCardsSectionResultDto>(url, {params}).then(oneDtoOf(ResultDto));
+    },
+
+    // 청구서계정 섹션 데이터 조회
+    invoiceAccountsSection(orgId: number, params: FromToQueryDto) {
+        const url = `/dashboard/${orgId}/sections/invoice-accounts`;
+        const ResultDto = DashboardInvoiceAccountsSectionResultDto;
+        return api.get<DashboardInvoiceAccountsSectionResultDto>(url, {params}).then(oneDtoOf(ResultDto));
     },
 };
--- src/models/_dashboard/type/index.ts
+++ src/models/_dashboard/type/index.ts
@@ -1,1 +1,2 @@
 export * from './dashboard.credit-cards.section.result.dto';
+export * from './dashboard.invoice-accounts.section.result.dto';
