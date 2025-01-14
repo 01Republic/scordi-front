9eee7f6414317b70c96a367dab586009be8deaaa
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-01-14 12:20:31 +0900

    대시보드 / 레이아웃에 공통 컨테이너 추가

--- src/clients/private/orgs/home/OrgDashboardPage/index.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MainLayout} from '^clients/private/_layouts/MainLayout';
+import {MainContainer, MainLayout} from '^clients/private/_layouts/MainLayout';
 import {QuickButtonSection} from './QuickButtonSection';
 import {MonthlyTotalExpenseSection} from './MonthlyTotalExpenseSection';
 import {PaymentMethodsSection} from './PaymentMethodsSection';
@@ -9,18 +9,20 @@ import {YearlySection} from './YearlySection';
 export const OrgDashboardPage = () => {
     return (
         <MainLayout>
-            <section className="container px-4 flex flex-col gap-10">
-                <div className="flex items-center justify-between mt-10">
-                    <h5 className="font-bold text-24">대시보드</h5>
-                    <QuickButtonSection />
+            <MainContainer>
+                <div className="flex flex-col gap-10">
+                    <div className="flex items-center justify-between mt-10">
+                        <h5 className="font-bold text-24">대시보드</h5>
+                        <QuickButtonSection />
+                    </div>
+                    <MonthlyTotalExpenseSection />
+                    <div className="flex gap-5">
+                        <PaymentMethodsSection />
+                        <InvoiceAccountsSection />
+                    </div>
+                    <YearlySection />
                 </div>
-                <MonthlyTotalExpenseSection />
-                <div className="flex gap-5">
-                    <PaymentMethodsSection />
-                    <InvoiceAccountsSection />
-                </div>
-                <YearlySection />
-            </section>
+            </MainContainer>
         </MainLayout>
     );
 };
