330b8b646727545d2265e02dce35fc5b40093659
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-15 11:59:53 +0900

    대시보드p / 올해의 구독 현황 섹션 hook 구현

--- src/models/BillingSchedule/hook/index.ts
+++ src/models/BillingSchedule/hook/index.ts
@@ -1,16 +1,15 @@
+import {useState} from 'react';
 import {RecoilState, useRecoilState, useRecoilValue} from 'recoil';
 import {useQuery} from '@tanstack/react-query';
 import {getBillingSchedules} from '^models/BillingSchedule/api';
+import {dashboardApi} from '^models/_dashboard/api';
 import {
     getBillingSchedulesQuery,
     orgBillingSchedulesQueryV3Atom,
     orgBillingSchedulesResultV3Atom,
 } from '^models/BillingSchedule/atom';
 import {Paginated} from '^types/utils/paginated.dto';
-import {useState} from 'react';
 import {BillingScheduleShallowDto as ScheduleDto, GetBillingSchedulesParams} from '^models/BillingSchedule/type';
-import {billingHistoryApi} from '^models/BillingHistory/api';
-import {GetBillingHistoriesParams} from '^models/BillingHistory/type';
 
 export const useBillingSchedules = () => useRecoilValue(getBillingSchedulesQuery);
 
@@ -42,17 +41,12 @@ export const useBillingSchedulesV3 = (option?: UseBillingSchedulesOption) => {
 
 // 대시보드 - 올해의 구독 현황 섹션 월 별 구독 예상 금액 리스트 불러오기
 export const useYearlySubscriptionScheduleLogInDashboard = (orgId: number) => {
-    const getThisYear = new Date().getFullYear();
-    // const getThisYear = 2024;
+    // const year = new Date().getFullYear();
+    const year = 2024;
 
-    const query: GetBillingSchedulesParams = {
-        where: {organizationId: orgId},
-        startDate: `${String(getThisYear)}`,
-        endDate: `${String(getThisYear)}`,
-    };
     return useQuery({
-        queryKey: ['monthlySubscriptionList', orgId],
-        queryFn: () => getBillingSchedules({...query, order: {billingDate: 'ASC'}}).then((res) => res.data),
+        queryKey: ['summaryYearMonthlyData', orgId],
+        queryFn: () => dashboardApi.summaryYearMonthly(orgId, year).then((res) => res.data),
         enabled: !!orgId || !isNaN(orgId),
     });
 };
