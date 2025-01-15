78d8ada6ec7127dcc3d7ee3c296d85e49ab93ae3
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-15 12:03:01 +0900

    대시보드p / 올해의 구독 현황 섹션 - 오늘 기준 구독액, 예상 구독 지출액 데이터 사용

--- src/clients/private/orgs/home/OrgDashboardPage/YearlySection/YearlySubscriptionsLogSection/index.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/YearlySection/YearlySubscriptionsLogSection/index.tsx
@@ -3,12 +3,19 @@ import {BarGraph} from '^clients/private/orgs/home/OrgDashboardPage/YearlySectio
 import {BillingHistoriesMonthlySumItemDto} from '^models/BillingHistory/type';
 import {memo} from 'react';
 
+import {useRecoilValue} from 'recoil';
+import {displayCurrencyAtom} from '^tasting/pageAtoms';
+import {currencyFormat, roundNumber} from '^utils/number';
+import {DashboardSummaryYearMonthlyResultDto} from '^models/_dashboard/type';
+
 interface YearlySubscriptionsLogSectionProps {
     monthsHistoriesLog: {month: string; items: BillingHistoriesMonthlySumItemDto[] | undefined}[];
+    monthlyBillingSchedule: DashboardSummaryYearMonthlyResultDto | undefined;
 }
 
 export const YearlySubscriptionsLogSection = memo((props: YearlySubscriptionsLogSectionProps) => {
-    const {monthsHistoriesLog} = props;
+    const {monthsHistoriesLog, monthlyBillingSchedule} = props;
+    const getThisYear = new Date().getFullYear();
 
     return (
         <DashboardLayout title="올해의 구독 현황" className="!w-2/3">
@@ -17,19 +24,27 @@ export const YearlySubscriptionsLogSection = memo((props: YearlySubscriptionsLog
                     <section className="w-full flex flex-col gap-3 border rounded-xl p-5">
                         <div className="flex items-center gap-2">
                             <div className="w-2 h-2 bg-blue-800 rounded-full" />
-                            <p>2024년, 오늘 기준 구독 지출액</p>
+                            <p>{`${getThisYear}년, 오늘 기준 구독 지출액`}</p>
                         </div>
-                        <p className="font-bold text-28">5,000,000원</p>
+                        <p className="font-bold text-28">
+                            {monthlyBillingSchedule
+                                ? currencyFormat(roundNumber(monthlyBillingSchedule?.didPayAmount))
+                                : '0원'}
+                        </p>
                     </section>
                     <section className="w-full flex flex-col gap-3 border rounded-xl p-5">
                         <div className="flex items-center gap-2">
                             <div className="w-2 h-2 bg-pink-200 rounded-full" />
-                            <p>2024년, 예상 구독 지출액</p>
+                            <p>{`${getThisYear}년, 예상 구독 지출액`}</p>
                         </div>
-                        <p className="font-bold text-28">10,000,000원</p>
+                        <p className="font-bold text-28">
+                            {monthlyBillingSchedule
+                                ? currencyFormat(roundNumber(monthlyBillingSchedule?.willPayAmount))
+                                : '0원'}
+                        </p>
                     </section>
                 </div>
-                <BarGraph monthsHistoriesLog={monthsHistoriesLog} />
+                <BarGraph monthsHistoriesLog={monthsHistoriesLog} monthlyBillingSchedule={monthlyBillingSchedule} />
             </section>
         </DashboardLayout>
     );
