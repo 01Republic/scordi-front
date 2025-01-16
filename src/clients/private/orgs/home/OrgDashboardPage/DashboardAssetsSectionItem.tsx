9e3b6266723421a5471ad8da32900bb56ebe5601
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-16 16:48:45 +0900

    대시보드p / DashboardAssetsSectionItem - 불필요한 props 제거

--- src/clients/private/orgs/home/OrgDashboardPage/DashboardAssetsSectionItem.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/DashboardAssetsSectionItem.tsx
@@ -6,50 +6,18 @@ import {Avatar} from '^components/Avatar';
 interface ListItemLayoutProps {
     url: string;
     message: string;
-    src?: string;
-    ProfileContent?: React.ElementType;
-    Icon?: React.ElementType;
-    avatarClassName?: string;
-    title?: string;
-    subTitle?: string;
+    ProfileContent: React.ElementType;
+    className?: string;
 }
 
 export const DashboardAssetsSectionItem = memo((props: ListItemLayoutProps) => {
-    const {url, src, avatarClassName, title, subTitle, message, Icon, ProfileContent} = props;
+    const {url, message, ProfileContent, className = ''} = props;
 
     return (
-        <li className="first:border-t-0 last:border-b-0 border-t-[0.5px] border-b-[0.5px] p-4">
+        <li className={`first:border-t-0 last:border-b-0 border-t-[0.5px] border-b-[0.5px] px-4 ${className}`}>
             <Link href={url}>
                 <div className="flex items-center justify-between">
-                    {ProfileContent ? (
-                        <ProfileContent />
-                    ) : (
-                        <div className="flex items-center gap-2">
-                            {Icon ? (
-                                <div className="w-7 h-7 rounded-full text-scordi flex items-center justify-center border border-scordi-light">
-                                    <Icon />
-                                </div>
-                            ) : (
-                                <>
-                                    {src ? (
-                                        <Avatar
-                                            src={src}
-                                            className={avatarClassName}
-                                            draggable={false}
-                                            loading="lazy"
-                                        />
-                                    ) : (
-                                        <Avatar className={avatarClassName} draggable={false} loading="lazy" />
-                                    )}
-                                </>
-                            )}
-
-                            <div className="flex-col">
-                                <p className="font-normal text-14 text-gray-800">{title}</p>
-                                <p className="font-normal text-12 text-gray-400">{subTitle}</p>
-                            </div>
-                        </div>
-                    )}
+                    <ProfileContent />
                     <p className="font-medium text-16 text-gray-900">{message}</p>
                 </div>
             </Link>
