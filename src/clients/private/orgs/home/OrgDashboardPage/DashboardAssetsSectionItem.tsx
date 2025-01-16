7f26db28cfb7b451bc52878101f774ad8c63190a
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-01-16 15:06:30 +0900

    대시보드p / DashboardAssetsSectionItem에서 프로필을 컴포넌트로 받을 수 있도록 변경

--- src/clients/private/orgs/home/OrgDashboardPage/DashboardAssetsSectionItem.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/DashboardAssetsSectionItem.tsx
@@ -6,6 +6,7 @@ import {Avatar} from '^components/Avatar';
 interface ListItemLayoutProps {
     url: string;
     src: string;
+    ProfileContent?: React.ElementType;
     Icon?: React.ElementType;
     avatarClassName?: string;
     title: string;
@@ -15,37 +16,46 @@ interface ListItemLayoutProps {
 }
 
 export const DashboardAssetsSectionItem = memo((props: ListItemLayoutProps) => {
-    const {url, src, avatarClassName, title, subTitle, message, Icon, low = false} = props;
+    const {url, src, avatarClassName, title, subTitle, message, Icon, low = false, ProfileContent} = props;
 
     return (
         <li className="first:border-t-0 last:border-b-0 border-t-[0.5px] border-b-[0.5px] p-4">
             <Link href={url}>
                 <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2">
-                        {Icon ? (
-                            <div className="w-7 h-7 rounded-full text-scordi flex items-center justify-center border border-scordi-light">
-                                <Icon />
-                            </div>
-                        ) : (
-                            <>
-                                {src ? (
-                                    <Avatar src={src} className={avatarClassName} draggable={false} loading="lazy" />
-                                ) : (
-                                    <Avatar className={avatarClassName} draggable={false} loading="lazy" />
-                                )}
-                            </>
-                        )}
+                    {ProfileContent ? (
+                        <ProfileContent />
+                    ) : (
+                        <div className="flex items-center gap-2">
+                            {Icon ? (
+                                <div className="w-7 h-7 rounded-full text-scordi flex items-center justify-center border border-scordi-light">
+                                    <Icon />
+                                </div>
+                            ) : (
+                                <>
+                                    {src ? (
+                                        <Avatar
+                                            src={src}
+                                            className={avatarClassName}
+                                            draggable={false}
+                                            loading="lazy"
+                                        />
+                                    ) : (
+                                        <Avatar className={avatarClassName} draggable={false} loading="lazy" />
+                                    )}
+                                </>
+                            )}
 
-                        <div
-                            className={cn('flex', {
-                                'flex-row items-center gap-3': low,
-                                'flex-col': !low,
-                            })}
-                        >
-                            <p className="font-normal text-14 text-gray-800">{title}</p>
-                            <p className="font-normal text-12 text-gray-400">{subTitle}</p>
+                            <div
+                                className={cn('flex', {
+                                    'flex-row items-center gap-3': low,
+                                    'flex-col': !low,
+                                })}
+                            >
+                                <p className="font-normal text-14 text-gray-800">{title}</p>
+                                <p className="font-normal text-12 text-gray-400">{subTitle}</p>
+                            </div>
                         </div>
-                    </div>
+                    )}
                     <p className="font-medium text-16 text-gray-900">{message}</p>
                 </div>
             </Link>
