443bdac1760d40d5f8dbf67a6e20df1e2c81ad7f
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-01-16 17:17:15 +0900

    대시보드 / 대시보드 전용 버튼 CSS 클래스 생성

--- src/clients/private/orgs/home/OrgDashboardPage/QuickButtonSection/QuickButton.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/QuickButtonSection/QuickButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
-import cn from 'classnames';
 import {WithChildren} from '^types/global.type';
+import {LinkTo} from '^components/util/LinkTo';
 
 interface QuickButtonProps extends WithChildren {
     text: string;
@@ -14,24 +13,9 @@ export const QuickButton = (props: QuickButtonProps) => {
     const {text, url, Icon, onClick} = props;
 
     return (
-        <>
-            {url ? (
-                <Link
-                    href={url ? url : '#'}
-                    className="flex items-center gap-2 px-3 py-2 text-scordi border border-scordi-light rounded-lg hover:bg-scordi-50 hover:border-scordi-300"
-                >
-                    <Icon />
-                    <p className="font-semibold">{text}</p>
-                </Link>
-            ) : (
-                <button
-                    onClick={onClick}
-                    className="flex items-center gap-2 px-3 py-2 text-scordi border border-scordi-light rounded-lg hover:bg-scordi-50 hover:border-scordi-300"
-                >
-                    <Icon />
-                    <p className="font-semibold">{text}</p>
-                </button>
-            )}
-        </>
+        <LinkTo href={url} onClick={onClick} className="btn-dashboard">
+            <Icon />
+            <p className="font-semibold">{text}</p>
+        </LinkTo>
     );
 };
