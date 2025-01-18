a78561013d0b8796c3c783e2033fb66e1d887cee
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-01-18 17:42:16 +0900

    대시보드 / 대시보드 섹션 레이아웃에 Plus 버튼 기능 추가

--- src/clients/private/orgs/home/OrgDashboardPage/DashboardSectionLayout.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/DashboardSectionLayout.tsx
@@ -1,6 +1,7 @@
 import {memo} from 'react';
 import {WithChildren} from '^types/global.type';
 import {LoadableBox} from '^components/util/loading';
+import {FaPlus} from 'react-icons/fa6';
 
 interface DashboardLayoutProps extends WithChildren {
     title: string;
@@ -8,17 +9,29 @@ interface DashboardLayoutProps extends WithChildren {
     subTitle?: string;
     isLoading?: boolean;
     Buttons?: () => JSX.Element;
+    plusButtonClick?: () => any;
 }
 
 export const DashboardSectionLayout = memo((props: DashboardLayoutProps) => {
-    const {title, className = '', subTitle, Buttons, isLoading = false, children} = props;
+    const {title, className = '', subTitle, Buttons, plusButtonClick, isLoading = false, children} = props;
 
     return (
         <section
             className={`w-full flex flex-col justify-start gap-5 border rounded-3xl bg-white p-6 max-h-[850px] overflow-hidden px-4 ${className}`}
         >
             <div className="flex items-center justify-between">
-                <h6 className="font-semibold text-20">{title}</h6>
+                <div className="flex items-center gap-2">
+                    <h6 className="font-semibold text-20">{title}</h6>
+
+                    {plusButtonClick && (
+                        <button
+                            onClick={plusButtonClick}
+                            className="btn btn-ghost btn-square btn-xs text-gray-400 hover:text-black"
+                        >
+                            <FaPlus />
+                        </button>
+                    )}
+                </div>
                 {subTitle && <p className="font-semibold text-[22px]">{subTitle}</p>}
                 {Buttons && <Buttons />}
             </div>
