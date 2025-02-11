63dffdc91ba0d7fc146623cc237980935cc7a664
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-02-11 12:07:10 +0900

    [구독상세p] 구독 프로필 섹션 > 구독 이미지 없는 경우 아이콘 변경

--- src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionProfilePanel/index.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionProfilePanel/index.tsx
@@ -1,8 +1,8 @@
 import React, {memo} from 'react';
-import {Avatar} from '^components/Avatar';
-import {FaRegCreditCard} from 'react-icons/fa6';
 import {SubscriptionUsingStatusTag} from '^models/Subscription/components';
 import {SubscriptionDto} from '^models/Subscription/types';
+import {Avatar} from '^components/Avatar';
+import {FaQuestion} from 'react-icons/fa6';
 
 interface SubscriptionProfilePanelProps {
     subscription: SubscriptionDto;
@@ -10,23 +10,22 @@ interface SubscriptionProfilePanelProps {
 
 export const SubscriptionProfilePanel = memo((props: SubscriptionProfilePanelProps) => {
     const {subscription} = props;
+    const {product} = subscription;
 
     return (
         <div className="flex items-start gap-6">
-            <Avatar className="w-14 h-14" src={subscription.product.image} alt={subscription.product.name()}>
-                <FaRegCreditCard size={20} className="h-full w-full p-[6px]" />
+            <Avatar className="w-14 h-14" src={product.image} alt={product.name()} draggable={false} loading="lazy">
+                <FaQuestion size={24} className="text-gray-300 h-full w-full p-[6px]" />
             </Avatar>
-
             <div className="flex flex-col gap-0.5 overflow-hidden text-left">
                 <p className="flex gap-2 text-18 font-semibold items-center group-hover:text-scordi leading-none py-1">
-                    <span className="truncate">{subscription.product.name()}</span>
+                    <span className="truncate">{product.name()}</span>
                 </p>
                 {subscription.alias && (
                     <p className="block text-14 font-normal text-gray-400 group-hover:text-scordi-300 leading-none">
                         {subscription.alias}
                     </p>
                 )}
-
                 <div className="flex items-center gap-3 pt-3">
                     <SubscriptionUsingStatusTag
                         value={subscription.usingStatus}
