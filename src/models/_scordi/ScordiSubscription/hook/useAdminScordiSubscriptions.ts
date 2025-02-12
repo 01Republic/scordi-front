a4903795203d2d54af867de39a62968835be494c
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-02-12 15:49:21 +0900

    어드민 / 스코디 빌링 구독관리 목록 페이지 추가

--- src/models/_scordi/ScordiSubscription/hook/useAdminScordiSubscriptions.ts
+++ src/models/_scordi/ScordiSubscription/hook/useAdminScordiSubscriptions.ts
@@ -2,13 +2,23 @@ import {useQuery} from '@tanstack/react-query';
 import {adminScordiSubscriptionsApi} from '^models/_scordi/ScordiSubscription/api';
 import {Paginated} from '^types/utils/paginated.dto';
 import {FindAllScordiSubscriptionsForAdminDto} from '^models/_scordi/ScordiSubscription/type';
+import {useState} from 'react';
 
 export const useAdminScordiSubscriptions = (params: FindAllScordiSubscriptionsForAdminDto = {}) => {
-    const query = useQuery({
-        queryKey: ['useAdminScordiSubscriptionList', params],
-        queryFn: () => adminScordiSubscriptionsApi.index(params).then((res) => res.data),
+    const [query, setQuery] = useState<FindAllScordiSubscriptionsForAdminDto>(params);
+    const queryResult = useQuery({
+        queryKey: ['useAdminScordiSubscriptionList', query],
+        queryFn: () => adminScordiSubscriptionsApi.index(query).then((res) => res.data),
         initialData: Paginated.init(),
     });
 
-    return {...query};
+    const search = setQuery;
+    const clearQuery = () => search(params);
+
+    return {
+        ...queryResult,
+        params: query,
+        search: setQuery,
+        clearQuery,
+    };
 };
