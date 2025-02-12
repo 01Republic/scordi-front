f4ecec8639d9ae895a28c381140cbc28e113b8a0
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-02-12 16:55:57 +0900

    대시보드p / 구독리스트 섹션 - 구독 카드 클릭 시 구독상세로 이동

--- src/clients/private/orgs/home/OrgDashboardPage/YearlySection/YearMonthlySubscriptionsSection/PaidSubscriptionSpendItem.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/YearlySection/YearMonthlySubscriptionsSection/PaidSubscriptionSpendItem.tsx
@@ -3,6 +3,8 @@ import {Avatar} from '^components/Avatar';
 import {TbLayoutGrid} from 'react-icons/tb';
 import {SubscriptionDto} from '^models/Subscription/types';
 import {currencyFormat, roundNumber} from '^utils/number';
+import {LinkTo} from '^components/util/LinkTo';
+import {OrgSubscriptionDetailPageRoute} from '^pages/orgs/[id]/subscriptions/[subscriptionId]';
 
 interface PaidSubscriptionSpendItemProps {
     amount: number;
@@ -14,18 +16,23 @@ export const PaidSubscriptionSpendItem = memo((props: PaidSubscriptionSpendItemP
     const {product} = subscription;
 
     return (
-        <li className="w-full py-5 flex items-center justify-between">
-            <div className="flex items-center gap-3 font-medium text-16 text-neutral-900 whitespace-nowrap">
-                {product.image ? (
-                    <Avatar src={product.image} className="w-5 h-5" draggable={false} loading="lazy" />
-                ) : (
-                    <div className="w-5 h-5 rounded-full text-scordi flex items-center justify-center border border-scordi-light">
-                        <TbLayoutGrid />
-                    </div>
-                )}
-                <p>{product.name()}</p>
-            </div>
-            <p className="whitespace-nowrap"> {amount > 0 ? currencyFormat(roundNumber(amount) * -1) : '-'}</p>
+        <li className="w-full">
+            <LinkTo
+                href={OrgSubscriptionDetailPageRoute.path(subscription.organizationId, subscription.id)}
+                className="w-full py-5 flex items-center justify-between"
+            >
+                <div className="flex items-center gap-3 font-medium text-16 text-neutral-900 whitespace-nowrap">
+                    {product.image ? (
+                        <Avatar src={product.image} className="w-5 h-5" draggable={false} loading="lazy" />
+                    ) : (
+                        <div className="w-5 h-5 rounded-full text-scordi flex items-center justify-center border border-scordi-light">
+                            <TbLayoutGrid />
+                        </div>
+                    )}
+                    <p>{product.name()}</p>
+                </div>
+                <p className="whitespace-nowrap"> {amount > 0 ? currencyFormat(roundNumber(amount) * -1) : '-'}</p>
+            </LinkTo>
         </li>
     );
 });
