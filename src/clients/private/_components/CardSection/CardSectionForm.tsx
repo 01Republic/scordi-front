5779984682ee61ca3bf18f38458e9d28522b01b7
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-02-19 16:19:01 +0900

    구독상세p/CardSectionEditButton 제거..!!

--- src/clients/private/_components/CardSection/CardSectionForm.tsx
+++ src/clients/private/_components/CardSection/CardSectionForm.tsx
@@ -1,6 +1,6 @@
 import React, {FormEventHandler, memo} from 'react';
 import {WithChildren} from '^types/global.type';
-import {CardSectionEditButton} from './CardSectionEditButton';
+import cn from 'classnames';
 
 interface CardSectionFormProps extends WithChildren {
     title: string;
@@ -8,20 +8,23 @@ interface CardSectionFormProps extends WithChildren {
     setIsEditMode: (isEditMode: boolean) => void;
     onSubmit?: FormEventHandler<HTMLFormElement>;
     isButtonText?: boolean;
+    isSaving: boolean;
 }
 
 export const CardSectionForm = memo((props: CardSectionFormProps) => {
-    const {title, isEditMode, setIsEditMode, onSubmit, isButtonText = false, children} = props;
+    const {title, isEditMode, setIsEditMode, onSubmit, isButtonText = false, children, isSaving} = props;
 
     return (
         <form onSubmit={onSubmit}>
             <div className="flex items-start justify-between">
                 <h2 className="leading-none text-xl font-semibold ">{title}</h2>
-                <CardSectionEditButton
-                    isEditMode={isEditMode}
-                    setIsEditMode={setIsEditMode}
-                    isButtonText={isButtonText}
-                />
+                <div className="absolute right-0 top-0 px-8 py-8 flex items-center gap-4">
+                    <a className="link text-14" onClick={() => setIsEditMode(!isEditMode)}>
+                        {isEditMode ? '취소' : isButtonText ? '추가' : '수정'}
+                    </a>
+
+                    {isEditMode && <button className={cn('btn btn-sm btn-scordi', {loading: isSaving})}>저장</button>}
+                </div>
             </div>
             {!isButtonText ? (
                 <div className="max-w-md flex flex-col gap-4 pt-8">{children}</div>
