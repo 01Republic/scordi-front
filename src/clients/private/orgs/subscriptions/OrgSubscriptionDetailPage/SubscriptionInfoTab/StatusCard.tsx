8277738bb78d22ecb9d9b73e55f25651e5cdb67e
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-02-21 19:38:46 +0900

    구독상세p/상태카드 스타일 수정

--- src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionInfoTab/StatusCard.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionInfoTab/StatusCard.tsx
@@ -11,11 +11,11 @@ export const StatusCard = memo((props: StatusCardProps) => {
     const {title, titleValue, icon, iconColor} = props;
     return (
         <div className="col-span-1 bg-white px-4 py-6 rounded-lg flex justify-between border border-gray-300">
-            <div>
+            <div className="flex flex-col justify-between">
                 <div className="text-gray-600">{title}</div>
                 <div className="text-2xl font-medium max-w-[150px]">{titleValue}</div>
             </div>
-            <div className={`rounded-full p-2 ${iconColor} w-16 h-16`}>{icon}</div>
+            <div className={` rounded-full p-2 ${iconColor} w-16 h-16`}>{icon}</div>
         </div>
     );
 });
