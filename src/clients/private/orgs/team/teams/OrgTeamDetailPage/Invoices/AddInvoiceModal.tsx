c6ddb41c26d15c696d678d2fd940789f379453aa
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-02-28 12:12:38 +0900

    SlideUpSelectModal 사용처에 따라 toast message가 다를 수 있도록 수정

--- src/clients/private/orgs/team/teams/OrgTeamDetailPage/Invoices/AddInvoiceModal.tsx
+++ src/clients/private/orgs/team/teams/OrgTeamDetailPage/Invoices/AddInvoiceModal.tsx
@@ -64,6 +64,7 @@ export const AddInvoiceModal = memo((props: AddInvoiceModalProps) => {
             title="팀에 연결할 청구서 메일을 모두 선택해 주세요."
             ctaInactiveText="청구서 메일을 선택해주세요."
             ctaActiveText="%n개의 선택된 청구서 메일 연결하기"
+            toastMessage="선택한 청구서 메일을 연결했어요."
         />
     );
 });
