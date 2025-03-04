3e8262cbfa42754c64ccb5f7ab1a8e06e3eef00b
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-03-04 10:00:28 +0900

    [구성원연결] 구독 등록하기-모두 선택하기 기능 추가

--- src/clients/private/orgs/subscriptions/OrgSubscriptionConnectsPage/ContentFunnels/inputs/TeamMemberSelect/index.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionConnectsPage/ContentFunnels/inputs/TeamMemberSelect/index.tsx
@@ -13,11 +13,12 @@ import {TeamMemberSelectableSection} from './TeamMemberSelectableSection';
 import {TeamMemberCreateMethodModal} from './TeamMemberCreateMethodModal';
 
 export const TeamMemberSelect = memo(function TeamMemberSelect() {
-    const {search, reload} = useTeamMemberListInCreateSubscription();
+    const {search, reload, result} = useTeamMemberListInCreateSubscription();
     const [isCreateMethodModalOpened, setCreateMethodModalOpened] = useState(false);
     const [isCreateAutoModalOpened, setCreateAutoModalOpened] = useState(false);
     const [isCreateManualModalOpened, setCreateManualModalOpened] = useState(false);
     const [isCreateByExcelModalOpened, setCreateByExcelModalOpened] = useState(false);
+    const [isAllSelectTeamMember, setIsAllSelectTeamMember] = useState(false);
 
     useEffect(() => {
         search({});
@@ -29,17 +30,24 @@ export const TeamMemberSelect = memo(function TeamMemberSelect() {
                 <div className="grid grid-cols-2 gap-2">
                     <TeamMemberSearchInput />
 
-                    <div className="flex items-center justify-start">
+                    <div className="flex items-center justify-start gap-2">
                         <button className="btn btn-scordi gap-2" onClick={() => setCreateMethodModalOpened(true)}>
                             <FaPlus />
                             <span>구성원 추가</span>
+                        </button>
+                        <button
+                            className={`btn btn-white gap-2 ${result.items.length === 0 && 'disabled'}`}
+                            disabled={result.items.length === 0}
+                            onClick={() => setIsAllSelectTeamMember(!isAllSelectTeamMember)}
+                        >
+                            <span>{isAllSelectTeamMember ? '선택취소' : '전체선택'}</span>
                         </button>
                     </div>
                 </div>
 
                 <TeamMemberSelectedSection />
 
-                <TeamMemberSelectableSection />
+                <TeamMemberSelectableSection isAllSelectTeamMember={isAllSelectTeamMember} />
             </div>
 
             <TeamMemberCreateMethodModal
