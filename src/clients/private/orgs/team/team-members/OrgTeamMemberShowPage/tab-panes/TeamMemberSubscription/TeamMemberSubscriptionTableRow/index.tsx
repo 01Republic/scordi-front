cb9ac286002fe02913028fa10a54da4d451e1284
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-03-04 14:27:27 +0900

    [구성원상세p > 이용구독] 클릭 시 구독상세로 이동

--- src/clients/private/orgs/team/team-members/OrgTeamMemberShowPage/tab-panes/TeamMemberSubscription/TeamMemberSubscriptionTableRow/index.tsx
+++ src/clients/private/orgs/team/team-members/OrgTeamMemberShowPage/tab-panes/TeamMemberSubscription/TeamMemberSubscriptionTableRow/index.tsx
@@ -16,6 +16,8 @@ import {
 } from '^models/Subscription/components';
 import {CreditCardProfileCompact} from '^models/CreditCard/components';
 import {AirInputText} from '^v3/share/table/columns/share/AirInputText';
+import {OrgSubscriptionDetailPageRoute} from '^pages/orgs/[id]/subscriptions/[subscriptionId]';
+import {OpenButtonColumn} from '^clients/private/_components/table/OpenButton';
 
 interface TeamMemberSubscriptionTableRowProps {
     teamMember: TeamMemberDto;
@@ -50,11 +52,15 @@ export const TeamMemberSubscriptionTableRow = memo((props: TeamMemberSubscriptio
         reload();
     };
 
+    const showPagePath = OrgSubscriptionDetailPageRoute.path(subscription.organizationId, subscription.id);
+
     return (
         <tr onClick={() => console.log(subscription)}>
             {/* 서비스 명 */}
             <td>
-                <SubscriptionProfile subscription={subscription} />
+                <OpenButtonColumn href={showPagePath}>
+                    <SubscriptionProfile subscription={subscription} />
+                </OpenButtonColumn>
             </td>
 
             {/* 유/무료 */}
