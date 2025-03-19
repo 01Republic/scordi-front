aab6943b01783f38dc04a5b82c94e90717e491d6
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-03-19 13:14:20 +0900

    Feat: 워크스페이스명 섹션 컴포넌트 추가

---
+++ src/clients/public/home/LandingPages/SignAdditionalInfoPage/OrganizationInfoSection/OrganizationNameSection.tsx
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import {useFormContext} from 'react-hook-form';
+import {Building, TriangleAlert} from 'lucide-react';
+import cn from 'classnames';
+import {WorkspacesAndCompanyInfoType} from '^models/User/types';
+import {StepButton} from '^clients/public/home/LandingPages/SignAdditionalInfoPage/StepButton';
+
+interface OrganizationNameSectionProps {
+    setIsNext?: (val: boolean) => void;
+    isDisabled?: boolean;
+}
+
+export const OrganizationNameSection = (props: OrganizationNameSectionProps) => {
+    const {setIsNext, isDisabled = false} = props;
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const {
+        register,
+        watch,
+        formState: {errors, isValid},
+    } = useFormContext<WorkspacesAndCompanyInfoType>();
+    const value = watch('organizationName');
+    const {onBlur: registerOnBlur, ...restRegister} = register('organizationName', {
+        required: '워크스페이스명을 입력해주세요.',
+    });
+
+    return (
+        <>
+            <label htmlFor="워크스페이스명" className="block relative">
+                <div className="relative">
+                    <input
+                        disabled={isDisabled}
+                        readOnly={isDisabled}
+                        onClick={() => setIsActive(true)}
+                        onBlur={(e) => {
+                            registerOnBlur(e);
+                            if (!value) {
+                                setIsActive(false);
+                            }
+                        }}
+                        {...restRegister}
+                        className={cn(
+                            'w-full h-12 border text-sm text-neutral-900 rounded-lg pl-12 pr-5 pt-3 focus:outline focus:outline-1',
+                            {
+                                'bg-gray-100': isDisabled,
+                                'bg-white': !isDisabled,
+                                'border-red-400 focus:outline-red-400': errors.organizationName,
+                                'border-neutral-300 focus:outline-primaryColor-900': !errors.organizationName,
+                            },
+                        )}
+                    />
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+                        <Building className="absolute left-4 top-1/2 transform -translate-y-1/2 text-violet-200 text-[22px]" />
+                    </div>
+                    <div
+                        className={cn(
+                            'absolute flex pl-12 left-0 pointer-events-none transition duration-700 ease text-neutral-400',
+                            isActive || value ? 'flex-col top-1 text-xs' : 'items-center inset-y-0 text-md',
+                        )}
+                    >
+                        <span>워크스페이스명</span>
+                    </div>
+                </div>
+            </label>
+            {errors.organizationName && (
+                <section className="flex gap-1 text-red-400 w-full justify-start -mt-1">
+                    <TriangleAlert className="text-red-400" />
+                    <p className="font-normal text-12">{errors.organizationName?.message}</p>
+                </section>
+            )}
+
+            {setIsNext && (
+                <section className="w-full mt-10">
+                    <StepButton text="계속" disabled={isValid} onClick={() => setIsNext(true)} />
+                </section>
+            )}
+        </>
+    );
+};
