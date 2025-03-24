e7865480ab28ffdcd9d60d803eaaefa018ec7f97
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-03-24 18:16:06 +0900

    Remove: signAdditionalInfoPage 폴더명 → signAuthPage 폴더명으로 변경

--- src/clients/public/home/LandingPages/BetaSignPhoneAuthPage/v2.tsx
+++ src/clients/public/home/LandingPages/BetaSignPhoneAuthPage/v2.tsx
@@ -14,12 +14,10 @@ import {googleAccessTokenAtom} from '^components/pages/UsersLogin/atom';
 import {userSocialGoogleApi} from '^api/social-google.api';
 import {TermModalV2} from '^clients/public/home/LandingPages/BetaSignPhoneAuthPage/TermModalV2';
 import {useSocialLoginV2} from '^models/User/hook';
-import {UserAdditionalInfoType, UserGoogleSocialSignUpRequestDtoV2} from '^models/User/types';
+import {UserGoogleSocialSignUpRequestDtoV2} from '^models/User/types';
 import {V3OrgJoinErrorPageRoute} from '^pages/v3/orgs/[orgId]/error';
 import {SignWelcomePageRoute} from '^pages/sign/welcome';
-import {SignAdditionalInfoPage} from 'src/clients/public/home/LandingPages/SignAdditionalInfoPage';
 import {Loader} from 'lucide-react';
-import {OrganizationInfoSectionPage} from '^clients/public/home/LandingPages/SignAdditionalInfoPage/OrganizationInfoSection';
 
 export const BetaSignPhoneAuthPage2 = memo(() => {
     const router = useRouter();
@@ -144,7 +142,6 @@ export const BetaSignPhoneAuthPage2 = memo(() => {
     if (orgNameInputDisplay) {
         return (
             <LandingPageLayout pageName="BetaSignPhoneAuthPage" hideNav hideFooter>
-                <OrganizationInfoSectionPage />
                 <div className="mx-auto text-center pt-[30vh] w-full max-w-lg space-y-5 h-screen">
                     <h1
                         className="text-3xl sm:text-4xl font-bold leading-loose"
@@ -200,7 +197,6 @@ export const BetaSignPhoneAuthPage2 = memo(() => {
 
     return (
         <LandingPageLayout pageName="BetaSignPhoneAuthPage" hideNav hideFooter>
-            <SignAdditionalInfoPage onClick={() => !isLoading && agreeModalOnConfirm()} />
             <div className=" mx-auto text-center pt-[30vh] w-full max-w-lg space-y-5 h-screen">
                 <h1
                     className="text-3xl sm:text-4xl font-bold"
