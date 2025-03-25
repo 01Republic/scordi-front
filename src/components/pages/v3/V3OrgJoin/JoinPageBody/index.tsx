250c780395fb3311e87dc0267f3b52ebf513f8d2
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-03-25 22:37:57 +0900

    Style: 로그인,회원가입,조인(초대받은경우)p/문구변경 및 로고 추가

--- src/components/pages/v3/V3OrgJoin/JoinPageBody/index.tsx
+++ src/components/pages/v3/V3OrgJoin/JoinPageBody/index.tsx
@@ -1,4 +1,5 @@
 import React, {memo} from 'react';
+import Image from 'next/image';
 import {GoogleOAuthProvider} from '@react-oauth/google';
 import {googleOAuth} from '^config/environments';
 import {GoogleLoginBtn} from '^components/pages/UsersLogin/GoogleLoginBtn';
@@ -17,25 +18,34 @@ import {errorToast} from '^api/api';
 import {userSocialGoogleApi} from '^api/social-google.api';
 import {ArrowRight} from 'lucide-react';
 import {SignAuthCreateUserPageRoute} from '^pages/sign/createUser';
+import scordiLogo from '^public/images/renewallogo/scordi-symbol-logo.png';
+import scordiTextLogo from '^public/images/renewallogo/scordi-text-loco.png';
+import {NewLandingPageLayout} from '^clients/public/home/LandingPages/NewLandingPageLayout';
 
 export const JoinPageBody = memo(() => {
     return (
-        <div className="flex items-center justify-center" style={{height: '100vh'}}>
-            <div className="m-auto text-center min-w-[400px]">
-                <h1
-                    className="mb-1 text-gradient-color"
-                    style={{background: 'linear-gradient(to right, #5c5fee, #a5a6f5)'}}
-                >
-                    초대를 받으셨군요!
-                </h1>
-                <p className="mb-5 text-14 font-semibold text-gray-500">
-                    소프트웨어 구독관리에 더 이상 시간 쓰지 마세요
-                </p>
-                <div className="pb-20">
+        <NewLandingPageLayout pageName="joinpage" hideNav>
+            <div className="flex items-center justify-center" style={{height: '100vh'}}>
+                <div className="flex flex-col items-center justify-center w-[400px] gap-5">
+                    <section className="flex items-center justify-center gap-1">
+                        <Image src={scordiLogo} alt="scordiLogo" width={20} height={20} priority />
+                        <Image src={scordiTextLogo} alt="scordiLogo" width={70} height={36} priority />
+                    </section>
+                    <section className="flex flex-col items-center justify-center gap-1">
+                        <h1
+                            className="text-gradient-color"
+                            style={{background: 'linear-gradient(to right, #5c5fee, #a5a6f5)'}}
+                        >
+                            초대를 받으셨군요!
+                        </h1>
+                        <p className="text-14 font-semibold text-gray-500">
+                            소프트웨어 구독관리에 더 이상 시간 쓰지 마세요
+                        </p>
+                    </section>
                     <InvitedGoogleLoginButton />
                 </div>
             </div>
-        </div>
+        </NewLandingPageLayout>
     );
 });
 
@@ -104,7 +114,7 @@ const InvitedGoogleLoginButton = memo(() => {
                 className="btn-block justify-start relative"
                 buttonText={
                     <span>
-                        Google 계정으로 시작하기{' '}
+                        Google 계정으로 시작하기
                         <span className="absolute right-4">
                             <ArrowRight />
                         </span>
