48f8da1e4d9558e6217779d027b24c0bec45f7d8
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-03 20:49:07 +0900

    feat: 탑라인배너 타입 추가, 카드사에 따라 개월수를 반환하는 유틸함수 추가

--- src/models/TopLineBanner/type/index.ts
+++ src/models/TopLineBanner/type/index.ts
@@ -1,18 +1,42 @@
+import {PencilLine} from 'lucide-react';
 import {FindAllQueryDto} from '^types/utils/findAll.query.dto';
+import {ReactNode} from 'react';
 
 export type LineBannerTheme = 'notice' | 'waring' | 'danger' | 'basicInfo' | 'thanksTo';
+export type LineBannerType = 'text' | 'button' | 'link';
 
 export class TopLineBannerDto {
-    id: number;
+    id: number | null;
     text: string;
-    url?: string;
-    animation?: boolean;
-    fixed: boolean;
-    closeButton?: boolean;
-    timeout: number | null;
+    type: LineBannerType;
     theme: LineBannerTheme;
+    icon?: ReactNode | null;
+    animation?: boolean | null;
+    fixed?: boolean | null;
+    closeButton?: boolean | null;
+    timeout?: number | null;
+    url?: string | null;
+    onClick?: () => void | null;
 }
 
 export class FindAllTopLineBannersQueryDto extends FindAllQueryDto<TopLineBannerDto> {
     //
+}
+
+export function getCreditCardPolicyDuration(company: string): string {
+    console.log('company', company);
+
+    const oneYear = ['BC', '수협', '제주', '광주', '전북', '국민', '우리', '하나', 'NH', '씨티'];
+    const sixMonth = ['신한'];
+    const threeMonth = ['삼성', '롯데', '현대'];
+
+    if (oneYear.some((name) => company.includes(name))) {
+        return '1년';
+    } else if (sixMonth.some((name) => company.includes(name))) {
+        return '6개월';
+    } else if (threeMonth.some((name) => company.includes(name))) {
+        return '3개월';
+    }
+
+    return '';
 }
