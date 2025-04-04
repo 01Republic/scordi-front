0237b4a524c6640e630301f4609cdae6627ef4c4
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-04 18:58:20 +0900

    Refactor:배너 컨테이너 id top-banner-container → page-flash 로 변경 및 포탈 컴포넌트 제거

--- src/clients/private/_components/rest-pages/ShowPage/index.tsx
+++ src/clients/private/_components/rest-pages/ShowPage/index.tsx
@@ -1,11 +1,9 @@
-import React, {memo, useEffect, useRef} from 'react';
+import React, {memo, useEffect} from 'react';
 import {WithChildren} from '^types/global.type';
 import {MainContainer, MainLayout} from '^clients/private/_layouts/MainLayout';
 import {Breadcrumb, BreadcrumbPath} from '^clients/private/_layouts/_shared/Breadcrumb';
 import {useOrgIdParam} from '^atoms/common';
 import {useRouter} from 'next/router';
-import {TopLineBannerPortal} from '^components/util/TopLineBannerPortal';
-import {TopLineBanner} from '^clients/private/_layouts/_shared/TopLineBanner';
 
 interface ShowPageProps extends WithChildren {
     onReady?: () => any;
@@ -16,7 +14,6 @@ export const ShowPage = memo((props: ShowPageProps) => {
     const {onReady, breadcrumb, children} = props;
     const orgId = useOrgIdParam();
     const router = useRouter();
-    const bannerContainerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if (!orgId || isNaN(orgId)) return;
@@ -26,10 +23,7 @@ export const ShowPage = memo((props: ShowPageProps) => {
 
     return (
         <MainLayout>
-            <div ref={bannerContainerRef} id="top-banner-container" className="relative h-0 overflow-visible" />
-            <TopLineBannerPortal container={bannerContainerRef.current}>
-                <TopLineBanner />
-            </TopLineBannerPortal>
+            <div id="page-flash" className="relative h-0 overflow-visible" />
             <MainContainer>
                 {breadcrumb && <Breadcrumb paths={breadcrumb} />}
                 {children}
