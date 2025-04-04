4c7e0d10665c1610cd4871da57d275de233452db
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-04 11:24:30 +0900

    refactor: 엑셀 업로드모달, 엑셀업로드 컨펌 모달 카드 상세 최상위index 위치로 이동

--- src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/index.tsx
+++ src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/index.tsx
@@ -1,15 +1,55 @@
-import React, {memo, useState} from 'react';
-import {useOrgIdParam} from '^atoms/common';
+import React, {memo, useEffect, useState} from 'react';
+import {useSetRecoilState} from 'recoil';
+import {topLineBannerAtom, useOrgIdParam} from '^atoms/common';
+import {pick} from '^types/utils/one-of-list.type';
 import {OrgCreditCardListPageRoute} from '^pages/orgs/[id]/creditCards';
+import {useCodefCardsOfCreditCardShow} from '^models/CodefCard/hook';
+import {useGetCodefCardHistory} from '^models/CodefCard/hooks/useCodefCardSync';
+import {getCreditCardPolicyDuration} from '^models/TopLineBanner/type';
 import {ShowPage} from '^clients/private/_components/rest-pages/ShowPage';
 import {MainTabButtons} from '^clients/private/_layouts/_shared/MainTabButton';
+import {UploadBillingHistoryExcelModalConfirm} from '^clients/private/_modals/UploadBillingHistoryExcelModalConfirm';
+import {BillingHistoryExcelUploadModal} from '../OrgCreditCardShowPage/CreditCardModals/BillingHistoryExcelUploadModal';
+import {SubscriptionListOfCreditCardTabContent, BillingHistoryListOfCreditCardTabContent} from './tab-panes';
 import {CreditCardProfilePanel} from './CreditCardProfilePanel';
 import {CreditCardActionPanel} from './CreditCardActionPanel';
 import {CardInformationPanel} from './CardInformationPanel';
-import {SubscriptionListOfCreditCardTabContent, BillingHistoryListOfCreditCardTabContent} from './tab-panes';
 
 export const OrgCreditCardShowPage = memo(function OrgCreditCardShowPage() {
     const orgId = useOrgIdParam();
+    const {result} = useCodefCardsOfCreditCardShow();
+    const currentCodefCard = pick(result.items[0]);
+    const codefCardHistory = useGetCodefCardHistory(orgId, 55);
+    const setTopLineBanner = useSetRecoilState(topLineBannerAtom);
+    const [isExcelUploadModalOpen, setIsExcelUploadModalOpen] = useState(false);
+    const [isExcelModalConfirmOpen, setIsExcelModalConfirmOpen] = useState(false);
+
+    const cardCompany = currentCodefCard?.resCardName || '';
+    const duration = getCreditCardPolicyDuration(cardCompany);
+    const topLineBannerText = `카드사 정책으로 인해 최근 ${duration} 내역만 불러왔어요. 엑셀로 결제내역을 등록해주세요!`;
+
+    const onClick = () => {
+        setIsExcelModalConfirmOpen(true);
+    };
+
+    console.log('codefCardDetail', codefCardHistory.data);
+    console.log('currentCodefCard', currentCodefCard?.id);
+
+    useEffect(() => {
+        if (!codefCardHistory || !currentCodefCard) return;
+
+        if (currentCodefCard) {
+            setTopLineBanner({
+                text: topLineBannerText,
+                id: currentCodefCard.id,
+                theme: 'waring',
+                type: 'button',
+                closeButton: true,
+                onClick: onClick,
+            });
+        }
+    }, [currentCodefCard]);
+
     const [activeTabIndex, setActiveTabIndex] = useState(0);
 
     return (
@@ -56,6 +96,18 @@ export const OrgCreditCardShowPage = memo(function OrgCreditCardShowPage() {
                 )}
                 {activeTabIndex == 1 && <BillingHistoryListOfCreditCardTabContent />}
                 {/*{activeTabIndex == 2 && <div>동기화</div>}*/}
+                <BillingHistoryExcelUploadModal
+                    isOpened={isExcelUploadModalOpen}
+                    onClose={() => setIsExcelUploadModalOpen(false)}
+                />
+                <UploadBillingHistoryExcelModalConfirm
+                    isOpened={isExcelModalConfirmOpen}
+                    onClose={() => setIsExcelModalConfirmOpen(false)}
+                    onClick={() => {
+                        setIsExcelUploadModalOpen(true);
+                        setIsExcelModalConfirmOpen(false);
+                    }}
+                />
             </main>
         </ShowPage>
     );
--- src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/tab-panes/BillingHistoryListOfCreditCardTabContent/BillingHistoryTableControl.tsx
+++ src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/tab-panes/BillingHistoryListOfCreditCardTabContent/BillingHistoryTableControl.tsx
@@ -1,84 +1,39 @@
-import React, {memo, useEffect, useState} from 'react';
+import React, {memo} from 'react';
 import Image from 'next/image';
-import {useSetRecoilState} from 'recoil';
-import {topLineBannerAtom, useOrgIdParam} from '^atoms/common';
-import {getCreditCardPolicyDuration} from '^models/TopLineBanner/type';
-import {useGetCodefCardDetail} from '^models/CodefCard/hooks/useCodefCardSync';
-import {useCodefCardsOfCreditCardShow} from '^models/CodefCard/hook';
 import excelIcon from 'src/images/icon/excelIcon.png';
-import {pick} from '^types/utils/one-of-list.type';
 import {BillingHistoryScopeHandler} from './BillingHistoryScopeHandler';
-import {BillingHistoryExcelUploadModal} from './BillingHistoryExcelUploadModal';
-import {UploadBillingHistoryExcelModalConfirm} from '^clients/private/_modals/UploadBillingHistoryExcelModalConfirm';
 
-export const BillingHistoryTableControl = memo(() => {
+interface BillingHistoryTableControlProps {
+    excelUploadModalClose: () => void;
+}
+
+export const BillingHistoryTableControl = memo((props: BillingHistoryTableControlProps) => {
+    const {excelUploadModalClose} = props;
     return (
         <div className="flex items-center justify-between mb-4">
             <BillingHistoryScopeHandler />
 
             <div>
                 <div className="flex items-center gap-2">
-                    <ExcelUploadButton />
+                    <ExcelUploadButton excelUploadModalClose={excelUploadModalClose} />
                 </div>
             </div>
         </div>
     );
 });
 
-export const ExcelUploadButton = memo(() => {
-    const orgId = useOrgIdParam();
-    const {result} = useCodefCardsOfCreditCardShow();
-    const currentCodefCard = pick(result.items[0]);
-    const codefCardDetail = useGetCodefCardDetail(orgId, currentCodefCard?.id);
-    const setTopLineBanner = useSetRecoilState(topLineBannerAtom);
-    const [isExcelUploadModalOpen, setIsExcelUploadModalOpen] = useState(false);
-    const [isExcelModalConfirmOpen, setIsExcelModalConfirmOpen] = useState(false);
+interface ExcelUploadButtonProps {
+    excelUploadModalClose: () => void;
+}
 
-    const cardCompany = currentCodefCard?.resCardName || '';
-    const duration = getCreditCardPolicyDuration(cardCompany);
-    const topLineBannerText = `카드사 정책으로 인해 최근 ${duration} 내역만 불러왔어요. 엑셀로 결제내역을 등록해주세요!`;
-
-    const onClick = () => {
-        setIsExcelModalConfirmOpen(true);
-    };
-
-    useEffect(() => {
-        if (!codefCardDetail || !currentCodefCard) return;
-
-        if (currentCodefCard) {
-            setTopLineBanner({
-                text: topLineBannerText,
-                id: currentCodefCard.id,
-                theme: 'waring',
-                type: 'button',
-                closeButton: true,
-                onClick: onClick,
-            });
-        }
-    }, [currentCodefCard]);
-
+export const ExcelUploadButton = memo((props: ExcelUploadButtonProps) => {
+    const {excelUploadModalClose} = props;
     return (
         <>
-            <button
-                type="button"
-                onClick={() => setIsExcelUploadModalOpen(true)}
-                className="btn btn-sm btn-white gap-2 "
-            >
+            <button type="button" onClick={excelUploadModalClose} className="btn btn-sm btn-white gap-2 ">
                 <Image src={excelIcon} alt="excelIcon" width={14} height={14} priority />
                 <span>엑셀로 등록하기</span>
             </button>
-            <BillingHistoryExcelUploadModal
-                isOpened={isExcelUploadModalOpen}
-                onClose={() => setIsExcelUploadModalOpen(false)}
-            />
-            <UploadBillingHistoryExcelModalConfirm
-                isOpened={isExcelModalConfirmOpen}
-                onClose={() => setIsExcelModalConfirmOpen(false)}
-                onClick={() => {
-                    setIsExcelUploadModalOpen(true);
-                    setIsExcelModalConfirmOpen(false);
-                }}
-            />
         </>
     );
 });
