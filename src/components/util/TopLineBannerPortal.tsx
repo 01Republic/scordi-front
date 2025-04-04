4f7cdbe87cd59705c5f2ff2c5338a069010f6ac5
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-04 19:01:16 +0900

    Refactor: TopLineBannerPortal → PageFlashPortal 로 변경
    - container를 props로 받던 부분을 제거했어요.

--- src/components/util/TopLineBannerPortal.tsx
+++ src/components/util/TopLineBannerPortal.tsx
@@ -1,12 +1,7 @@
 import {createPortal} from 'react-dom';
 import {WithChildren} from '^types/global.type';
 
-interface TopLineBannerPortalProps extends WithChildren {
-    container: Element | null;
-}
-
-export const TopLineBannerPortal = (props: TopLineBannerPortalProps) => {
-    const {container, children} = props;
-    const target = container || document.body;
+export const PageFlashPortal = ({children}: WithChildren) => {
+    const target = document.querySelector('#page-flash') || document.body;
     return createPortal(children, target);
 };
