ab76064b0bdf61b89dc5411344c3ea2396d76438
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-04-05 14:04:49 +0900

    슬랙 연동 상세페이지 / 설정 탭에서 구성원 계정 목록 동기화 실행 이후 리로드 개선

--- src/clients/private/orgs/settings/integrations/slack/OrgIntegrationSlackWorkspaceDetailPage/SlackWorkspaceSetting/index.tsx
+++ src/clients/private/orgs/settings/integrations/slack/OrgIntegrationSlackWorkspaceDetailPage/SlackWorkspaceSetting/index.tsx
@@ -9,19 +9,24 @@ interface SlackWorkspaceSettingProps {
 }
 
 export const SlackWorkspaceSetting = memo((props: SlackWorkspaceSettingProps) => {
-    const {data: workspace} = useSlackWorkspaceInDetailPage();
-    const {refetch} = useSlackMembersInDetailPage({
+    const {data: workspace, refetch: reloadWorkspace} = useSlackWorkspaceInDetailPage();
+    const {refetch: reloadSlackMembers} = useSlackMembersInDetailPage({
         relations: ['teamMember'],
         order: {isDeleted: 'ASC', id: 'DESC'},
         itemsPerPage: 0,
     });
 
+    const reload = async () => {
+        reloadSlackMembers();
+        reloadWorkspace();
+    };
+
     return (
         <div className="mt-8">
             <div className="card card-bordered card-body rounded-lg overflow-hidden w-full bg-white">
-                <SyncSection workspace={workspace} reload={() => refetch()} />
+                <SyncSection workspace={workspace} reload={reload} />
                 <br />
-                <RemoveWorkspaceSection workspace={workspace} reload={() => refetch()} />
+                <RemoveWorkspaceSection workspace={workspace} reload={reload} />
             </div>
         </div>
     );
