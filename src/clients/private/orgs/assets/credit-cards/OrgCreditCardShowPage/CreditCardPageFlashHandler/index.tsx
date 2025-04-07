7907d559ba83b2344ccb850b59bcb4b7e2e7c087
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-07 15:25:28 +0900

    refactor: CreditCardPageFlashHandler 에 pageFlash를 보여주는 조건 수정

--- src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/CreditCardPageFlashHandler/index.tsx
+++ src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/CreditCardPageFlashHandler/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import {useOrgIdParam} from '^atoms/common';
 import {pick} from '^types/utils/one-of-list.type';
 import {useCodefCardsOfCreditCardShow} from '^models/CodefCard/hook';
 import {getCreditCardPolicyDuration} from '^models/TopLineBanner/type';
-import {useGetCodefCardHistory} from '^models/CodefCard/hooks/useCodefCardSync';
+import {useOldestCodefBillingHistory} from '^models/CodefBillingHistory/hook';
 import {PageFlash} from '^clients/private/_layouts/_shared/PageFlash';
-import {useCurrentCreditCard} from '^clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/atom';
+import {yyyy_mm} from '^utils/dateTime';
 
 interface CreditCardPageFlashHandlerProps {
     uploadExcelModalConfirmOpen: () => void;
@@ -16,18 +17,24 @@ export const CreditCardPageFlashHandler = (props: CreditCardPageFlashHandlerProp
     const orgId = useOrgIdParam();
     const {result} = useCodefCardsOfCreditCardShow();
     const currentCodefCard = pick(result.items[0]);
-    const {currentCreditCard} = useCurrentCreditCard();
+    const codefId = currentCodefCard?.id;
 
-    const creditCardHistory = useGetCodefCardHistory(orgId, {
-        relations: ['subscription'],
-        where: {
-            creditCardId: currentCreditCard?.id,
-            organizationId: orgId,
-        },
-        order: {issuedAt: 'DESC'},
-    });
+    const {data: oldestCodefBillingHistory, isError} = useOldestCodefBillingHistory(orgId, codefId);
 
-    if (!currentCodefCard?.id) return <></>;
+    /* pageFlash return 조건
+     * 1. codefId 가 없으면 return
+     * 2. oldestCodefBillingHistory 가 없다면 return
+     * 2. codef 카드 최초 발급일자(년,월) / 불러온 마지막 결제일(년,월) 비교하여 일치하면 return
+     * 3. codef 카드 최초 발급일자가 없다면 return
+     * TODO. codef 카드 최초 발급일자(년,월) / 불러온 마지막 결제일(년,월) 비교하는 것은 정확한 정보가 아니기 때문에 개선 필요
+     */
+
+    if (!currentCodefCard || !oldestCodefBillingHistory || isError) return <></>;
+
+    const codefCardResIssueDate = yyyy_mm(dayjs(currentCodefCard.resIssueDate, 'YYYYMM').toDate());
+    const oldestCodefBillingHistoryDate = yyyy_mm(oldestCodefBillingHistory.usedAt);
+
+    if (codefCardResIssueDate === oldestCodefBillingHistoryDate) return <></>;
 
     const cardCompany = currentCodefCard?.resCardName || '';
     const duration = getCreditCardPolicyDuration(cardCompany);
