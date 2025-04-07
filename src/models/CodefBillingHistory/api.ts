7b2306d595e93a8f8299c144d887f2acf006be7f
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-07 15:21:16 +0900

    feat: codefBillingHistory에 가장 오래된 결제내역 단건을 조회할 수 있는 api client, hook 추가

--- src/models/CodefBillingHistory/api.ts
+++ src/models/CodefBillingHistory/api.ts
@@ -1,6 +1,15 @@
 import {CodefBillingHistoryDto, FindAllCodefBillingHistoryAdminQueryDto} from '^models/CodefBillingHistory/type';
 import {api} from '^api/api';
-import {paginatedDtoOf} from '^types/utils/response-of';
+import {oneDtoOf, paginatedDtoOf} from '^types/utils/response-of';
+
+// 코드에프카드 - 유저 데이터 조회
+export const codefBillingHistoriesApi = {
+    // 가장 오래된 코드에프카드 단건 결제내역 조회
+    index(orgId: number, codefCardId: number) {
+        const url = `/organizations/${orgId}/codef-cards/${codefCardId}/codef-billing-histories/oldest`;
+        return api.get(url).then(oneDtoOf(CodefBillingHistoryDto));
+    },
+};
 
 export const codefBillingHistoriesAdminApi = {
     index(params: FindAllCodefBillingHistoryAdminQueryDto) {
