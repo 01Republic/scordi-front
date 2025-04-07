13415b29d4e2e7667679479e9c206568d07236a5
seongonecho <seongonecho@gmail.com>
2025-04-08 00:42:07 +0900

    feat: 연동 완료 페이지 UI

--- src/clients/private/orgs/assets/connect/AutoConnect/AutoConnectComplete/index.tsx
+++ src/clients/private/orgs/assets/connect/AutoConnect/AutoConnectComplete/index.tsx
@@ -1,3 +1,19 @@
-export const AutoConnectCompletePage = () => {
-    return <div>AutoConnectCompletePage</div>;
-};
+import { MainContainer, MainLayout } from "^clients/private/_layouts/MainLayout";
+import { NextImage } from "^components/NextImage";
+import { memo } from "react";
+import ClappingHands from "/src/images/ClappingHands.png";
+
+export const AutoConnectCompletePage = memo(() => {
+    return (
+        <MainLayout>
+            <MainContainer>
+                <div className="mb-12 space-y-2">
+                    <NextImage src={ClappingHands} alt="clapping hands" width={60} height={60} />
+                    <div className="text-2xl font-bold">자산 연동이 완료 되었어요</div>
+                </div>
+            </MainContainer>
+        </MainLayout>
+    );
+});
+
+AutoConnectCompletePage.displayName = 'AutoConnectCompletePage';
