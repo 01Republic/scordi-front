26231cd8f3c06106263139f1c0698a3a37d7b888
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-08 10:32:36 +0900

    Remove: 주석 제거

--- src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/CreditCardPageFlashHandler/index.tsx
+++ src/clients/private/orgs/assets/credit-cards/OrgCreditCardShowPage/CreditCardPageFlashHandler/index.tsx
@@ -21,13 +21,6 @@ export const CreditCardPageFlashHandler = (props: CreditCardPageFlashHandlerProp
 
     const {data: oldestCodefBillingHistory, isError} = useOldestCodefBillingHistory(orgId, codefId);
 
-    /* pageFlash return 조건
-     * 1. codefId 가 없으면 return
-     * 2. oldestCodefBillingHistory 가 없다면 return
-     * 2. codef 카드 최초 발급일자(년,월) / 불러온 마지막 결제일(년,월) 비교하여 일치하면 return
-     * 3. codef 카드 최초 발급일자가 없다면 return
-     * TODO. codef 카드 최초 발급일자(년,월) / 불러온 마지막 결제일(년,월) 비교하는 것은 정확한 정보가 아니기 때문에 개선 필요
-     */
 
     if (!currentCodefCard || !oldestCodefBillingHistory || isError) return <></>;
 
