b554c15b0c16dce4963556e11cf9b19505dd24aa
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-10 12:00:05 +0900

    hotfix/결제내역 엑셀등록QA/카드상세p-UploadBillingHistoryExcelModalConfirm 스타일 수정
    1. 베이직 모달로 수정하여 애니메이션 제거
    2. 설명 문구 변경
    3. 모달 크기 max-w-xl → max-w-lg 변경
    4. 닫기 아이콘 크기 size-8 → size-6 변경
    5. 엑셀로 구독 불러오기 버튼 크기 전체 넓이 → w-1/2 변경

--- src/clients/private/_modals/UploadBillingHistoryExcelModalConfirm.tsx
+++ src/clients/private/_modals/UploadBillingHistoryExcelModalConfirm.tsx
@@ -1,7 +1,7 @@
-import {AnimatedModal} from '^components/modals/_shared/AnimatedModal';
+import Image from 'next/image';
 import {X, Sparkles} from 'lucide-react';
 import excelIcon from 'src/images/icon/excelIcon.png';
-import Image from 'next/image';
+import {BasicModal} from '^components/modals/_shared/BasicModal';
 
 interface UploadBillingHistoryExcelModalConfirmProps {
     isOpened: boolean;
@@ -12,31 +12,33 @@ interface UploadBillingHistoryExcelModalConfirmProps {
 export const UploadBillingHistoryExcelModalConfirm = (props: UploadBillingHistoryExcelModalConfirmProps) => {
     const {isOpened, onClose, onClick} = props;
     return (
-        <AnimatedModal open={isOpened} onClose={onClose}>
-            <section className="modal-box relative max-w-xl flex flex-col justify-between gap-10 py-10 px-5">
+        <BasicModal open={isOpened} onClose={onClose}>
+            <div className="modal-box relative max-w-lg flex flex-col justify-between gap-5 py-10 px-5">
                 <X
                     onClick={onClose}
-                    className="absolute top-5 right-5 size-8 text-gray-300 hover:text-gray-200 cursor-pointer"
+                    className="absolute top-5 right-5 size-6 text-gray-300 hover:text-gray-200 cursor-pointer"
                 />
-                <div className="flex flex-col justify-start gap-1">
+                <section className="flex flex-col justify-start gap-1">
                     <div className="text-14 text-scordi font-medium">과거 결제내역 가져오기</div>
                     <div className="flex items-center gap-2">
                         <Image src={excelIcon} alt="excelIcon" width={32} height={32} priority />
                         <h3>엑셀로 구독을 한 번에 불러와요.</h3>
                     </div>
-                </div>
+                </section>
 
                 <p className="text-gray-600 text-16 whitespace-pre-wrap">
-                    카드사 홈페이지에서 과거 결제내역을내려 받기 한 후, <br />
-                    스코디에 엑셀 파일을 업로드 해주세요. <br />
+                    카드사 홈페이지에서 승인내역을 엑셀로 내려 받기 한 후, <br />
+                    스코디 엑셀 양식에 맞게 파일을 업로드 해주세요. <br />
                     카드사 정책으로 인해 불러오지 못한 구독도 모두 확인할 수 있어요.
                 </p>
 
-                <button type="button" onClick={onClick} className="btn btn-scordi btn-block gap-1">
-                    <Sparkles />
-                    <span>엑셀로 구독 불러오기</span>
-                </button>
-            </section>
-        </AnimatedModal>
+                <section className="flex justify-center">
+                    <button type="button" onClick={onClick} className="btn btn-scordi gap-1 w-1/2">
+                        <Sparkles />
+                        <span>엑셀로 구독 불러오기</span>
+                    </button>
+                </section>
+            </div>
+        </BasicModal>
     );
 };
