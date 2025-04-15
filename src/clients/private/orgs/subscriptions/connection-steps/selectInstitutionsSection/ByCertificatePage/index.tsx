43e8e7d5a0b8f0febfbd7355d93552e5372f84da
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-15 11:34:28 +0900

    feat: 공동인증서로 자산 연동할 수 있는 페이지 추가

---
+++ src/clients/private/orgs/subscriptions/connection-steps/selectInstitutionsSection/ByCertificatePage/index.tsx
@@ -0,0 +1,35 @@
+import {memo} from 'react';
+import {useFormContext} from 'react-hook-form';
+import {CreateAccountRequestDto} from '^models/CodefAccount/type/create-account.request.dto';
+import {PureLayout} from '^clients/private/_layouts/PureLayout';
+import {StatusHeader} from '../../common/StatusHeader';
+import {BusinessTypeSelector} from '../../common/BusinessTypeSelector';
+import {BankSelector} from '../../common/BankSelector';
+import {CardSelector} from '../../common/CardSelector';
+import {NextStepButton} from '../../common/NextStepButton';
+
+export const ByCertificatePage = memo(() => {
+    const {reset} = useFormContext<CreateAccountRequestDto>();
+    const onclick = () => {};
+
+    return (
+        <PureLayout>
+            <article className="w-full flex flex-col gap-20">
+                <div className="flex flex-col gap-10">
+                    <StatusHeader
+                        title="어떤 자산을 연결할까요?"
+                        subTitle="개인사업자의 경우 금융사마다 정의가 달라요. 두 항목 모두 시도해보세요."
+                        onClick={() => reset({loginType: undefined})}
+                    />
+                    <BusinessTypeSelector />
+                </div>
+
+                <BankSelector />
+                <CardSelector />
+                <section className="w-full flex items-center justify-center">
+                    <NextStepButton onClick={onclick} />
+                </section>
+            </article>
+        </PureLayout>
+    );
+});
