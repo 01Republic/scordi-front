368ae50053c858f19f193d4ba20f2f2cd8232969
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-15 11:38:41 +0900

    feat: 기관(카드사/은행사) 옵션리스트를 보여주고 선택할 수 있는 컴포넌트 추가
    -BankSelector: 은행사 선택
    -BankSelector: 카드사 선택

---
+++ src/clients/private/orgs/subscriptions/connection-steps/common/BankSelector.tsx
@@ -0,0 +1,57 @@
+import {memo, useState} from 'react';
+import {BankAccountsStaticData} from '^models/CodefAccount/bank-account-static-data';
+import {InstitutionOption} from '^clients/private/orgs/subscriptions/connection-steps/common/InstitutionOption';
+import {AllSelectInstitutionOptions} from '^clients/private/orgs/subscriptions/connection-steps/common/AllSelectInstitutionOptions';
+import {useFormContext} from 'react-hook-form';
+import {CreateAccountRequestDto} from '^models/CodefAccount/type/create-account.request.dto';
+import {CodefCustomerType} from '^models/CodefAccount/type/enums';
+
+interface BankSelectorProps {}
+
+export const BankSelector = memo((props: BankSelectorProps) => {
+    const {} = props;
+    const {setValue, getValues} = useFormContext<CreateAccountRequestDto>();
+    const [selectedBanks, setSelectedBanks] = useState<BankAccountsStaticData[]>([]);
+    const clientType = getValues('clientType') || CodefCustomerType.Business;
+
+    const banks = BankAccountsStaticData.findByClientType(clientType);
+
+    const handleSelectBank = (bank: BankAccountsStaticData) => {
+        if (selectedBanks.some((b) => b.param === bank.param)) {
+            setSelectedBanks(selectedBanks.filter((b) => b.param !== bank.param));
+        } else {
+            setSelectedBanks([...selectedBanks, bank]);
+        }
+    };
+
+    const handleSelectAllBanks = () => {
+        if (selectedBanks.length === BankAccountsStaticData.findByClientType(clientType).length) {
+            setSelectedBanks([]);
+        } else {
+            setSelectedBanks(BankAccountsStaticData.findByClientType(clientType));
+        }
+    };
+
+    return (
+        <section className="flex flex-col gap-6">
+            <div className="flex items-center justify-between">
+                <h2 className="text-xl font-semibold text-neutral-900">은행</h2>
+                <AllSelectInstitutionOptions
+                    isAllSelected={selectedBanks.length === banks.length}
+                    onClick={handleSelectAllBanks}
+                />
+            </div>
+            <div className="grid grid-cols-5 gap-4">
+                {banks.map((bank) => (
+                    <InstitutionOption
+                        key={bank.param}
+                        logo={bank.logo}
+                        title={bank.displayName}
+                        isSelected={selectedBanks.some((b) => b.param === bank.param)}
+                        onClick={() => handleSelectBank(bank)}
+                    />
+                ))}
+            </div>
+        </section>
+    );
+});
