c1626dc20223d56324ee2a4878f77ea3b1580fb9
seongonecho <seongonecho@gmail.com>
2025-04-22 00:50:37 +0900

    요청 건너뛰기 시 메인 화면이 아니라 온보딩 완료 페이지로 이동

--- src/clients/private/orgs/onboarding/OrgOnboardingRequestPage.tsx
+++ src/clients/private/orgs/onboarding/OrgOnboardingRequestPage.tsx
@@ -1,5 +1,5 @@
 import {orgIdParamState} from '^atoms/common';
-import {OrgMainPageRoute} from '^pages/orgs/[id]';
+import {OrgOnboardingCompletePageRoute} from '^pages/orgs/[id]/onboarding/complete';
 import {OrgReviewCampaignNewPageRoute} from '^pages/orgs/[id]/reviewCampaigns/new';
 import {Button} from '^public/components/ui/button';
 import {useRouter} from 'next/router';
@@ -17,7 +17,7 @@ export const OrgOnboardingRequestPage = memo(() => {
             title={`구성원에게 이용 중인\n구독을 조사해요`}
             description={`실사용 여부를 파악해서 \n쓰지 않는 구독을 정리해요`}
             image="/images/examples/ex_request.png"
-            onSkip={() => router.push(OrgMainPageRoute.path(orgId, {confetti: 'true'}))}
+            onSkip={() => router.push(OrgOnboardingCompletePageRoute.path(orgId))}
             button={
                 <div className="flex flex-col gap-4">
                     <Button
