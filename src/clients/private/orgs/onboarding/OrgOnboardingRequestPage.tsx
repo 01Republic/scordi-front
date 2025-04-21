5600305162b7a16883afe8f941bb242211ce75fe
seongonecho <seongonecho@gmail.com>
2025-04-22 00:46:21 +0900

    요청 건너뛰기 시 메인화면 컨페티 효과

--- src/clients/private/orgs/onboarding/OrgOnboardingRequestPage.tsx
+++ src/clients/private/orgs/onboarding/OrgOnboardingRequestPage.tsx
@@ -17,7 +17,7 @@ export const OrgOnboardingRequestPage = memo(() => {
             title={`구성원에게 이용 중인\n구독을 조사해요`}
             description={`실사용 여부를 파악해서 \n쓰지 않는 구독을 정리해요`}
             image="/images/examples/ex_request.png"
-            onSkip={() => router.push(OrgMainPageRoute.path(orgId))}
+            onSkip={() => router.push(OrgMainPageRoute.path(orgId, {confetti: 'true'}))}
             button={
                 <div className="flex flex-col gap-4">
                     <Button
