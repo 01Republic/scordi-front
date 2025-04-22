2e266456415681c779989744559431c8c415b9a8
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-04-23 04:11:51 +0900

    I18n 국제화 언어팩 복구 및 프로필 드롭다운 i18n 적용 확인

--- src/pages/orgs/[id]/reviewCampaigns/[reviewCampaignId]/reviewResponses/[reviewResponseId]/edit/complete.tsx
+++ src/pages/orgs/[id]/reviewCampaigns/[reviewCampaignId]/reviewResponses/[reviewResponseId]/edit/complete.tsx
@@ -2,11 +2,27 @@ import {orgIdParamState, useRouterIdParamState} from '^atoms/common';
 import {ReviewResponseCompletePage} from '^clients/private/orgs/reviewCampaigns/OrgReviewResponseCompletePage';
 import {useCurrentOrg} from '^models/Organization/hook';
 import {pathReplace, pathRoute} from '^types/pageRoute.type';
+import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
+import {v3CommonRequires} from '^types/utils/18n.type';
 
 export const OrgReviewResponseCompletePageRoute = pathRoute({
     pathname: '/orgs/[id]/reviewCampaigns/[reviewCampaignId]/reviewResponses/[reviewResponseId]/edit/complete',
     path: (orgId: number, reviewCampaignId: number, reviewResponseId: number) =>
         pathReplace(OrgReviewResponseCompletePageRoute.pathname, {id: orgId, reviewCampaignId, reviewResponseId}),
+});
+
+export const getStaticPaths = async () => ({
+    paths: [{params: {id: '1', reviewCampaignId: '1', reviewResponseId: '1'}}],
+    fallback: true,
+});
+
+export const getStaticProps = async ({locale}: any) => ({
+    props: {
+        // Will be passed to the page component as props
+        ...(await serverSideTranslations(locale, [
+            ...v3CommonRequires, // 여기에 이 페이지에서 사용할 locale 파일을 추가하세요.
+        ])),
+    },
 });
 
 export default function Page() {
