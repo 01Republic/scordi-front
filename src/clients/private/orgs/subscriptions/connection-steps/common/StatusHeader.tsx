a1ea3a63696acc7201f8ed9e93d55dd0787c5f7b
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-23 09:30:27 +0900

    feat: statusHeader 컴포넌트-subTitle과 icon 을 상황에 따라 추가할 수 있도록 수정
    - subTitle: 필수값 → 옵셔널로 변경
    - icon: lottie 또는 icon 등을 옵셔널로 받을 수 있도록 추가

--- src/clients/private/orgs/subscriptions/connection-steps/common/StatusHeader.tsx
+++ src/clients/private/orgs/subscriptions/connection-steps/common/StatusHeader.tsx
@@ -1,15 +1,15 @@
-import {memo} from 'react';
+import {memo, ReactNode} from 'react';
 import {ArrowLeft} from 'lucide-react';
-import {useRouter} from 'next/router';
 
 interface StatusHeaderProps {
     title: string;
-    subTitle: string;
+    subTitle?: string;
+    icon?: ReactNode;
     onClick: () => void;
 }
 
 export const StatusHeader = memo((props: StatusHeaderProps) => {
-    const {title, subTitle, onClick} = props;
+    const {title, subTitle, icon, onClick} = props;
     return (
         <article className="flex flex-col justify-start gap-20">
             <button
@@ -19,9 +19,12 @@ export const StatusHeader = memo((props: StatusHeaderProps) => {
                 <ArrowLeft className="size-6 text-neutral-600" />
                 뒤로가기
             </button>
-            <section className="flex flex-col gap-5 text-neutral-900">
-                <span className="text-2xl font-bold">{title}</span>
-                <span className="text-base font-normal">{subTitle}</span>
+            <section className="flex flex-col gap-5 text-neutral-900 font-normal">
+                <div className="flex items-center gap-1">
+                    {icon && icon}
+                    <span className="text-2xl font-bold">{title}</span>
+                </div>
+                {subTitle && <span className="text-base font-normal">{subTitle}</span>}
             </section>
         </article>
     );
