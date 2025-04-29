10c28cbf233a32b5af34955b0aac91a57ae6e803
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-29 15:07:27 +0900

    feat: 스코디 자산으로 연결된 아이템을 보여줄 수 있는 아이템 컴포넌트 추가

---
+++ src/clients/private/orgs/subscriptions/connection-steps/ConnectionSuccessPage/ConnectedItem.tsx
@@ -0,0 +1,46 @@
+import React, {memo, ReactNode} from 'react';
+import cn from 'classnames';
+import {NextImage} from '^components/NextImage';
+import {Check, CreditCard} from 'lucide-react';
+
+interface ConnectionSuccessItemSelectProps {
+    url?: string;
+    mainText: string;
+    subText: string;
+    icon: ReactNode;
+    isSelected?: boolean;
+    onClick?: () => void;
+}
+
+export const ConnectedItem = memo((props: ConnectionSuccessItemSelectProps) => {
+    const {url, mainText, subText, icon, isSelected = false, onClick} = props;
+    return (
+        <li
+            onClick={onClick}
+            className={cn(
+                'w-full rounded-btn py-3.5 px-4 flex items-center justify-between hover:shadow-lg cursor-pointer transition-all group',
+                isSelected ? 'bg-primaryColor-bg border border-primaryColor-900' : 'bg-white border border-white',
+            )}
+        >
+            <section className="flex items-center gap-4">
+                {url ? (
+                    <NextImage src={url} alt="cardProfilImg" width={40} height={40} className="rounded-full" />
+                ) : (
+                    icon
+                )}
+                <div className="flex gap-1 text-grayColor-800 text-sm">
+                    <span>{mainText}</span>
+                    {subText.length > 0 && <span>({subText})</span>}
+                </div>
+            </section>
+
+            <Check
+                strokeWidth={3}
+                className={cn(
+                    'text-20',
+                    isSelected ? 'text-indigo-500' : 'text-transparent group-hover:text-indigo-200',
+                )}
+            />
+        </li>
+    );
+});
