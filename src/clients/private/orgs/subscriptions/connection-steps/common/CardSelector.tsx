f14892b930635cb220d765892c9dee173a0b00c7
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-04-29 15:04:04 +0900

    feat: 카드 셀렉터 기능 추가 및 수정
    - 계정으로 자산 연동 시 모달로 스코디 카드를 생성할 수 있도록 추가
    - 연결된 자산인지 아닌지 확인하는 기능 추가
    - 파라미터 명 수정

--- src/clients/private/orgs/subscriptions/connection-steps/common/CardSelector.tsx
+++ src/clients/private/orgs/subscriptions/connection-steps/common/CardSelector.tsx
@@ -1,22 +1,38 @@
-import {memo, useState} from 'react';
+import React, {memo, useEffect, useState} from 'react';
 import {useFormContext} from 'react-hook-form';
-import {CreateAccountRequestDto} from '^models/CodefAccount/type/create-account.request.dto';
 import {CardAccountsStaticData} from '^models/CodefAccount/card-accounts-static-data';
+import {CreateAccountRequestDto} from '^models/CodefAccount/type/create-account.request.dto';
 import {CodefCardCompanyCode, CodefCustomerType, CodefLoginType} from '^models/CodefAccount/type/enums';
+import {ConnectStepsModal} from '../selectInstitutionsSection/ByAccountPage/ConnectStepsModal';
 import {AllSelectInstitutionOptions} from './AllSelectInstitutionOptions';
 import {InstitutionOption} from './InstitutionOption';
+import {useCodefAccountsInConnector} from '^models/CodefAccount/hook';
+import {useRecoilValue} from 'recoil';
+import {orgIdParamState} from '^atoms/common';
+import {useRouter} from 'next/router';
 
 export const CardSelector = memo(() => {
+    const orgId = useRecoilValue(orgIdParamState);
+    const router = useRouter();
+    const {result, search} = useCodefAccountsInConnector();
     const {setValue, getValues} = useFormContext<CreateAccountRequestDto>();
     const [selectedCards, setSelectedCards] = useState<CardAccountsStaticData[]>([]);
+    const [cardCompany, setCardCompany] = useState<CardAccountsStaticData>();
+    const [isConnectStepsModalOpen, setIsConnectStepsModalOpen] = useState(false);
+
+    useEffect(() => {
+        if (!orgId || isNaN(orgId)) return;
+        search({itemsPerPage: 0}, false, true);
+    }, [router.isReady, orgId]);
 
     const clientType = getValues('clientType') || CodefCustomerType.Business;
-    const isCertificateLoginType = getValues('loginType') === CodefLoginType.Certificate;
+    const loginType = getValues('loginType');
 
     // 공동인증서 연동 시 롯데,하나,삼성은 연동불가
-    const disabledCardParams = isCertificateLoginType
-        ? [CodefCardCompanyCode.롯데카드, CodefCardCompanyCode.하나카드, CodefCardCompanyCode.삼성카드]
-        : [];
+    const disabledCardParams =
+        loginType === CodefLoginType.Certificate
+            ? [CodefCardCompanyCode.롯데카드, CodefCardCompanyCode.하나카드, CodefCardCompanyCode.삼성카드]
+            : [];
 
     const cards = CardAccountsStaticData.findByClientType(clientType);
     const selectableCards = cards.filter((card) => !disabledCardParams.includes(card.param));
@@ -25,8 +41,14 @@ export const CardSelector = memo(() => {
     const handleSelectCard = (card: CardAccountsStaticData) => {
         if (disabledCardParams.includes(card.param)) return;
 
-        if (selectedCards.some((c) => c.param === card.param)) {
-            setSelectedCards(selectedCards.filter((c) => c.param !== card.param));
+        if (loginType === CodefLoginType.IdAccount) {
+            setCardCompany(card);
+            setIsConnectStepsModalOpen(true);
+            return;
+        }
+
+        if (selectedCards.some((cardAccount) => cardAccount.param === card.param)) {
+            setSelectedCards(selectedCards.filter((cardAccount) => cardAccount.param !== card.param));
         } else {
             setSelectedCards([...selectedCards, card]);
         }
@@ -44,24 +66,43 @@ export const CardSelector = memo(() => {
         <section className="flex flex-col gap-6">
             <div className="flex items-center justify-between">
                 <h2 className="text-xl font-semibold text-neutral-900">카드</h2>
-                <AllSelectInstitutionOptions
-                    isAllSelected={selectedCards.length === cards.length}
-                    onClick={handleSelectAllCards}
-                />
+                {loginType === CodefLoginType.Certificate && (
+                    <AllSelectInstitutionOptions
+                        isAllSelected={selectedCards.length === cards.length}
+                        onClick={handleSelectAllCards}
+                    />
+                )}
             </div>
             <div className="grid grid-cols-5 gap-4">
-                {cards.map((card) => (
-                    <InstitutionOption
-                        key={card.param}
-                        logo={card.logo}
-                        title={card.displayName}
-                        isSelected={selectedCards.some((b) => b.param === card.param)}
-                        isAllSelected={isAllSelected}
-                        isDisabled={disabledCardParams.includes(card.param)}
-                        onClick={() => handleSelectCard(card)}
-                    />
-                ))}
+                {cards.map((card) => {
+                    const isConnected =
+                        loginType === CodefLoginType.IdAccount
+                            ? result.items.some((account) => account.company === card.displayName)
+                            : false; // * TODO: 공동인증서 연동 체크 영역
+                    return (
+                        <InstitutionOption
+                            key={card.param}
+                            logo={card.logo}
+                            title={card.displayName}
+                            connect={isConnected}
+                            isSelected={selectedCards.some((b) => b.param === card.param)}
+                            isAllSelected={isAllSelected}
+                            isDisabled={disabledCardParams.includes(card.param)}
+                            onClick={() => handleSelectCard(card)}
+                        />
+                    );
+                })}
             </div>
+
+            {/* 로그인/비밀번호로 연동하는 경우 모달을 이용해 연결 */}
+            {cardCompany && (
+                <ConnectStepsModal
+                    cardCompany={cardCompany}
+                    setCardCompany={setCardCompany}
+                    isConnectStepsModalOpen={isConnectStepsModalOpen}
+                    setIsConnectStepsModalOpen={setIsConnectStepsModalOpen}
+                />
+            )}
         </section>
     );
 });
