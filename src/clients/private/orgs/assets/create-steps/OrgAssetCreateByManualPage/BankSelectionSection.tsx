581be76a2b9928f05edb24d90c33ed19b81683e5
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-05-13 18:06:59 +0900

    refactor: 계좌 수동등록 컴포넌트 분리
    - form 영역을 BankManualForm로 분리

--- src/clients/private/orgs/assets/create-steps/OrgAssetCreateByManualPage/BankSelectionSection.tsx
+++ src/clients/private/orgs/assets/create-steps/OrgAssetCreateByManualPage/BankSelectionSection.tsx
@@ -1,24 +1,3 @@
-import { orgIdParamState } from '^atoms/common';
-import { FormContainer } from '^clients/private/_components/containers/FormContainer';
-import { FormControl } from '^clients/private/_components/inputs/FormControl';
-import { FadeUp } from '^components/FadeUp';
-import { bankAccountApi } from '^models/BankAccount/api';
-import { BankAccountKind } from '^models/BankAccount/type/BankAccountKind.enum';
-import { BankAccountUsingStatus } from '^models/BankAccount/type/BankAccountUsingStatus.enum';
-import { CreateBankAccountRequestDto } from '^models/BankAccount/type/create.bank-account.request.dto';
-import { BankAccountsStaticData } from '^models/CodefAccount/bank-account-static-data';
-import { OrgBankAccountListPageRoute } from '^pages/orgs/[id]/bankAccounts';
-import { errorNotify } from '^utils/toast-notify';
-import { ConnectMethodCard } from '^v3/V3OrgConnectsPage/ConnectsPageBody/ConnectMethodCard';
-import { ArrowLeft } from 'lucide-react';
-import { useRouter } from 'next/router';
-import { memo, useEffect, useState } from 'react';
-import { UseFormReturn } from 'react-hook-form';
-import { toast } from 'react-toastify';
-import { useRecoilValue } from 'recoil';
-import { BankAccountHoldingMemberIdSelect } from '../../bank-accounts/OrgBankAccountNewPage/BankAccountHoldingMemberIdSelect';
-import { BankAccountIsPersonalSelect } from '../../bank-accounts/OrgBankAccountNewPage/BankAccountIsPersonalSelect';
-import { BankUsingStatusSelect } from '../../bank-accounts/OrgBankAccountNewPage/BankUsingStatusSelect';
 
 interface BankSelectionSectionProps {
     onSelect: (bank: BankAccountsStaticData | null) => void;
@@ -29,9 +8,6 @@ interface BankSelectionSectionProps {
 }
 
 export const BankSelectionSection = memo(function BankSelectionSection({ onSelect, selectedBank, form, onBack, isPersonal }: BankSelectionSectionProps) {
-    const [isLoading, setIsLoading] = useState(false);
-    const router = useRouter();
-    const orgId = useRecoilValue(orgIdParamState);
 
     const handleBankSelect = (bank: BankAccountsStaticData) => {
         if (selectedBank?.param === bank.param) {
@@ -41,27 +17,7 @@ export const BankSelectionSection = memo(function BankSelectionSection({ onSelec
         }
     };
 
-    useEffect(() => {
-        if (!router.isReady) return;
-        form.setValue('usingStatus', BankAccountUsingStatus.InUse);
-        form.setValue('isPersonal', isPersonal);
-        form.setValue('kind', BankAccountKind.DEPOSIT_TRUST);
-    }, [router.isReady]);
 
-    const onSubmit = async (data: CreateBankAccountRequestDto) => {
-        if (!data.name) {
-            toast.error('계좌 별칭을 입력해주세요');
-            return;
-        }
-
-        setIsLoading(true);
-        bankAccountApi
-            .create(orgId, data)
-            .then(() => toast.success('계좌를 추가했어요.'))
-            .then(() => router.push(OrgBankAccountListPageRoute.path(orgId)))
-            .catch(errorNotify)
-            .finally(() => setIsLoading(false));
-    };
 
     return (
         <section className="relative mb-20">
@@ -110,64 +66,6 @@ export const BankSelectionSection = memo(function BankSelectionSection({ onSelec
                 </div>
             )}
 
-            <FadeUp show={!!selectedBank} delay="delay-[50ms]" leaveDuration="duration-0" leaveNoEffect>
-                <FormContainer onSubmit={form.handleSubmit(onSubmit)} isLoading={isLoading}>
-                    <input type="hidden" name="issuerCompany" value={selectedBank?.displayName} />
-                    <div className="px-4 py-8 border-b">
-                        <div className="max-w-md mx-auto flex flex-col gap-8 mb-16">
-                            <h2 className="leading-none text-xl font-semibold">필수정보</h2>
-                            <FormControl label="계좌 이름" required>
-                                <input
-                                    className={`input input-underline !bg-slate-100 w-full ${isLoading ? 'opacity-50 pointer-events-none' : ''
-                                        }`}
-                                    readOnly={isLoading}
-                                    {...form.register('name', { required: true })}
-                                    required
-                                />
-                                <span />
-                            </FormControl>
-                        </div>
-
-                        <div className="max-w-md mx-auto flex flex-col gap-8">
-                            <h2 className="leading-none text-xl font-semibold">선택정보</h2>
-                            <FormControl label="계좌 번호">
-                                <input
-                                    type={'number'}
-                                    className={`input input-underline !bg-slate-100 w-full ${isLoading ? 'opacity-50 pointer-events-none' : ''
-                                        }`}
-                                    readOnly={isLoading}
-                                    {...form.register('number')}
-                                />
-                                <span />
-                            </FormControl>
-                            <BankUsingStatusSelect
-                                isLoading={isLoading}
-                                defaultValue={form.getValues('usingStatus') || BankAccountUsingStatus.InUse}
-                                onChange={(status) => form.setValue('usingStatus', status)}
-                            />
-                            <BankAccountIsPersonalSelect
-                                isLoading={isLoading}
-                                defaultValue={form.getValues('isPersonal') ?? undefined}
-                                onChange={(isPersonal) => form.setValue('isPersonal', isPersonal || false)}
-                            />
-                            <BankAccountHoldingMemberIdSelect
-                                isLoading={isLoading}
-                                defaultValue={form.getValues('holdingMemberId') || undefined}
-                                onChange={(member) => form.setValue('holdingMemberId', member)}
-                            />
-                            <FormControl label="비고">
-                                <input
-                                    className={`input input-underline !bg-slate-100 w-full ${isLoading ? 'opacity-50 pointer-events-none' : ''
-                                        }`}
-                                    readOnly={isLoading}
-                                    {...form.register('memo')}
-                                />
-                                <span />
-                            </FormControl>
-                        </div>
-                    </div>
-                </FormContainer>
-            </FadeUp>
         </section>
     );
 });
