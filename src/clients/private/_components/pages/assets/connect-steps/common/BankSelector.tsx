8091e45477bbeffdaae5378bae6c5991129f1594
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-05-13 17:26:42 +0900

    remove/style: 단순 import문 정리 및 제거, 스타일 수정

--- src/clients/private/_components/pages/assets/connect-steps/common/BankSelector.tsx
+++ src/clients/private/_components/pages/assets/connect-steps/common/BankSelector.tsx
@@ -41,7 +41,7 @@ export const BankSelector = memo((props: BankSelectorProps) => {
                     onClick={handleSelectAllBanks}
                 />
             </div>
-            <div className="grid grid-cols-5 gap-4">
+            <div className="grid grid-cols-2 md2:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                 {banks.map((bank) => (
                     <InstitutionOption
                         key={bank.param}
