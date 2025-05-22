e486910422f606e8c6a5ac3df5e5e3cc62be4c0b
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-05-22 20:26:47 +0900

    feat: codef에 공동인증서 인증을 통해 요청 시 성공값과 실패값을 담기위한 interface 추가

---
+++ src/models/CodefAccount/type/CreateCodefAssets.ts
@@ -0,0 +1,14 @@
+import {AccountCreatedResponseDto} from '^models/CodefAccount/type/create-account.response.dto';
+import {BankAccountsStaticData} from '^models/CodefAccount/bank-account-static-data';
+import {CardAccountsStaticData} from '^models/CodefAccount/card-accounts-static-data';
+import {CodefAccountDto} from '^models/CodefAccount/type/CodefAccountDto';
+
+export interface CreateCodefBankAssets {
+    successes: {item: BankAccountsStaticData; response: CodefAccountDto}[];
+    failures: {item: BankAccountsStaticData | CardAccountsStaticData; reason: any}[];
+}
+
+export interface CreateCodefCardAssets {
+    successes: {item: CardAccountsStaticData; response: CodefAccountDto}[];
+    failures: {item: BankAccountsStaticData | CardAccountsStaticData; reason: any}[];
+}
