8990d1bec80ac62b03e97dae06b1ee857f9d10eb
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-05-22 09:03:37 +0900

    CodefBankAccountDto 추가

---
+++ src/models/CodefBankAccount/type/CodefBankAccount.dto.ts
@@ -0,0 +1,40 @@
+import {CodefBillingHistoryDto} from '^models/CodefBillingHistory/type';
+import {TypeCast} from '^types/utils/class-transformer';
+import {CodefAccountDto} from '^models/CodefAccount/type/CodefAccountDto';
+import {BankAccountDto} from '^models/BankAccount/type';
+
+/**
+ * [codef] 연동된 은행 계좌
+ */
+export class CodefBankAccountDto {
+    id: number; // ID
+    accountId: number; // 계정 정보 FK
+    bankAccountId: number | null; // 등록된 계좌 FK
+    @TypeCast(() => Date) syncedStartDate: Date | null; // 연동된 거래내역 시작일시
+    @TypeCast(() => Date) syncedEndDate: Date | null; // 연동된 거래내역 종료일시
+    resOverdraftAcctYN: boolean | null; // 마이너스 통장 여부
+    resAccountDisplay: string; // 계좌번호_표시용
+    resLastTranDate: string | null; // 최종거래일
+    resLoanKind: string | null; // 대출종류
+    resLoanBalance: string | null; // 대출잔액
+    resLoanStartDate: string | null; // 대출신규일
+    resLoanEndDate: string | null; // 대출만기일
+    resEarningsRate: string | null; // 수익률
+    resAccountInvestedCost: string | null; // 투자원금
+    resAccountLoanExecNo: string | null; // 대출실행번호
+    resAccountLifetime: string | null; // 평생계좌번호
+    resAccountEndDate: string | null; // 만기일
+    resAccountStartDate: string | null; // 신규일
+    resAccountNickName: string | null; // 계좌별칭
+    resAccountCurrency: string; // 통화코드
+    resAccountBalance: string; // 현재잔액
+    resAccount: string; // 계좌번호
+    resAccountDeposit: string; // 예금구분
+    resAccountName: string; // 계좌명(종류)
+    @TypeCast(() => Date) createdAt: Date; // 생성일시
+    @TypeCast(() => Date) updatedAt: Date; // 수정일시
+
+    @TypeCast(() => CodefAccountDto) account?: CodefAccountDto; // 계정 정보
+    @TypeCast(() => BankAccountDto) bankAccount?: BankAccountDto; // 등록된 계좌
+    @TypeCast(() => CodefBillingHistoryDto) codefBillingHistories?: CodefBillingHistoryDto[]; // 코드에프 결제내역
+}
