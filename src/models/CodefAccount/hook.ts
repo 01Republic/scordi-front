87f48f78e3ad5a54eb3ecdd497232e15a8c6d908
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-05-22 20:15:37 +0900

    feat: 코드에프-은행사의 계좌를 단건,다건 으로 조회하는 hook 추가

--- src/models/CodefAccount/hook.ts
+++ src/models/CodefAccount/hook.ts
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import {useQueryClient} from '@tanstack/react-query';
+import {useMemo, useState} from 'react';
+import {useQueries, useQuery, useQueryClient} from '@tanstack/react-query';
 import {Paginated} from '^types/utils/paginated.dto';
 import {PagedResourceAtoms, usePagedResource} from '^hooks/usePagedResource';
 import {CardAccountsStaticData} from '^models/CodefAccount/card-accounts-static-data';
@@ -7,6 +7,8 @@ import {codefAccountApi} from './api';
 import {codefAccountsAlreadyIs, codefAccountsInConnector} from './atom';
 import {CodefAccountDto} from './type/CodefAccountDto';
 import {FindAllAccountQueryDto} from './type/find-all-account.query.dto';
+import {FindAllBankAccountQueryDto} from '^models/CodefBankAccount/type/find-all.bank-account.query.dto';
+import {CodefBankAccountDto} from '^models/CodefBankAccount/type/CodefBankAccount.dto';
 
 /** 구독 불러오기 (연동페이지) 에서, 연결된 카드사 계정 리스트를 보여줄 때 사용 */
 export const useCodefAccountsInConnector = () => useCodefAccountsV3(codefAccountsInConnector);
@@ -59,6 +61,58 @@ const useCodefAccountsV3 = (atoms: PagedResourceAtoms<CodefAccountDto, FindAllAc
         getId: 'id',
         mergeMode,
     });
+};
+
+/* 코드에프 계좌 조회 - 은행사의 계좌를 조회 */
+export const useFindBankAccount = (orgId: number, accountId: number, params?: FindAllBankAccountQueryDto) => {
+    const queryParams = {
+        sync: true,
+        itemsPerPage: 0,
+        ...params,
+    };
+
+    return useQuery({
+        queryKey: ['findBankAccount', orgId, accountId, queryParams],
+        queryFn: () =>
+            codefAccountApi
+                .findBankAccounts<CodefBankAccountDto>(orgId, accountId, queryParams)
+                .then((res) => res.data),
+        enabled: !!orgId && !isNaN(orgId),
+    });
+};
+
+/* 코드에프 계좌 조회 - 여러은행사의 계좌를 조회 */
+export const useFindBankAccounts = (orgId: number, accountIds: number[], params?: FindAllBankAccountQueryDto) => {
+    const queryResults = useQueries({
+        queries: accountIds.map((accountId) => {
+            const queryParams = {
+                relations: ['account'],
+                where: {accountId: accountId},
+                sync: true,
+                itemsPerPage: 0,
+                ...params,
+            };
+
+            return {
+                queryKey: ['findBankAccounts', orgId, accountId, queryParams],
+                queryFn: () =>
+                    codefAccountApi
+                        .findBankAccounts<CodefBankAccountDto>(orgId, accountId, queryParams)
+                        .then((res) => res.data.items),
+                enabled: !!orgId && !isNaN(orgId) && accountId != null,
+            };
+        }),
+    });
+
+    const items = useMemo(
+        () => queryResults.map((codefBankAccount) => codefBankAccount.data ?? []).flat(),
+        [queryResults],
+    );
+
+    const isLoading = queryResults.some((codefBankAccount) => codefBankAccount.isLoading);
+    const isError = queryResults.some((codefBankAccount) => codefBankAccount.isError);
+
+    return {items, isLoading, isError};
 };
 
 export * from './hooks/useCreateCodefAccount';
