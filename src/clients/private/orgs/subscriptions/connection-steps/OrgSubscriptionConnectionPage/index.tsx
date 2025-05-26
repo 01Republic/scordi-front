7b133750bc9e69c230e2810f237a2d039f382bba
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-05-26 11:25:38 +0900

    feat: OrgSubscriptionConnectionPage 페이지 컴포넌트에서 setSuccessCodefBanks와 entryPath 추가

--- src/clients/private/orgs/subscriptions/connection-steps/OrgSubscriptionConnectionPage/index.tsx
+++ src/clients/private/orgs/subscriptions/connection-steps/OrgSubscriptionConnectionPage/index.tsx
@@ -2,11 +2,11 @@ import React, {memo} from 'react';
 import {useRouter} from 'next/router';
 import {useSetRecoilState} from 'recoil';
 import {useOrgIdParam} from '^atoms/common';
-import {subscriptionConnectedCodefCardsAtom} from '^models/CodefCard/atom';
-import {AssetConnectPageTemplate} from '^_components/pages/assets/connect-steps';
-import {LinkTo} from '^components/util/LinkTo';
 import {OrgSubscriptionSelectPageRoute} from '^pages/orgs/[id]/subscriptions/select';
 import {OrgSubscriptionConnectionSuccessPageRoute} from '^pages/orgs/[id]/subscriptions/connection/success';
+import {subscriptionConnectedCodefBanksAtom, subscriptionConnectedCodefCardsAtom} from '^models/CodefCard/atom';
+import {LinkTo} from '^components/util/LinkTo';
+import {AssetConnectPageTemplate, EntryPath} from '^_components/pages/assets/connect-steps';
 
 /**
  * 구독 등록
@@ -15,9 +15,11 @@ export const OrgSubscriptionConnectionPage = memo(() => {
     const router = useRouter();
     const orgId = useOrgIdParam();
     const setSuccessCodefCards = useSetRecoilState(subscriptionConnectedCodefCardsAtom);
+    const setSuccessCodefBanks = useSetRecoilState(subscriptionConnectedCodefBanksAtom);
 
     return (
         <AssetConnectPageTemplate
+            entryPath={EntryPath.Subscription}
             ConnectMethodAltActionButton={() => (
                 <LinkTo
                     href={OrgSubscriptionSelectPageRoute.path(orgId)}
@@ -27,8 +29,13 @@ export const OrgSubscriptionConnectionPage = memo(() => {
                     수동으로 등록하기
                 </LinkTo>
             )}
+            onSuccessfullyCreateByCertificate={(codefBanks, codefCards) => {
+                setSuccessCodefBanks(codefBanks || []);
+                setSuccessCodefCards(codefCards || []);
+                router.push(OrgSubscriptionConnectionSuccessPageRoute.path(orgId));
+            }}
             onSuccessfullyCreatedByAccount={(codefCards) => {
-                setSuccessCodefCards(codefCards);
+                setSuccessCodefCards(codefCards || []);
                 router.push(OrgSubscriptionConnectionSuccessPageRoute.path(orgId));
             }}
         />
