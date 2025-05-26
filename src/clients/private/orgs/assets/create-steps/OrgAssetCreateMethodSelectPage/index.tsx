1f8e901ff43934e677679a3370ecfd36bad73049
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-05-26 11:24:55 +0900

    feat: 자산등록/선택페이지에서 setSuccessCodefBanks와 entryPath 추가
    해당 기능이 돌아갈 수 있도록 수정 필요

--- src/clients/private/orgs/assets/create-steps/OrgAssetCreateMethodSelectPage/index.tsx
+++ src/clients/private/orgs/assets/create-steps/OrgAssetCreateMethodSelectPage/index.tsx
@@ -1,23 +1,26 @@
 import React, {memo} from 'react';
 import {useRouter} from 'next/router';
-import {AssetConnectPageTemplate} from '^_components/pages/assets/connect-steps';
+import {AssetConnectPageTemplate, EntryPath} from '^_components/pages/assets/connect-steps';
 import {LinkTo} from '^components/util/LinkTo';
 import {useOrgIdParam} from '^atoms/common';
 import {OrgAssetsCreateByManualPageRoute} from '^pages/orgs/[id]/assets/new/by-manual';
 import {OrgAssetsCreateCompletePageRoute} from '^pages/orgs/[id]/assets/new/complete';
 import {useSetRecoilState} from 'recoil';
-import {assetConnectedCodefCardsAtom} from '^models/CodefCard/atom';
+import {assetConnectedCodefBanksAtom, assetConnectedCodefCardsAtom} from '^models/CodefCard/atom';
 
 /**
  * 자산 등록
  */
+
 export const OrgAssetCreateMethodSelectPage = memo(() => {
     const router = useRouter();
     const orgId = useOrgIdParam();
     const setSuccessCodefCards = useSetRecoilState(assetConnectedCodefCardsAtom);
+    const setSuccessCodefBanks = useSetRecoilState(assetConnectedCodefBanksAtom);
 
     return (
         <AssetConnectPageTemplate
+            entryPath={EntryPath.Asset}
             ConnectMethodAltActionButton={() => (
                 <LinkTo
                     href={OrgAssetsCreateByManualPageRoute.path(orgId)}
@@ -27,13 +30,8 @@ export const OrgAssetCreateMethodSelectPage = memo(() => {
                     수동으로 등록하기
                 </LinkTo>
             )}
-            // onSuccessfullyCreatedByCertificate={(codefCards, bankAccounts) => {
-            //     setSuccessCodefCards(codefCards);
-            //     // setSuccessBankAccounts(bankAccounts);
-            //     router.push(OrgAssetsCreateCompletePageRoute.path(orgId));
-            // }}
             onSuccessfullyCreatedByAccount={(codefCards) => {
-                setSuccessCodefCards(codefCards);
+                setSuccessCodefCards(codefCards || []);
                 router.push(OrgAssetsCreateCompletePageRoute.path(orgId));
             }}
         />
