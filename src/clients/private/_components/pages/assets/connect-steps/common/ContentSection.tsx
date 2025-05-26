62b5d282a8e90a9092945cb0563919e69a717aa4
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-05-26 11:21:22 +0900

    refactor: ContentSection 컴포넌트 isLoading 상태를 처리할 수 있도록 수정

--- src/clients/private/_components/pages/assets/connect-steps/common/ContentSection.tsx
+++ src/clients/private/_components/pages/assets/connect-steps/common/ContentSection.tsx
@@ -1,41 +1,36 @@
 import React, {memo} from 'react';
-import {WithChildren} from '^types/global.type';
 import {Loader} from 'lucide-react';
 
-interface ContentSectionProps extends WithChildren {
+interface ContentSectionProps {
     text: string;
     handleSelectAll?: () => void;
     isAllSelected: boolean;
     isLoading: boolean;
+    children?: React.ReactNode;
 }
 
 export const ContentSection = memo((props: ContentSectionProps) => {
     const {text, handleSelectAll, isAllSelected, isLoading, children} = props;
     return (
-        <>
+        <section className="flex flex-col gap-5 ">
+            <div className="flex items-center justify-between">
+                <span className="text-xl text-gray-900 font-semibold">{text}</span>
+
+                <button
+                    type="button"
+                    onClick={handleSelectAll}
+                    className="text-scordi cursor-pointer text-base hover:underline "
+                >
+                    {isAllSelected ? '선택취소' : '전체선택'}
+                </button>
+            </div>
             {isLoading ? (
-                <div className=" min-h-5">
-                    <div className="animate-spin">
-                        <Loader />
-                    </div>
+                <div className="flex items-center justify-center h-10">
+                    <Loader className="animate-spin size-3" />
                 </div>
             ) : (
-                <section className="flex flex-col gap-5 ">
-                    <div className="flex items-center justify-between">
-                        <span className="text-xl text-gray-900 font-semibold">{text}</span>
-
-                        <button
-                            type="button"
-                            onClick={handleSelectAll}
-                            className="text-scordi cursor-pointer text-base hover:underline "
-                        >
-                            {isAllSelected ? '선택취소' : '전체선택'}
-                        </button>
-                    </div>
-
-                    {children}
-                </section>
+                <>{children}</>
             )}
-        </>
+        </section>
     );
 });
