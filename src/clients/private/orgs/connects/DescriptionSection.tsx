17547caef436e5c578d089e889029a1fe1d4fa4e
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-06-16 13:27:21 +0900

    feat: DescriptionSection 컴포넌트 추가
    설명영역 반복되는 구조이기에 컴포넌트로 뺌

---
+++ src/clients/private/orgs/connects/DescriptionSection.tsx
@@ -0,0 +1,34 @@
+import {memo, ReactNode} from 'react';
+import {StaticImageData} from 'next/image';
+import {NextImage} from '^components/NextImage';
+
+interface DescriptionSectionProps {
+    title: string;
+    steps: ReactNode[];
+    image: StaticImageData;
+    alt: string;
+}
+
+export const DescriptionSection = memo((props: DescriptionSectionProps) => {
+    const {title, steps, image, alt} = props;
+    return (
+        <section className="flex flex-col gap-5">
+            <span className="text-2xl font-bold text-gray-900">{title}</span>
+            <ul className="list-decimal pl-4 text-16 leading-loose space-y-2">
+                {steps.map((step, idx) => (
+                    <li key={idx}>{step}</li>
+                ))}
+            </ul>
+            <div className="w-full">
+                <NextImage
+                    src={image}
+                    alt={alt}
+                    width={image.width}
+                    height={image.height}
+                    className="w-full h-auto object-contain"
+                    loading="lazy"
+                />
+            </div>
+        </section>
+    );
+});
