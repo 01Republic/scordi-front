85c90258f6b6f7d263f7bb056fb99bec081ae1cf
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-06-16 18:31:53 +0900

    구독상세p / 정보탭 - 결제정보 섹션 - 연결된 카드와 연결된 계좌가 수정될 때 UI 전파체계 재조정

--- src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionInfoTab/SubscriptionPaymentInfoSection/SubscriptionBankAccount.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionInfoTab/SubscriptionPaymentInfoSection/SubscriptionBankAccount.tsx
@@ -4,6 +4,8 @@ import {SubscriptionDto, UpdateSubscriptionRequestDto} from '^models/Subscriptio
 import {BankAccountProfileCompact} from '^models/BankAccount/components';
 import {FormControl} from '^clients/private/_components/inputs/FormControl';
 import {BankAccountSelect} from '^models/BankAccount/components/BankAccountSelect';
+import {useQuery} from '@tanstack/react-query';
+import {bankAccountApi} from '^models/BankAccount/api';
 
 interface SubscriptionBankAccountProps {
     isEditMode?: boolean;
@@ -13,13 +15,23 @@ interface SubscriptionBankAccountProps {
 
 export const SubscriptionBankAccount = memo((props: SubscriptionBankAccountProps) => {
     const {isEditMode, form, subscription} = props;
+    const {id, organizationId: orgId, bankAccountId} = subscription;
+    const bankAccountQuery = useQuery({
+        queryKey: ['subscription.bankAccount', id, bankAccountId],
+        queryFn: async () => {
+            return bankAccountApi.show(orgId, bankAccountId!).then((res) => res.data);
+        },
+        enabled: !!bankAccountId,
+    });
+
+    const defaultBankAccount = bankAccountQuery.data || subscription.bankAccount;
 
     return (
         <FormControl label="연결된 계좌">
             {isEditMode ? (
                 <div className={'input border-gray-200 bg-gray-100 w-full flex flex-col justify-center'}>
                     <BankAccountSelect
-                        defaultValue={subscription.bankAccount}
+                        defaultValue={defaultBankAccount}
                         onChange={(bankAccount) => {
                             form.setValue('bankAccountId', bankAccount?.id || null);
                         }}
@@ -35,7 +47,7 @@ export const SubscriptionBankAccount = memo((props: SubscriptionBankAccountProps
                 </div>
             ) : (
                 <div className="flex items-center h-[50px] font-normal text-16 text-slate-950">
-                    <BankAccountProfileCompact item={subscription.bankAccount} />
+                    <BankAccountProfileCompact item={defaultBankAccount} />
                 </div>
             )}
             <span />
--- src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionInfoTab/SubscriptionPaymentInfoSection/SubscriptionCreditCard.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionDetailPage/SubscriptionInfoTab/SubscriptionPaymentInfoSection/SubscriptionCreditCard.tsx
@@ -1,8 +1,10 @@
 import React, {memo} from 'react';
-import {FormControl} from '^clients/private/_components/inputs/FormControl';
-import {CreditCardProfileCompact, CreditCardSelect} from '^models/CreditCard/components';
 import {UseFormReturn} from 'react-hook-form';
+import {useQuery} from '@tanstack/react-query';
+import {FormControl} from '^clients/private/_components/inputs/FormControl';
+import {creditCardApi} from '^models/CreditCard/api';
 import {SubscriptionDto, UpdateSubscriptionRequestDto} from '^models/Subscription/types';
+import {CreditCardProfileCompact, CreditCardSelect} from '^models/CreditCard/components';
 
 interface SubscriptionCreditCardProps {
     isEditMode?: boolean;
@@ -12,13 +14,23 @@ interface SubscriptionCreditCardProps {
 
 export const SubscriptionCreditCard = memo((props: SubscriptionCreditCardProps) => {
     const {isEditMode, form, subscription} = props;
+    const {id, organizationId: orgId, creditCardId} = subscription;
+    const creditCardQuery = useQuery({
+        queryKey: ['subscription.creditCard', id, creditCardId],
+        queryFn: async () => {
+            return creditCardApi.show(orgId, creditCardId!).then((res) => res.data);
+        },
+        enabled: !!creditCardId,
+    });
+
+    const defaultCreditCard = creditCardQuery.data || subscription.creditCard;
 
     return (
         <FormControl label="연결된 카드">
             {isEditMode ? (
                 <div className={'input border-gray-200 bg-gray-100 w-full flex flex-col justify-center'}>
                     <CreditCardSelect
-                        defaultValue={subscription.creditCard}
+                        defaultValue={defaultCreditCard}
                         onChange={(creditCard) => {
                             form.setValue('creditCardId', creditCard?.id || null);
                         }}
@@ -34,7 +46,7 @@ export const SubscriptionCreditCard = memo((props: SubscriptionCreditCardProps) 
                 </div>
             ) : (
                 <div className="flex items-center h-[50px] font-normal text-16 text-slate-950">
-                    <CreditCardProfileCompact item={subscription.creditCard} />
+                    <CreditCardProfileCompact item={defaultCreditCard} />
                 </div>
             )}
             <span />
