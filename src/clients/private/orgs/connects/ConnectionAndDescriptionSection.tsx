fdbef8ec15f4ac4e6b2f0a296a0001945e5686ff
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-06-17 12:01:05 +0900

    style: ConnectionAndDescriptionSection 단순 스타일 수정

--- src/clients/private/orgs/connects/ConnectionAndDescriptionSection.tsx
+++ src/clients/private/orgs/connects/ConnectionAndDescriptionSection.tsx
@@ -18,39 +18,44 @@ export const ConnectionAndDescriptionSection = memo((props: DescriptionLayoutPro
     const {src, alt, title, warnTexts, connectButton, onClick, children} = props;
     return (
         <PureLayout>
-            <div className="px-40 py-14 grid grid-cols-2 gap-24 items-start">
-                {/* connection section */}
-                <section className="sticky top-14 col-span-1 w-full flex flex-col gap-10">
-                    <BackButton />
-                    <div className="mb-4 flex items-center justify-between">
-                        <NextImage src={src} alt={alt} className="avatar bg-white" width={48} height={48} />
-                        <SafeBadge />
-                    </div>
-                    <span className="text-32 font-semibold text-gray-900 whitespace-pre-line">{title}</span>
-                    <div className="flex flex-col gap-3 rounded-box p-4 bg-red-50 text-red-400 border border-red-400">
-                        <p className="font-semibold text-20">잠깐, 다음과 같은 안내를 확인해주세요.</p>
-                        <ul className="list-disc pl-4 font-normal text-18">
-                            {warnTexts.map((warn, idx) => (
-                                <li key={idx}>{warn}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="w-full grid grid-cols-2 gap-4">{connectButton}</div>
-                </section>
+            <div className="px-40 py-14">
+                <div className="flex flex-col gap-10">
+                    <section className="sticky top-14">
+                        <BackButton />
+                    </section>
+                    <div className="grid grid-cols-2 gap-24 items-start">
+                        {/* connection 섹션 */}
+                        <section className="sticky top-[7.5rem] flex flex-col gap-10">
+                            <div className="flex items-center justify-between">
+                                <NextImage src={src} alt={alt} className="avatar bg-white" width={48} height={48} />
+                                <SafeBadge />
+                            </div>
+                            <span className="text-32 font-semibold text-gray-900 whitespace-pre-line">{title}</span>
+                            <div className="flex flex-col gap-3 rounded-box p-4 bg-red-50 text-red-400 border border-red-400">
+                                <p className="font-semibold text-20">잠깐, 다음과 같은 안내를 확인해주세요.</p>
+                                <ul className="list-disc pl-4 font-normal text-18">
+                                    {warnTexts.map((warn, idx) => (
+                                        <li key={idx}>{warn}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="w-full grid grid-cols-2 gap-4">{connectButton}</div>
+                        </section>
 
-                {/* description section */}
-                <section className="col-span-1">
-                    {/*<div></div>*/}
-                    <div className="w-full flex flex-col gap-20">
-                        {children}
-                        <section className="flex flex-col gap-6">
-                            <p className="text-2xl font-bold">연결을 시작해볼까요?</p>
-                            <button onClick={onClick} className="btn btn-md btn-block btn-scordi">
-                                연결 시작하기
-                            </button>
+                        {/* description */}
+                        <section className="col-span-1">
+                            <div className="w-full flex flex-col gap-20">
+                                {children}
+                                <section className="flex flex-col gap-6">
+                                    <p className="text-2xl font-bold">연결을 시작해볼까요?</p>
+                                    <button onClick={onClick} className="btn btn-md btn-block btn-scordi">
+                                        연결 시작하기
+                                    </button>
+                                </section>
+                            </div>
                         </section>
                     </div>
-                </section>
+                </div>
             </div>
         </PureLayout>
     );
