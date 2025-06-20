1d0f8480dce410f8587d305beb3f172cfe99c2a1
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-06-20 16:59:08 +0900

    utils/ dateTime - '방금전' 과 같은 ui 친화적인 형태의 formatDate 함수 추가

--- src/utils/dateTime.ts
+++ src/utils/dateTime.ts
@@ -1,5 +1,5 @@
 import {captures} from '^utils/array';
-import {format, formatDistance} from 'date-fns';
+import {format, formatDistance, formatDistanceToNow} from 'date-fns';
 import {ko} from 'date-fns/locale';
 
 export const zeroPad = (num: string): string => (num.length == 1 ? `0${num}` : num);
@@ -278,6 +278,21 @@ export function ago(
     const distanceOfDay = Math.floor(distance / (1000 * 3600 * 24));
 
     return distanceOfDay > 0 ? format(target, 'yyyy-MM-dd', options) : `${formatDistance(target, base, options)}전`;
+}
+
+export function formatDate(date: Date) {
+    const d = new Date(date);
+    const now = Date.now();
+    const diff = (now - d.getTime()) / 1000; // 현재 시간과의 차이(초)
+    if (diff < 60 * 1) {
+        // 1분 미만일땐 방금 전 표기
+        return '방금 전';
+    }
+    if (diff < 60 * 60 * 24 * 3) {
+        // 3일 미만일땐 시간차이 출력(몇시간 전, 몇일 전)
+        return formatDistanceToNow(d, {addSuffix: true, locale: ko});
+    }
+    return format(d, 'P p', {locale: ko}); // 날짜 포맷
 }
 
 /**
