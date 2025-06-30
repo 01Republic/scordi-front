524052107a8f0678da14074fb97356e828d08dcc
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-06-30 11:54:01 +0900

    refactor: 비밀번호 검증 시 특수문자 필수가 아닐 수 있도록 수정

--- src/utils/valildation.ts
+++ src/utils/valildation.ts
@@ -3,4 +3,4 @@ export const validateEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 /* 사업자등록번호 검증 */
 export const validBizNoRegex = /^(\d{3})-?(\d{2})-?(\d{5})$/;
 /* 비밀번호 검증 */
-export const validPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,16}$/;
+export const validPasswordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,20}$/;
