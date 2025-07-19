0e85de55586a74bfcbd3ccc7180b4fce793753a1
seongonecho <seongonecho@gmail.com>
2025-07-20 01:44:30 +0900

    푸터

--- src/clients/private/_layouts/_shared/Footer.tsx
+++ src/clients/private/_layouts/_shared/Footer.tsx
@@ -4,9 +4,11 @@ import {useRecoilValue} from 'recoil';
 import {currentOrgAtom} from '^models/Organization/atom';
 import {V3OrgHomePageRoute} from '^pages/v3/orgs/[orgId]';
 import {termsUrl} from '^config/environments';
+import {useTranslation} from 'next-i18next';
 
 export const Footer = memo(function Footer() {
     const currentOrg = useRecoilValue(currentOrgAtom);
+    const {t} = useTranslation('common');
 
     return (
         <footer className="container px-4 md:max-w-screen-lg flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-8 py-6 sticky top-[100%]">
@@ -20,11 +22,10 @@ export const Footer = memo(function Footer() {
 
             <div className="flex-auto text-gray-500">
                 <p>
-                    (주) 제로원리퍼블릭 (01Republic, Inc.) 대표: 김용현, 김규리 <br className="sm:hidden" />{' '}
-                    사업자등록번호: 227-86-02683 <br />
-                    통신판매업번호: 2023-서울서초-3752 <br className="sm:hidden" /> 주소: 서울시 종로구 종로 6, 5층{' '}
-                    <br className="sm:hidden" /> 고객센터 유선전화: 02-2088-6337 <br />
-                    Copyright 2024 © 01Republic. All Rights Reserved
+                    {t('footer.companyName')} {t('footer.ceo')} <br className="sm:hidden" /> {t('footer.regNo')} <br />
+                    {t('footer.salesNo')} <br className="sm:hidden" /> {t('footer.address')}{' '}
+                    <br className="sm:hidden" /> {t('footer.customerService')} <br />
+                    {t('footer.copyright')}
                     {currentOrg && (
                         <LinkTo text="." href={V3OrgHomePageRoute.path(currentOrg.id)} displayLoading={false} />
                     )}
@@ -32,8 +33,8 @@ export const Footer = memo(function Footer() {
             </div>
 
             <div className="flex flex-wrap items-center gap-3 font-semibold text-14">
-                <LinkTo target="_blank" href={termsUrl.privacy} text="개인정보처리방침" displayLoading={false} />
-                <LinkTo target="_blank" href={termsUrl.serviceUsage} text="이용약관" displayLoading={false} />
+                <LinkTo target="_blank" href={termsUrl.privacy} text={t('footer.privacy')} displayLoading={false} />
+                <LinkTo target="_blank" href={termsUrl.serviceUsage} text={t('footer.terms')} displayLoading={false} />
             </div>
         </footer>
     );
