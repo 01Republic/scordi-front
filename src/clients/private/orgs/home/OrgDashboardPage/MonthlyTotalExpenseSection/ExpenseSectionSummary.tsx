2800697338f2eed1570db5666e402550ae2c68bf
seongonecho <seongonecho@gmail.com>
2025-07-20 13:29:59 +0900

    갯수 단위

--- src/clients/private/orgs/home/OrgDashboardPage/MonthlyTotalExpenseSection/ExpenseSectionSummary.tsx
+++ src/clients/private/orgs/home/OrgDashboardPage/MonthlyTotalExpenseSection/ExpenseSectionSummary.tsx
@@ -1,6 +1,7 @@
 import {memo} from 'react';
 import {currencyFormat, roundNumber, unitFormat} from '^utils/number';
 import {SummaryOfState} from '^types/dashboard.type';
+import {useTranslation} from 'next-i18next';
 
 interface ExpenseSectionSummaryProps {
     summaryOfState?: SummaryOfState;
@@ -8,12 +9,13 @@ interface ExpenseSectionSummaryProps {
 
 export const ExpenseSectionSummary = memo((props: ExpenseSectionSummaryProps) => {
     const {summaryOfState} = props;
+    const {t} = useTranslation('common');
 
     return (
         <p className="font-bold text-20 md:text-24 lg:text-28 flex items-center gap-2 lg:gap-3">
             <span>{currencyFormat(roundNumber(summaryOfState?.amount || 0))}</span>
             <span>·</span>
-            <span>{unitFormat(summaryOfState?.count || 0, '개')}</span>
+            <span>{unitFormat(summaryOfState?.count || 0, t('items') as string)}</span>
         </p>
     );
 });
