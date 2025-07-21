1997bf75739e9cd2d3c4030e4101b50f043de611
miju <im.ymiju@gmail.com>
2025-07-21 18:38:03 +0900

    feat/1개 또는 다른 구독을 합치는 경우 툴팁알림 및 버튼 비활성화

--- src/clients/private/orgs/subscriptions/OrgSubscriptionListPage/SubscriptionTableRow/BottomAction/BottomActionBar.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionListPage/SubscriptionTableRow/BottomAction/BottomActionBar.tsx
@@ -1,3 +1,4 @@
+import Tippy from '@tippyjs/react';
 import {MergeSubscriptionModal} from '^clients/private/orgs/subscriptions/OrgSubscriptionListPage/SubscriptionTableRow/BottomAction/MergeSubscriptionModal';
 import {CheckboxHandler} from '^hooks/useCheckboxHandler';
 import {SubscriptionDto} from '^models/Subscription/types/Subscription.dto';
@@ -13,19 +14,40 @@ export const BottomActionBar = (props: BottomActionBarProps) => {
     const {items, onClear} = props;
     const itemCount = items.checkedItems.length;
     const [isMergeSubscriptionModal, setIsMergeSubscriptionModal] = useState(false);
+
+    const uniqueProductCount = new Set(items.checkedItems.map((subscription) => subscription.productId)).size;
+
+    const canMerge = itemCount >= 2 && uniqueProductCount === 1;
+
+    const tippyMessage =
+        itemCount < 2
+            ? '두개 이상 선택 시 병합이 가능합니다'
+            : uniqueProductCount > 1
+            ? '서로 다른 구독은 병합할 수 없습니다'
+            : '';
+
     return (
         <div className="flex justify-center items-center w-full text-14">
             <div className="flex flex-row gap-3 items-center px-6 py-1 bg-white rounded-full border border-gray-300 shadow-lg min-w-md">
                 <span className="text-gray-600">{itemCount}개 선택됨</span>
-                <button
-                    className={`flex gap-1 btn btn-sm no-animation btn-animation ${
-                        itemCount < 2 ? 'btn-disabled2' : 'btn-white'
-                    }`}
-                    onClick={() => setIsMergeSubscriptionModal(true)}
-                >
-                    <Merge />
-                    구독 병합
-                </button>
+
+                {canMerge ? (
+                    <button
+                        className="flex gap-1 btn btn-sm no-animation btn-animation btn-white"
+                        onClick={() => setIsMergeSubscriptionModal(true)}
+                    >
+                        <Merge />
+                        구독 병합
+                    </button>
+                ) : (
+                    <Tippy content={tippyMessage}>
+                        <div className="flex gap-1 text-gray-400 bg-gray-200 btn btn-sm no-animation btn-animation hover:!bg-gray-150">
+                            <Merge />
+                            구독 병합
+                        </div>
+                    </Tippy>
+                )}
+
                 <RotateCcw className="text-gray-500 cursor-pointer hover:text-red-400" onClick={onClear} />
             </div>
             <MergeSubscriptionModal
