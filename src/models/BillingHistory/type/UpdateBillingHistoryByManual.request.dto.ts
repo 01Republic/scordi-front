40b7e68eaab0770f1b202a01117d2b891f3d26a0
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-08-01 11:23:12 +0900

    refactor: subscriptionId 타입 선언 위치 이동
    UpdateBillingHistoryByManualRequestDto →  CreateBillingHistoryByManualRequestDto

--- src/models/BillingHistory/type/UpdateBillingHistoryByManual.request.dto.ts
+++ src/models/BillingHistory/type/UpdateBillingHistoryByManual.request.dto.ts
@@ -1,7 +1,4 @@
 import {PartialType} from '^types/utils/partial-type';
 import {CreateBillingHistoryByManualRequestDto} from '^models/BillingHistory/type/CreateBillingHistoryByManual.request.dto';
 
-export class UpdateBillingHistoryByManualRequestDto extends PartialType(CreateBillingHistoryByManualRequestDto) {
-    // 변경할 구독 ID
-    subscriptionId?: number;
-}
+export class UpdateBillingHistoryByManualRequestDto extends PartialType(CreateBillingHistoryByManualRequestDto) {}
