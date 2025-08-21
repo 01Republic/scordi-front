0e1eb9c92d56e8d20a4154d907852449de3f150c
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2025-08-21 20:07:34 +0900

    Update OutLink.tsx

--- src/components/OutLink.tsx
+++ src/components/OutLink.tsx
@@ -1,7 +1,8 @@
 import React, {HTMLAttributeAnchorTarget, memo, ReactNode} from 'react';
+import {ExternalLink} from 'lucide-react';
+import {cn} from '^public/lib/utils';
 import {WithChildren} from '^types/global.type';
 import {LinkTo} from '^components/util/LinkTo';
-import {ExternalLink} from 'lucide-react';
 
 interface OutLinkProps {
     href: string;
@@ -26,7 +27,31 @@ export const OutLink = memo((props: OutLinkProps & WithChildren) => {
                 <LinkTo
                     href={href}
                     target={target}
-                    className={`link text-gray-400 hover:text-gray-800 transition-all inline-flex items-center gap-1 ${className}`}
+                    className={cn(`link text-gray-400 hover:text-gray-800 transition-all`, className)}
+                >
+                    {children || text || href}
+                    <span className="inline-block relative top-0.5 ml-1"> {icon ? icon : <ExternalLink />}</span>
+                </LinkTo>
+            ) : (
+                <a className="inline-flex items-center">{children || text || href}</a>
+            )}
+        </>
+    );
+});
+
+export const OutLink2 = memo((props: OutLinkProps & WithChildren) => {
+    const {href, text, target = '_blank', icon, className = '', children} = props;
+
+    return (
+        <>
+            {isLinkString(href) ? (
+                <LinkTo
+                    href={href}
+                    target={target}
+                    className={cn(
+                        `link text-gray-400 hover:text-gray-800 transition-all inline-flex items-center gap-1`,
+                        className,
+                    )}
                 >
                     <span>{children || text || href}</span>
                     {icon ? icon : <ExternalLink size={11} />}
