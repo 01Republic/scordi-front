ef743cf6dbaa887348347f76bab13b8f75924859
MIJU YOON <134301942+anywhereim@users.noreply.github.com>
2025-08-26 18:19:47 +0900

    코어라인/구독리스트페이지/그룹별 product라인에 마지막 결제 금액 합산하여 노출

--- src/clients/private/orgs/subscriptions/OrgSubscriptionListPage/GroupedByProductTable/GroupedByProductTableRow.tsx
+++ src/clients/private/orgs/subscriptions/OrgSubscriptionListPage/GroupedByProductTable/GroupedByProductTableRow.tsx
@@ -1,10 +1,13 @@
 import React, {memo, useState} from 'react';
-import {useRecoilState} from 'recoil';
+import {useRecoilState, useRecoilValue} from 'recoil';
 import {ChevronDown, ChevronRight} from 'lucide-react';
 import {ProductDto} from '^models/Product/type';
 import {checkedSubscriptionList} from '../atom';
 import {ProductProfile} from './ProductProfile';
 import {GroupedSubscriptionTableRow} from './GroupedSubscriptionTableRow';
+import {displayCurrencyAtom} from '^tasting/pageAtoms';
+import {getCurrencySymbol} from '^api/tasting.api/gmail/agent/parse-email-price';
+import {currencyFormat} from '^utils/number';
 
 interface GroupedByProductTableRowProps {
     product: ProductDto;
@@ -14,14 +17,23 @@ interface GroupedByProductTableRowProps {
 export const GroupedByProductTableRow = memo((props: GroupedByProductTableRowProps) => {
     const {product, reload} = props;
     const [checkedItems, setCheckedItems] = useRecoilState(checkedSubscriptionList);
+    const displayCurrency = useRecoilValue(displayCurrencyAtom);
     const [isOpen, setIsOpen] = useState(true);
 
     const hoverBgColor = 'group-hover:bg-scordi-light-50 transition-all';
 
+    const symbol = getCurrencySymbol(displayCurrency);
+
+    const totalSubscriptionAmountByProduct =
+        product.subscriptions?.reduce(
+            (sum, subscription) => sum + (subscription.currentBillingAmount?.toDisplayPrice(displayCurrency) || 0),
+            0,
+        ) || 0;
+
     return (
         <>
             <tr>
-                <td className={`pr-1 pl-3 text-start ${hoverBgColor}`} colSpan={11}>
+                <td className={`pr-1 pl-3 text-start ${hoverBgColor}`} colSpan={6}>
                     <button
                         type="button"
                         className="flex gap-2 justify-center items-center"
@@ -31,6 +43,13 @@ export const GroupedByProductTableRow = memo((props: GroupedByProductTableRowPro
                         <ProductProfile product={product} />
                     </button>
                 </td>
+                <td className="text-right" colSpan={1}>
+                    <p className="text-sm">
+                        <small className="mr-1">{symbol}</small>
+                        <span>{currencyFormat(totalSubscriptionAmountByProduct, '')}</span>
+                    </p>
+                </td>
+                <td colSpan={4} />
             </tr>
             {isOpen &&
                 product.subscriptions?.map((subscription) => {
